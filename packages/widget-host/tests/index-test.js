911b73e1de6e8e8291fa005d3bedb184f9842aea
jemason <eric.mason@yupiq.com>
2018-01-31 16:35:05 -0800

    EM updated test

--- packages/widget-host/tests/index-test.js
+++ packages/widget-host/tests/index-test.js
@@ -20,7 +20,7 @@ describe('Module template', () => {
     window.widgetIdent = {
       userId: "5a6bcb1d66d5cf27ae5968f7",
       accountId: "TF3H82GWUL3CUU8I", 
-      token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7I…yJ9fQ.MZ-xq2hpVvbA_usK-fSkvb6zJmh6tp7uGnFPDng1RWE", 
+      token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImFjY291bnRJZCI6IlRGM0g4MkdXVUwzQ1VVOEkiLCJmaXJzdE5hbWUiOiJDb3JuZWxpYSIsImxhc3ROYW1lIjoiV29vZCIsImlkIjoiNWE2YmNiMWQ2NmQ1Y2YyN2FlNTk2OGY3IiwiZW1haWwiOiJjb3JuZWxpYXdvb2QudWV4d2x0Z2g2LnVleHdsdGdoQG1haWxvc2F1ci5pbyJ9fQ.MZ-xq2hpVvbA_usK-fSkvb6zJmh6tp7uGnFPDng1RWE", 
       tenantAlias: "test_aodmqrtid2vmr", 
       appDomain: "http://localhost:9000"};
 
