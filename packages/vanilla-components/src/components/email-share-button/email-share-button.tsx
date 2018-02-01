8e10820611b36c62dd063fe9c2718b59492ebd95
zmcadie <zolamcadie@gmail.com>
2018-02-01 14:24:39 -0800

    Add shadeColor function for hover state

--- packages/vanilla-components/src/components/email-share-button/email-share-button.tsx
+++ packages/vanilla-components/src/components/email-share-button/email-share-button.tsx
@@ -21,6 +21,40 @@ const mailTo = (url) => {
   }, 500);
 }
 
+const cutHex = (hex) => {
+  return (hex.charAt(0) == "#") ? hex.substring(1) : hex;
+}
+
+const cutRgb = (rgb) => {
+  return (rgb.substring(4, rgb.length - 1))
+}
+
+const hexToRgb = (hex) => {
+  const noHex = cutHex(hex);
+  const r = parseInt(noHex.substring(0, 2), 16);
+  const g = parseInt(noHex.substring(2, 4), 16);
+  const b = parseInt(noHex.substring(4, 6), 16);
+  return `rgb(${r}, ${g}, ${b})`
+}
+
+const isHex = (string) => {
+  return !!string.match(/^#[0-9a-f]{6}$/i);
+}
+
+const isRgb = (string) => {
+  const rgbArray = cutRgb(string).split(", ");
+  if (rgbArray.length !== 3) return false;
+  return rgbArray.every((x) => parseInt(x) >= 0 && parseInt(x) <= 255)
+}
+
+const shadeColor = (color, percent) => {
+  if (!isHex(color) && !isRgb(color)) return;
+  const rgb = (isHex(color)) ? hexToRgb(color) : color;
+  const decimalPercent = percent / 100
+  const shadedArray = cutRgb(rgb).split(", ").map((n) => { return Math.floor(parseInt(n) * decimalPercent)})
+  return `rgb(${shadedArray.join(", ")})`
+}
+
 @Component({
   tag: 'email-share-button',
   styleUrl: 'email-share-button.scss'
@@ -52,13 +86,13 @@ export class EmailShareButton {
   addStyle() {
     const css = ` .emailShare {
                     background-color: ${this.backgroundColor};
-                    border-color: ${this.borderColor};
+                    border: 1px solid ${this.borderColor};
                     color: ${this.textColor};
                   }
                   
                   .emailShare:hover {
-                    background: darken(${this.backgroundColor}, 10%);
-                    border-color: darken(#${this.borderColor}, 12%);
+                    background: ${shadeColor(this.backgroundColor, 10)};
+                    border-color: ${shadeColor(this.borderColor, 12)};
                     color: ${this.textColor};
                   }
                   
