16bdda77fedbf658dd4428364f19920b47d721b5
Jorge Conde <jorge@referralsaasquatch.com>
2018-07-11 14:52:27 -0600

    JC remove template from plugin

--- packages/vanilla-components-grapesjs/src/index.js
+++ packages/vanilla-components-grapesjs/src/index.js
@@ -14,9 +14,5 @@ export default grapesjs.plugins.add('vanilla-components-grapesjs-plugin', (edito
   loadBlocks(editor, options);
 
   // TODO Remove
-  editor.on('load', () => editor.addComponents(`<sqh-global-container>
-  <sqh-text-component type='banner'></sqh-text-component>
-  <sqh-text-component type='header'></sqh-text-component>
-  </sqh-global-container>
-  <link rel="stylesheet" type="text/css" href="https://unpkg.com/@saasquatch/vanilla-components-assets@latest/icons.css"></link>`, { at: 0 }))
+  // editor.on('load', () => editor.addComponents(``, { at: 0 }))
 });
