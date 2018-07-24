0f3c16485c28cb904bacdff28df1f03944d44567
unknown <craigricetong@gmail.com>
2018-07-24 09:05:13 -0700

    cleaning up css

--- packages/vanilla-components/src/components/image-component/image-component.tsx
+++ packages/vanilla-components/src/components/image-component/image-component.tsx
@@ -17,13 +17,10 @@ export class ImageComponent {
   render() {
     const imageString = this.url;
     const alignment = css `{
-      position: relative;
       text-align: ${this.alignment};
     }`
     const myStyle = css`{
       width: ${this.width}px;
-      max-width: 100%;
-      height: auto;
       border-radius: ${this.borderradius}px;
       ${this.css}
     }`
