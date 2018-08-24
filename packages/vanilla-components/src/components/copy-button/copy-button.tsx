81f6d30a1644bbb3fb946b8f64b3117e4c3b3b0e
unknown <craigricetong@gmail.com>
2018-08-24 10:48:55 -0700

    added a copy button

---
+++ packages/vanilla-components/src/components/copy-button/copy-button.tsx
@@ -0,0 +1,14 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'sqh-copy-button',
+  styleUrl: 'copy-button.scss'
+})
+export class CopyButton {
+
+  render() {
+    return (
+      <span>Copy Button</span>
+    );
+  }
+}
