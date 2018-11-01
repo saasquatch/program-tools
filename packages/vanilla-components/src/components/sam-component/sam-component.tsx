3a9a3e1c65f2d4a96bfb2dd045837b45f7450ada
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2018-11-01 16:29:23 -0700

    Turned sam-component into styled form which receives input and console logs it

--- packages/vanilla-components/src/components/sam-component/sam-component.tsx
+++ packages/vanilla-components/src/components/sam-component/sam-component.tsx
@@ -1,4 +1,4 @@
-import { Component } from '@stencil/core';
+import { Component, State } from '@stencil/core';
 
 @Component({
     tag: 'sqh-sam-component',
@@ -6,17 +6,41 @@ import { Component } from '@stencil/core';
   })
 
   export class SamComponent {
+    @State() firstName: string;
+    @State() lastName: string;
+    @State() email: string;
+
+    handleSubmit(e) {
+      e.preventDefault()
+      console.log(this.firstName);
+      console.log(this.lastName);
+      console.log(this.email);
+      // send data to our backend
+    }
+
+    handleFirstName(event) {
+      this.firstName = event.target.value;
+    }
+
+    handleLastName(event) {
+      this.lastName = event.target.value;
+    }
+    handleEmail(event) {
+      this.email = event.target.value;
+    }
+
     render() {
-        return (
-            <div>
-							<select name="select1">
-								<option value="ref-widget">Referrer's Widget</option>
-							</select>
-							<select name="select2">
-								<option value="registered">Registered State</option>
-								<option value="unregistered">Unregistered State</option>
-							</select>
-						</div>
-        );
-      }
+      return (
+        <div>
+          <div class="input-group">
+            <form onSubmit={(e) => this.handleSubmit(e)}>
+              <input type="text" class="form-input" value={this.firstName} onInput={(e) => this.handleFirstName(e)} placeholder="First Name" />
+              <input type="text" class="form-input"  value={this.lastName} onInput={(e) => this.handleLastName(e)} placeholder="Last Name" />
+              <input type="text" class="form-input"  value={this.email} onInput={(e) => this.handleEmail(e)} placeholder="Email" />
+              <input type="submit" class="sqh-continue-btn" value="Continue" />
+            </form>
+          </div>
+        </div>
+      );
+    }
   }
