ac24e69cf351d865fe7ddfb740fb3237b1c4ba6c
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2018-11-02 11:25:40 -0700

    waits for promise before displaying whether submission was a fail or success

--- packages/vanilla-components/src/components/sam-component/sam-component.tsx
+++ packages/vanilla-components/src/components/sam-component/sam-component.tsx
@@ -5,19 +5,16 @@ interface FormState {
   lastName?: string;
   email?: string;
   valid?: boolean;
+  loading?: boolean;
 }
 
-function delayPromise(duration) {
-  return function () {
-    return new Promise(function (resolve, reject) {
-      // Loading...
-      setTimeout(function () {
-        // failure?
-        resolve();
-      }, duration)
-    });
-  };
-};
+function delayPromise(duration) { 
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  });
+}
 
 @Component({
   tag: 'sqh-sam-component',
@@ -30,34 +27,74 @@ export class SamComponent {
   async handleSubmit(e) {
     console.log(e)
     e.preventDefault()
+
+    // disable form and load
+    this.formData = {
+      ...this.formData,
+      loading: true
+    }
+
     console.log("Submitted", this.formData);
 
     //API.graphql
     // send data to our backend
+    // display Loading... text
+    await delayPromise(3000);
 
-    // Validation before sending
-
-
-    await delayPromise(3000)
-
-    // success?
-
-
+    if(Math.random() >= 0.5){
+      // Successfully signed up!
+      // Hide form
+      console.log("form submission success")
+    } else {
+      // Something went wrong, please try again.
+      // Form re-enabled
+      this.formData = {
+        ...this.formData,
+        loading: false
+      }
+      console.log("Form submission failed")
+    }
   }
 
-  //validation -- fields are outlined with red
-  //loading -- form is disabled, loading indicator shown
-  //submit success state -- form is hidden, replaced with a success message
-  //submit failure state -- red alert area, form is enabled to try again
-
   handleFirstName(event) {
+    if(!event.target.value){
+      event.target.className = "invalid"
+      this.formData= {
+        ...this.formData,
+         valid: false
+      }
+    } else {
+      event.target.className = "valid"
+      this.formData= {
+        ...this.formData,
+         valid: true
+      }
+    }
+
     this.formData = {
       ...this.formData,
       firstName: event.target.value
     }
+    
   }
 
   handleLastName(event) {
+    if(!event.target.value){
+      event.target.className = "invalid"
+
+      this.formData= {
+        ...this.formData,
+         valid: false
+      }
+    } else {
+      event.target.className = "valid"
+
+      this.formData= {
+        ...this.formData,
+         valid: true
+      }
+    }
+
     this.formData = {
       ...this.formData,
       lastName: event.target.value
@@ -66,15 +103,18 @@ export class SamComponent {
 
   handleEmail(event) {
 
-    if (event.target.validity.typeMismatch || !event.target.value) {
-      this.formData.valid = false;
-      console.log("red")
-      console.log(event.target.class);
+    if (event.target.validity.typeMismatch || !event.target.value) { 
       event.target.className = "invalid"
+      this.formData= {
+        ...this.formData,
+         valid: false
+      }
     } else {
-      console.log("green")
       event.target.className = "valid"
-      this.formData.valid = true;
+      this.formData= {
+        ...this.formData,
+         valid: true
+      }
     }
 
     this.formData = {
@@ -87,11 +127,29 @@ export class SamComponent {
     return (
       <div>
         <div class="input-group">
-          <form onSubmit={(e) => this.handleSubmit(e)}>
-            <input type="text" id="form-input" value={this.formData.firstName} onInput={(e) => this.handleFirstName(e)} placeholder="First Name" required />
-            <input type="text" id="form-input" value={this.formData.lastName} onInput={(e) => this.handleLastName(e)} placeholder="Last Name" required />
-            <input type="email" id="form-input" value={this.formData.email} onInput={(e) => this.handleEmail(e)} placeholder="Email" required />
-            <input type="submit" class="sqh-continue-btn" value="Continue" />
+          <form id="signup-form" onSubmit={(e) => this.handleSubmit(e)}>
+            <input type="text" id="form-input" 
+              value={this.formData.firstName} 
+              onInput={(e) => this.handleFirstName(e)} 
+              placeholder="First Name" 
+              disabled={this.formData.loading}
+              required 
+            />
+            <input type="text" id="form-input" 
+              value={this.formData.lastName} 
+              onInput={(e) => this.handleLastName(e)} 
+              placeholder="Last Name" 
+              disabled={this.formData.loading}
+              required 
+            />
+            <input type="email" id="form-input" 
+              value={this.formData.email} 
+              onInput={(e) => this.handleEmail(e)} 
+              placeholder="Email" 
+              disabled={this.formData.loading}
+              required 
+            />
+            <input type="submit" class="sqh-continue-btn" value="Continue" disabled={this.formData.loading} />
           </form>
         </div>
       </div>
