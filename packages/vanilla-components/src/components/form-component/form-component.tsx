295bd9e3045fd8e1bec4158ae8311e811608f1ab
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2018-11-09 12:06:26 -0800

    added some padding to success

--- packages/vanilla-components/src/components/form-component/form-component.tsx
+++ packages/vanilla-components/src/components/form-component/form-component.tsx
@@ -188,7 +188,7 @@ export class FormComponent {
                 value={this.loading ? this.loadingtext : this.buttontext} disabled={this.loading} />
             </form>
           </div>,
-        <div class="input-group" style={registered && !completedRegister ? null: hiddenStyle}>
+        <div class="input-group success-header" style={registered && !completedRegister ? null: hiddenStyle}>
           <h3>
             <i class="success icon icon-ok-circled" />
           { this.successtext }
