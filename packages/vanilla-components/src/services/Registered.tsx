2faeed047240fa3469a3e520817e4a716139928d
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2018-11-09 14:34:46 -0800

    cleared default value for widget type

--- packages/vanilla-components/src/services/Registered.tsx
+++ packages/vanilla-components/src/services/Registered.tsx
@@ -11,7 +11,7 @@ export interface State {
 }
 
 export default createProviderConsumer<State>({
-    widgetType: "stats",
+    widgetType: "",
     registered: false,
     completedRegister: false
   },
