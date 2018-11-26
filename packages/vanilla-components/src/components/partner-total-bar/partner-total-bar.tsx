c214152a6eec4453c97aa7f8af5a3467bc0bbc66
Craig Rice <craigricetong@gmail.com>
2018-11-25 20:31:52 -0800

    added vega for graphs in components

--- packages/vanilla-components/src/components/partner-total-bar/partner-total-bar.tsx
+++ packages/vanilla-components/src/components/partner-total-bar/partner-total-bar.tsx
@@ -1,5 +1,14 @@
 import { Component, Prop } from '@stencil/core';
+// import * as vega from "vega-lib"
+import vegaEmbed from "vega-embed";
 
+const spec = "https://raw.githubusercontent.com/vega/vega/master/docs/examples/bar-chart.vg.json";
+
+// const spec = "bar.vg.json";
+
+vegaEmbed('#vis', spec, {
+  defaultStyle: true
+});
 
 @Component({
   tag: 'sqh-partner-total-bar',
@@ -11,6 +20,7 @@ export class PartnerTotal {
   
   render() {
     return !this.ishidden && 
-    <div>hi</div>
+      <div id="vis"></div>
+      // <div id="view"></div>
   }
 }
