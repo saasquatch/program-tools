af49570bdac455f0405e751df00b54324511b4db
Craig Rice <35348171+cbot83@users.noreply.github.com>
2018-12-17 11:49:14 -0800

    ready for sam

--- packages/vanilla-components/src/components/progress-circle/progress-circle.tsx
+++ packages/vanilla-components/src/components/progress-circle/progress-circle.tsx
@@ -1,18 +1,32 @@
-import { Component, Prop } from '@stencil/core';
-import { ProgressBar } from 'progressbar.js';
+import { Component, Prop} from '@stencil/core';
 
 @Component({
   tag: 'sqh-progress-circle',
   styleUrl: 'progress-circle.scss'
 })
 export class ProgressCircle {
+
   @Prop() ishidden: boolean = false;
   @Prop() tiername: string;
 
+
+  constructor() {
+    // this.loading = true;
+  }
+
+  componentWillLoad() {
+    if (!this.ishidden) {
+
+    }
+  }
+
+
   render() {
+
     return !this.ishidden && 
       <div>
         {this.tiername}
+        <div id="container">{this.bar}</div>
       </div>;
   }
 }
