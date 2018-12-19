f6010e6b1b41023f27c111c88cbc90a36f729326
Logan Volkers <logan@saasquat.ch>
2018-12-19 09:59:38 -0800

    LV: Fixed tests and setup continuous integration

--- packages/vanilla-components/src/components/referral-code/referral-code.spec.ts
+++ packages/vanilla-components/src/components/referral-code/referral-code.spec.ts
@@ -1,15 +1,15 @@
 import { render } from '@stencil/core/testing';
-import { TextComponent } from './referral-code';
+import { ReferralCode } from './referral-code';
 
 describe('my-app', () => {
   it('should build', () => {
-    expect(new TextComponent()).toBeTruthy();
+    expect(new ReferralCode()).toBeTruthy();
   });
 
   describe('rendering', () => {
     beforeEach(async () => {
       await render({
-        components: [TextComponent],
+        components: [ReferralCode],
         html: '<sqh-referral-code></sqh-referral-code>'
       });
     });
