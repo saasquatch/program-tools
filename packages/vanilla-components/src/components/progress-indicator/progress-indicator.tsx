e8c3bde1448ebe832727e038344535ce9a5ee8f5
Craig Rice <35348171+cbot83@users.noreply.github.com>
2018-12-19 08:33:55 -0800

    refactored to stencil html input element

--- packages/vanilla-components/src/components/progress-indicator/progress-indicator.tsx
+++ packages/vanilla-components/src/components/progress-indicator/progress-indicator.tsx
@@ -52,9 +52,12 @@ export class ProgressIndicator {
   @Element() el: HTMLElement;
 
 
+  svgContainer!: HTMLInputElement;
+
+
   @Watch('progresstype')
   watchHandler() {
-    const element = this.el.querySelector('#container')
+    const element = this.svgContainer
     while (element.hasChildNodes()) {
       element.removeChild(element.lastChild)
     }
@@ -63,18 +66,13 @@ export class ProgressIndicator {
 
   @Watch('progresswidth')
   watchHandler2() {
-    const element = this.el.querySelector('#container')
+    const element = this.svgContainer
     while (element.hasChildNodes()) {
       element.removeChild(element.lastChild)
     }
     this.getProgress()
   }
 
-  // @Watch('progresswidth')
-  // watchHandler2() {
-  //   console.log('hi')
-  // }
- 
   async componentDidLoad(){
     const { rewardBalanceDetails } = await API.graphql.getBalanceDetails();
     let closestExpiry = {dateExpires: 0, balance: 0};
@@ -91,42 +89,59 @@ export class ProgressIndicator {
     this.getProgress();
   }
 
-  getProgress(){
-    
-    if(this.progresstype === "Circle") {
-      var bar = new ProgressBar[this.progresstype] ('#container', {
-        color: this.percentagecolor,
-        // This has to be the same size as the maximum width to
-        // prevent clipping
-        strokeWidth: 4,
-        trailWidth: 1,
-        easing: 'easeInOut',
-        duration: 1400,
-        text: {
-          style: {
-            // Text color.
-            // Default: same as stroke color (options.color)
-            color: `${ this.textcolor }`,
-            position: 'relative',
-            left: '50%',
-            bottom: `${this.progresswidth / 2}px`,
-            padding: 0,
-            margin: 0,
-            // You can specify styles which will be browser prefixed
-            transform: {
-                prefix: true,
-                value: 'translate(-50%, -50%)'
-            }
-        },
+  getProgress() {
+    // TODO: dynamically change progress
+    let progress = 0.75
 
-        // Only effective if the text.style is not null
-        // By default position: relative is applied to container if text
-        // is set. Setting this to false disables that feature.
-        autoStyleContainer: true,
-        },
-        from: { color: this.progressstartcolor, width: 1 },
-        to: { color: this.progressendcolor, width: 4 },
-        // Set default step function for all animate calls
+    
+
+    let progressBar = new ProgressBar[this.progresstype] (this.svgContainer, {
+      color: this.percentagecolor,
+      strokeWidth: 4,
+      trailWidth: 1,
+      easing: 'easeInOut',
+      duration: 1400,
+      text: {
+        style: {
+          color: `${ this.textcolor }`,
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          fontFamily: '"Raleway", Helvetica, sans-serif',
+          fontSize: `${this.progresswidth / 100}em`,
+          
+          padding: 0,
+          textAlign: 'center',
+          margin: 0,
+          transform: {
+              prefix: true,
+              value: 'translate(-50%, -50%)'
+          }
+      },
+      autoStyleContainer: true,
+      },
+      from: { color: this.progressstartcolor, width: 1 },
+      to: { color: this.progressendcolor, width: 4 },
+      step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+    
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+          circle.setText('');
+        } else {
+          circle.setText(`<img src="https://static.thenounproject.com/png/130115-200.png"><br>` + value + "%");
+        }
+    
+      }
+    });
+
+
+
+    if (this.progresstype === 'Circle') {
+      progressBar.animate(progress, {
+
+        // @ts-ignore
         step: function(state, circle) {
           circle.path.setAttribute('stroke', state.color);
           circle.path.setAttribute('stroke-width', state.width);
@@ -140,26 +155,14 @@ export class ProgressIndicator {
       
         }
       });
-      bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-      bar.text.style.fontSize = `${this.progresswidth / 100}em`;
     }
 
-    if(this.progresstype === "SemiCircle") {
-      var bar = new ProgressBar.SemiCircle('#container', {
-        strokeWidth: 6,
-        color: '#FFEA82',
-        trailColor: '#eee',
-        trailWidth: 1,
-        easing: 'easeInOut',
-        duration: 1400,
-        svgStyle: null,
-        text: {
-          value: '',
-          alignToBottom: false
-        },
-        from: {color: '#FFEA82'},
-        to: {color: '#ED6A5A'},
-        // Set default step function for all animate calls
+    if (this.progresstype === 'SemiCircle') {
+      progressBar.text.style.bottom = '0px'
+
+
+      progressBar.animate(progress, {
+        // @ts-ignore
         step: (state, bar) => {
           bar.path.setAttribute('stroke', state.color);
           var value = Math.round(bar.value() * 100);
@@ -172,44 +175,18 @@ export class ProgressIndicator {
           bar.text.style.color = state.color;
         }
       });
-      bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-      bar.text.style.fontSize = '2rem';
-      bar.text.style.marginBottom = '30px';
     }
 
-    if (this.progresstype === "Line") {
-      var bar = new ProgressBar.Line('#container', {
-        strokeWidth: 4,
-        easing: 'easeInOut',
-        duration: 1400,
-        color: '#FFEA82',
-        trailColor: '#eee',
-        trailWidth: 1,
-        svgStyle: {width: '100%', height: '100%'},
-        text: {
-          style: {
-            // Text color.
-            // Default: same as stroke color (options.color)
-            color: '#999',
-            padding: 0,
-            margin: 0,
-            transform: null
-          },
-          autoStyleContainer: false
-        },
-        from: {color: '#FFEA82'},
-        to: {color: '#ED6A5A'},
+    if (this.progresstype === 'Line') {
+      progressBar.text.style.top = '20px'
+
+      progressBar.animate(progress, {
         // @ts-ignore
         step: (state, bar) => {
           bar.setText(Math.round(bar.value() * 100) + ' %');
         }
       });
     }
-
-    console.log("Bar", bar)
-    console.log("Element", this.el.querySelector('#container'))
-    bar.animate(0.75);  // TODO: graphql call to get progress and goal to find out percentage and input
-    // TODO: Add in for Custom as well https://jsfiddle.net/kimmobrunfeldt/dnLLgm5o/
   }
   
   render() {
@@ -220,30 +197,25 @@ export class ProgressIndicator {
 
   const progressStyle = css`
     width: ${ this.progresswidth }px;
-    height: ${ this.progresswidth }px;
     margin: 30px auto;
     img {
       width: ${ this.progresswidth / 2}px ;
     }
   `
 
-  const balanceStyle = css`
-    padding: 15px 0;
-  `
-
   return !this.ishidden && 
   <div class={wrapperStyle}>
     {this.tiername}
 
-    <div class={progressStyle}>
-      <div id="container"></div>
+    <div class={progressStyle}> 
+      <div ref={(el) => this.svgContainer = el as HTMLInputElement}></div>
     </div>
 
     {/* customer editable / automatically set */}
     <div>{this.unit}</div>
 
     {/* automatically set */}
-    <div class={balanceStyle}>Balance: {this.rewardStats && this.rewardStats.balance}</div>
+    <div>Balance: {this.rewardStats && this.rewardStats.balance}</div>
 
     {/* automatically set */}
     <div>Expires: {this.rewardStats && this.rewardStats.dateExpires}</div>
