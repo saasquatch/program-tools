66faeec74e10f4517d3b8b22bdca0a135156ecf4
Jayden Chan <jaydencn7@gmail.com>
2019-01-25 11:42:30 -0800

    JC: Add webtask login

--- packages/program-dev-cli/src/cmd/login.js
+++ packages/program-dev-cli/src/cmd/login.js
@@ -8,7 +8,8 @@ export const desc = 'Login to Contentful and Webtask';
 const APP_ID = 'DavxURak0VBfgHXGRfCRnsvDe1bziBMuLi72cbYm5b2DqwqxN5VtZApSIDmBFCAF';
 const REDIRECT_URI = 'https://www.contentful.com/developers/cli-oauth-page/';
 // const O_AUTH_URL = `https://be.contentful.com/oauth/authorize?response_type=token&client_id=${APP_ID}&redirect_uri=${REDIRECT_URI}&scope=content_management_manage`;
-const O_AUTH_URL = `https://google.ca`;
+const CONTENTFUL_AUTH_URL = `https://google.ca`;
+const WEBTASK_AUTH_URL = `https://google.ca`;
 
 export async function login () {
   const cmaToken = null;
@@ -21,10 +22,12 @@ export async function login () {
     return cmaToken;
   }
 
-  console.log(`A browser window will open where you will log in (or sign up if you don’t have an account), authorize this CLI tool and paste your CMA token here:`);
+  console.log(`== Contentful authorization ==`);
+  console.log();
+  console.log(`A browser window will open where you will login to Contentful and authorize this CLI tool.`);
   console.log();
 
-  const confirmed = await inquirer.prompt([
+  const contentfulConfirmed = await inquirer.prompt([
     {
       type: 'confirm',
       name: 'ready',
@@ -33,18 +36,18 @@ export async function login () {
     }
   ]);
 
-  if (!confirmed.ready) {
+  if (!contentfulConfirmed.ready) {
     console.log('Log in aborted by the user.');
     return;
   }
 
   // We open the browser window only on Windows and OSX since this might fail or open the wrong browser on Linux.
   if (['win32', 'darwin'].includes(process.platform)) {
-    await opn(O_AUTH_URL, {
+    await opn(CONTENTFUL_AUTH_URL, {
       wait: false
     });
   } else {
-    console.log(`Unable to open your browser automatically. Please open the following URI in your browser:\n\n${O_AUTH_URL}\n\n`);
+    console.log(`Unable to open your browser automatically. Please open the following URI in your browser:\n\n${CONTENTFUL_AUTH_URL}\n\n`);
   }
 
   const tokenAnswer = await inquirer.prompt([
@@ -56,15 +59,50 @@ export async function login () {
     }
   ]);
 
-  // await setContext({
-  //   cmaToken: tokenAnswer.cmaToken
-  // })
-  // await storeRuntimeConfig()
   console.log();
-  console.log(`Great! Your CMA token is now stored on your system.`);
-  console.log(`You can always run program-dev-cli logout to remove it.`);
+  console.log(`Contentful authorization completed`);
+  console.log();
 
-  return tokenAnswer.cmaToken;
+  console.log(`== Webtask authorization ==`);
+  console.log();
+  console.log(`A browser window will open where you will login to Webtask and authorize this CLI tool.`);
+  console.log();
+
+  const webtaskConfirmed = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'ready',
+      message: 'Open browser window now?',
+      default: true
+    }
+  ]);
+
+  if (!webtaskConfirmed.ready) {
+    console.log('Log in aborted by the user.');
+    return;
+  }
+
+  // We open the browser window only on Windows and OSX since this might fail or open the wrong browser on Linux.
+  if (['win32', 'darwin'].includes(process.platform)) {
+    await opn(WEBTASK_AUTH_URL, {
+      wait: false
+    });
+  } else {
+    console.log(`Unable to open your browser automatically. Please open the following URI in your browser:\n\n${WEBTASK_AUTH_URL}\n\n`);
+  }
+
+  const webtaskAnswer = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'webtaskToken',
+      message: 'Paste your token here:',
+      validate: (val) => /^[a-f0-9]{64}$/.test(val.trim())
+    }
+  ]);
+
+  // Store the contentful and webtask tokens here
+
+  return;
 }
 
 login();
