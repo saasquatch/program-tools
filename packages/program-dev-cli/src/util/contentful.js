f0d12001776897cbe09bab45c0589889aeb81a48
Jayden Chan <jaydencn7@gmail.com>
2019-02-01 15:54:22 -0800

    JC: Don't wrap deploy commands in promise

--- packages/program-dev-cli/src/util/contentful.js
+++ packages/program-dev-cli/src/util/contentful.js
@@ -2,7 +2,7 @@ import inquirer from 'inquirer';
 import ora from 'ora';
 import diff from 'deep-diff';
 import { createClient } from 'contentful-management';
-import { readFile } from 'fs';
+import { readFile, readFileSync } from 'fs';
 
 import { hugeWarningConfirm } from '../util/actions';
 
@@ -11,119 +11,111 @@ import { hugeWarningConfirm } from '../util/actions';
  *
  * @param {Object} schema The program schema
  * @param {Object} config The cli config
- *
- * @return {Promise}
  */
 export const uploadSchema = async (schema, config) => {
-  return new Promise(async (resolve, reject) => {
-    const connectionSpinner = ora('Connecting to contentful').start();
-    const client = createClient({
-      accessToken: config.contentfulToken
+  const connectionSpinner = ora('Connecting to contentful').start();
+  const client = createClient({
+    accessToken: config.contentfulToken
+  });
+
+  const env = await client.getSpace(config.space.id)
+    .then(space => {
+      return space.getEnvironment('master');
     });
 
-    const env = await client.getSpace(config.space.id)
-      .then(space => {
-        return space.getEnvironment('master');
-      });
+  connectionSpinner.succeed('Connected');
+  const entryFindSpinner = ora('Finding entry ID...').start();
 
-    connectionSpinner.succeed('Connected');
-    const entryFindSpinner = ora('Finding entry ID...').start();
+  let data;
+  try {
+    data = readFileSync(schema, 'utf8');
+  } catch (err) {
+    const errMessage = `Failed to read schema file: ${err.message}`;
+    entryFindSpinner.fail(errMessage);
+    return;
+  }
 
-    readFile(schema, 'utf8', async (err, data) => {
-      if (err) {
-        const errMessage = `Failed to read schema file: ${err.message}`;
-        entryFindSpinner.fail(errMessage);
-        reject(new Error(errMessage));
-        return;
-      }
+  let newSchema;
+  try {
+    newSchema = JSON.parse(data);
+  } catch (err) {
+    const errMessage = `Failed to parse provided schema file: ${err.message}`;
+    entryFindSpinner.fail(errMessage);
+    return;
+  }
 
-      let newSchema;
-      try {
-        newSchema = JSON.parse(data);
-      } catch (err) {
-        const errMessage = `Failed to parse provided schema file: ${err.message}`;
-        entryFindSpinner.fail(errMessage);
-        reject(new Error(errMessage));
-        return;
-      }
-
-      let entryId = await env.getEntries({ 'content_type': 'programTemplate' })
-        .then(entries => {
-          for (let prop in entries.items) {
-            if (entries.items.hasOwnProperty(prop)) {
-              let tmpSchema = entries.items[prop].fields.schema['en-US'];
-              if (tmpSchema.name === newSchema.name
-                && tmpSchema.summary === newSchema.summary
-                && tmpSchema.longDescription === newSchema.longDescription) {
-                return entries.items[prop].sys.id;
-              }
-            }
+  let entryId = await env.getEntries({ 'content_type': 'programTemplate' })
+    .then(entries => {
+      for (let prop in entries.items) {
+        if (entries.items.hasOwnProperty(prop)) {
+          let tmpSchema = entries.items[prop].fields.schema['en-US'];
+          if (tmpSchema.name === newSchema.name
+            && tmpSchema.summary === newSchema.summary
+            && tmpSchema.longDescription === newSchema.longDescription) {
+            return entries.items[prop].sys.id;
           }
-        })
-        .catch(() => {
-          return null;
-        });
-
-      if (!entryId) {
-        entryFindSpinner.warn('Unable to automatically determine entry ID');
-        const answer = await inquirer.prompt([{
-          type: 'input',
-          name: 'entryId',
-          message: 'Please paste the entry ID:'
-        }]);
-
-        entryId = answer.entryId;
-      } else {
-        entryFindSpinner.succeed('Entry ID found');
-      }
-
-      if (config.space.live === true) {
-        const confirmed = await hugeWarningConfirm(
-          `You are deploying to '${config.space.name}'. Please confirm the entry ID: ${entryId}`,
-          `Yes, this is the correct entry ID.`
-        );
-
-        if (!confirmed) {
-          const answer = await inquirer.prompt([{
-            type: 'input',
-            name: 'entryId',
-            message: 'Please paste the entry ID:'
-          }]);
-
-          entryId = answer.entryId;
         }
       }
-
-      const uploadSpinner = ora('Uploading...').start();
-
-      env.getEntry(entryId)
-        .then(entry => {
-          const changed = diff(entry.fields.schema['en-US'], newSchema);
-          if (!changed) {
-            return new Promise(resolve => resolve(entry));
-          }
-
-          entry.fields.schema['en-US'] = newSchema;
-          return entry.update();
-        })
-        .then(entry => {
-          if (entry.isUpdated()) {
-            uploadSpinner.text = 'Publishing...';
-            entry.publish().then(() => {
-              uploadSpinner.succeed('New version of schema published.');
-              resolve();
-            });
-          } else {
-            uploadSpinner.succeed('Contentful schema was unchanged');
-            resolve();
-          }
-        })
-        .catch(err => {
-          const errMessage = `Failed to upload schema: ${err.message}`;
-          uploadSpinner.fail(errMessage);
-          reject(new Error(errMessage));
-          return;
-        });
+    })
+    .catch(() => {
+      return null;
     });
-  });
+
+  if (!entryId) {
+    entryFindSpinner.warn('Unable to automatically determine entry ID');
+    const answer = await inquirer.prompt([{
+      type: 'input',
+      name: 'entryId',
+      message: 'Please paste the entry ID:'
+    }]);
+
+    entryId = answer.entryId;
+  } else {
+    entryFindSpinner.succeed('Entry ID found');
+  }
+
+  if (config.space.live === true) {
+    const confirmed = await hugeWarningConfirm(
+      `You are deploying to '${config.space.name}'. Please confirm the entry ID: ${entryId}`,
+      `Yes, this is the correct entry ID.`
+    );
+
+    if (!confirmed) {
+      const answer = await inquirer.prompt([{
+        type: 'input',
+        name: 'entryId',
+        message: 'Please paste the entry ID:'
+      }]);
+
+      entryId = answer.entryId;
+    }
+  }
+
+  const uploadSpinner = ora('Uploading...').start();
+
+  await env.getEntry(entryId)
+    .then(entry => {
+      const changed = diff(entry.fields.schema['en-US'], newSchema);
+      if (!changed) {
+        return new Promise(resolve => resolve(entry));
+      }
+
+      entry.fields.schema['en-US'] = newSchema;
+      return entry.update();
+    })
+    .then(entry => {
+      if (entry.isUpdated()) {
+        uploadSpinner.text = 'Publishing...';
+        entry.publish().then(() => {
+          uploadSpinner.succeed('New version of schema published.');
+        });
+      } else {
+        uploadSpinner.succeed('Contentful schema was unchanged');
+      }
+    })
+    .catch(err => {
+      const errMessage = `Failed to upload schema: ${err.message}`;
+      uploadSpinner.fail(errMessage);
+      return;
+    });
 };
--- packages/program-dev-cli/src/util/webtask.js
+++ packages/program-dev-cli/src/util/webtask.js
@@ -1,7 +1,7 @@
 import sandbox from 'sandboxjs';
 import path from 'path';
 import ora from 'ora';
-import { readFile } from 'fs';
+import { readFileSync } from 'fs';
 
 const CONTAINER = 'saasquatch';
 const WEBTASK_URL = 'https://saasquatch.auth0-extend.com';
@@ -11,96 +11,86 @@ const WEBTASK_URL = 'https://saasquatch.auth0-extend.com';
  *
  * @param {String} source Path to source code
  * @param {Object} config cli config
- *
- * @return {Promise}
  */
-export const uploadWebtask = (source, config) => {
-  return new Promise(async (resolve, reject) => {
-    const prefix = config.space.live
-      ? ''
-      : 'staging-';
+export const uploadWebtask = async (source, config) => {
+  const prefix = config.space.live
+    ? ''
+    : 'staging-';
 
-    const webtaskName = `${prefix}${path.basename(source, '.js')}`;
-    const connectionSpinner = ora('Connecting to webtask').start();
+  const webtaskName = `${prefix}${path.basename(source, '.js')}`;
+  const connectionSpinner = ora('Connecting to webtask').start();
 
-    const profile = sandbox.init({
-      url: WEBTASK_URL,
-      container: CONTAINER,
-      token: config.webtaskToken
+  const profile = sandbox.init({
+    url: WEBTASK_URL,
+    container: CONTAINER,
+    token: config.webtaskToken
+  });
+
+  const webtask = await profile.getWebtask({
+    name: webtaskName,
+    fetch_code: true
+  })
+    .catch((err) => {
+      if (err.statusCode === 404) {
+        return undefined;
+      } else {
+        const errMessage = `Failed to connect to webtask: ${err.message}\n\nCode:${err.statusCode}`;
+        connectionSpinner.fail(errMessage);
+        return null;
+      }
     });
 
-    const webtask = await profile.getWebtask({
+  if (webtask === null) {
+    return;
+  }
+
+  connectionSpinner.succeed('Connected');
+  const readSpinner = ora('Reading source code file...').start();
+
+  let code;
+  try {
+    code = readFileSync(source, 'utf8');
+  } catch (err) {
+    const errMessage = `Failed to read source code file`;
+    readSpinner.fail(errMessage);
+    return;
+  }
+
+  if (webtask && webtask.code === code) {
+    readSpinner.succeed('Webtask code was unchanged');
+    return;
+  }
+
+  readSpinner.succeed('Source code loaded');
+  const uploadSpinner = ora();
+
+  if (webtask) {
+    uploadSpinner.start(`Updating existing webtask: '${webtaskName}'`);
+
+    await profile.updateWebtask({
       name: webtaskName,
-      fetch_code: true
+      code: code
     })
-      .catch((err) => {
-        if (err.statusCode === 404) {
-          return undefined;
-        } else {
-          const errMessage = `Failed to connect to webtask: ${err.message}\n\nCode:${err.statusCode}`;
-          connectionSpinner.fail(errMessage);
-          reject(new Error(errMessage));
-          return null;
-        }
+      .then(() => {
+        uploadSpinner.succeed(`Updated webtask '${webtaskName}'`);
+      })
+      .catch(err => {
+        const errMessage = `Error ocurred while updating webtask: ${err.message}`;
+        uploadSpinner.fail(errMessage);
       });
 
-    if (webtask === null) {
-      return;
-    }
+  } else {
+    uploadSpinner.start(`Creating new webtask: '${webtaskName}'`);
 
-    connectionSpinner.succeed('Connected');
-    const readSpinner = ora('Reading source code file...').start();
-
-    readFile(source, 'utf8', async (err, code) => {
-      if (err) {
-        const errMessage = `Failed to read source code file: ${err.message}`;
-        readSpinner.fail(errMessage);
-        reject(new Error(errMessage));
-        return;
-      }
-
-      if (webtask && webtask.code === code) {
-        readSpinner.succeed('Webtask code was unchanged');
-        resolve();
-        return;
-      }
-
-      readSpinner.succeed('Source code loaded');
-      const uploadSpinner = ora();
-
-      if (webtask) {
-        uploadSpinner.start(`Updating existing webtask: '${webtaskName}'`);
-
-        profile.updateWebtask({
-          name: webtaskName,
-          code: code
-        })
-          .then(() => {
-            uploadSpinner.succeed(`Updated webtask '${webtaskName}'`);
-            resolve();
-          })
-          .catch(err => {
-            const errMessage = `Error ocurred while updating webtask: ${err.message}`;
-            uploadSpinner.fail(errMessage);
-            reject(new Error(errMessage));
-          });
-
-      } else {
-        uploadSpinner.start(`Creating new webtask: '${webtaskName}'`);
-
-        profile.create(code, {
-          name: webtaskName
-        })
-          .then(() => {
-            uploadSpinner.succeed(`Created new webtask '${webtaskName}'`);
-            resolve();
-          })
-          .catch(err => {
-            const errMessage = `Error ocurred while creating webtask: ${err.message}`;
-            uploadSpinner.fail(errMessage);
-            reject(new Error(errMessage));
-          });
-      }
-    });
-  });
+    await profile.create(code, {
+      name: webtaskName
+    })
+      .then(() => {
+        uploadSpinner.succeed(`Created new webtask '${webtaskName}'`);
+      })
+      .catch(err => {
+        const errMessage = `Error ocurred while creating webtask: ${err.message}`;
+        uploadSpinner.fail(errMessage);
+      });
+  }
 };
