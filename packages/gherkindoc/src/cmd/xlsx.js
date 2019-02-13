e3fbc9ada8106d9a7d284dd08fb3d35d4876f763
Jayden Chan <jaydencn7@gmail.com>
2019-02-13 11:53:44 -0800

    JC: Example tables in XLSX output

--- packages/gherkindoc/src/cmd/xlsx.js
+++ packages/gherkindoc/src/cmd/xlsx.js
@@ -7,6 +7,8 @@ import { version } from '../../package.json';
 export const command = 'xlsx';
 export const desc = 'Parse the provided file or directory into XLSX';
 
+const NUM_CONTENT_ROWS = 30;
+
 export const handler = async (argv) => {
   argv._.shift();
 
@@ -32,47 +34,114 @@ export const handler = async (argv) => {
 
   let ws = wb.addWorksheet(name);
 
-  const styles = {
-    bold: {
-      name: 'Calibri',
-      bold: true
-    },
-    light: {
-      name: 'Calibri',
-      italic: true,
-      color: {
-        argb: 'FF505050'
-      }
-    }
-  };
-
-  ws.columns = [
-    { header: name, key: 'name', width: name.length},
-    { header: '', key: 'content1', width: 11},
-    { header: '', key: 'content2', width: 11},
-    { header: '', key: 'content3', width: 11}
+  let wscolumns = [
+    { header: name, key: 'name', width: name.length}
   ];
 
-  ws.getRow(1).getCell(1).font = styles.bold;
+  for (let i = 0; i < NUM_CONTENT_ROWS; i++) {
+    wscolumns.push({ header: '', key: `content${i}`, width: 9});
+  }
+
+  ws.columns = wscolumns;
+  ws.getRow(1).font = styles.bold;
 
   json.features[0].feature.featureElements.forEach(scenario => {
-    ws.addRow({content1: scenario.name});
+    ws.addRow({content0: scenario.name});
     ws.lastRow.font = styles.bold;
 
     if (scenario.tags) {
-      ws.addRow({content1: 'Tags: ', content2: scenario.tags.join(', ')});
+      ws.addRow({content0: 'Tags:', content1: scenario.tags.join(', ')});
       ws.lastRow.font = styles.light;
     }
 
     ws.addRow();
 
     scenario.steps.forEach(step => {
-      ws.addRow({content1: step.keyword, content2: step.text});
-      ws.lastRow.getCell(2).font = styles.bold;
+      ws.addRow({content1: step.rawKeyword, content2: step.text});
+      ws.lastRow.getCell(3).font = styles.bold;
+      ws.lastRow.getCell(3).alignment = styles.keywordAlignment;
+    });
+
+    if (scenario.examples.length > 0) {
+      ws.addRow();
+    }
+
+    scenario.examples.forEach(example => {
+      ws.addRow({content0: 'Examples:'});
+      let headerRow = {};
+      let cellsToStyle = [];
+      for (let i = 0; i < example.tableHeader.cells.length; i++) {
+        headerRow[`content${i+2}`] = example.tableHeader.cells[i].value;
+        cellsToStyle.push(i+4);
+      }
+      ws.addRow(headerRow);
+      ws.lastRow.font = styles.boldItalic;
+
+      cellsToStyle.forEach(cellNum => {
+        ws.lastRow.getCell(cellNum).fill = styles.blueFill;
+        ws.lastRow.getCell(cellNum).border = styles.fullBorder;
+      });
+
+      example.tableBody.forEach(row => {
+        let cellsToStyle = [];
+        for (let i = 0; i < row.cells.length; i++) {
+          headerRow[`content${i+2}`] = row.cells[i].value;
+          cellsToStyle.push(i+4);
+        }
+        ws.addRow(headerRow);
+
+        cellsToStyle.forEach(cellNum => {
+          ws.lastRow.getCell(cellNum).border = styles.fullBorder;
+        });
+      });
     });
 
     ws.addRow();
   });
 
   await wb.xlsx.writeFile('output.xlsx');
+};
+
+const styles = {
+  bold: {
+    name: 'Calibri',
+    bold: true
+  },
+  boldItalic: {
+    name: 'Calibri',
+    bold: true,
+    italic: true
+  },
+  light: {
+    name: 'Calibri',
+    italic: true,
+    color: {
+      argb: 'FF404040'
+    }
+  },
+  keywordAlignment: {
+    vertical: 'bottom',
+    horizontal: 'right'
+  },
+  blueFill: {
+    type: 'pattern',
+    pattern: 'solid',
+    fgColor: {
+      argb: "FFF0F8FF"
+    }
+  },
+  fullBorder: {
+    top: {
+      style:'thin'
+    },
+    left: {
+      style:'thin'
+    },
+    bottom: {
+      style:'thin'
+    },
+    right: {
+      style:'thin'
+    }
+  }
 };
