0d9bf80f6e4fd17ae1230692d5d83087be3c28dc
Jayden Chan <jaydencn7@gmail.com>
2019-02-13 12:09:57 -0800

    JC: Cleanup

--- packages/gherkindoc/src/cmd/xlsx.js
+++ packages/gherkindoc/src/cmd/xlsx.js
@@ -8,100 +8,6 @@ export const command = 'xlsx';
 export const desc = 'Parse the provided file or directory into XLSX';
 
 const NUM_CONTENT_ROWS = 30;
-
-export const handler = async (argv) => {
-  argv._.shift();
-
-  const args = argv._;
-
-  if (args.length !== 1) {
-    console.log('Wrong number of arguments.');
-    console.log('Pass a .feature file or directory.');
-    return;
-  }
-
-  const files = isDir(args[0]) ? gherkins(args[0]) : [args[0]];
-
-  const json = await generateJson(files);
-
-  let wb = new Excel.Workbook();
-
-  wb.creator = `${json.configuration.program} v${version}`;
-  wb.created = new Date(json.configuration.generatedOnTimestamp);
-  wb.modified = new Date(json.configuration.generatedOnTimestamp);
-
-  const name = json.features[0].feature.name;
-
-  let ws = wb.addWorksheet(name);
-
-  let wscolumns = [
-    { header: name, key: 'name', width: name.length}
-  ];
-
-  for (let i = 0; i < NUM_CONTENT_ROWS; i++) {
-    wscolumns.push({ header: '', key: `content${i}`, width: 9});
-  }
-
-  ws.columns = wscolumns;
-  ws.getRow(1).font = styles.bold;
-
-  json.features[0].feature.featureElements.forEach(scenario => {
-    ws.addRow({content0: scenario.name});
-    ws.lastRow.font = styles.bold;
-
-    if (scenario.tags) {
-      ws.addRow({content0: 'Tags:', content1: scenario.tags.join(', ')});
-      ws.lastRow.font = styles.light;
-    }
-
-    ws.addRow();
-
-    scenario.steps.forEach(step => {
-      ws.addRow({content1: step.rawKeyword, content2: step.text});
-      ws.lastRow.getCell(3).font = styles.bold;
-      ws.lastRow.getCell(3).alignment = styles.keywordAlignment;
-    });
-
-    if (scenario.examples.length > 0) {
-      ws.addRow();
-    }
-
-    scenario.examples.forEach(example => {
-      ws.addRow({content0: 'Examples:'});
-      let headerRow = {};
-      let cellsToStyle = [];
-      for (let i = 0; i < example.tableHeader.cells.length; i++) {
-        headerRow[`content${i+2}`] = example.tableHeader.cells[i].value;
-        cellsToStyle.push(i+4);
-      }
-      ws.addRow(headerRow);
-      ws.lastRow.font = styles.boldItalic;
-
-      cellsToStyle.forEach(cellNum => {
-        ws.lastRow.getCell(cellNum).fill = styles.blueFill;
-        ws.lastRow.getCell(cellNum).border = styles.fullBorder;
-      });
-
-      example.tableBody.forEach(row => {
-        let cellsToStyle = [];
-        for (let i = 0; i < row.cells.length; i++) {
-          headerRow[`content${i+2}`] = row.cells[i].value;
-          cellsToStyle.push(i+4);
-        }
-        ws.addRow(headerRow);
-
-        cellsToStyle.forEach(cellNum => {
-          ws.lastRow.getCell(cellNum).border = styles.fullBorder;
-        });
-      });
-    });
-
-    ws.addRow();
-  });
-
-  await wb.xlsx.writeFile('output.xlsx');
-};
-
 const styles = {
   bold: {
     name: 'Calibri',
@@ -127,7 +33,7 @@ const styles = {
     type: 'pattern',
     pattern: 'solid',
     fgColor: {
-      argb: "FFF0F8FF"
+      argb: 'FFF0F8FF'
     }
   },
   fullBorder: {
@@ -144,4 +50,105 @@ const styles = {
       style:'thin'
     }
   }
+};
+
+export const handler = async (argv) => {
+  argv._.shift();
+
+  const args = argv._;
+
+  if (args.length !== 1) {
+    console.log('Wrong number of arguments.');
+    console.log('Pass a .feature file or directory.');
+    return;
+  }
+
+  const files = isDir(args[0]) ? gherkins(args[0]) : [args[0]];
+  const json = await generateJson(files);
+
+  const wb = new Excel.Workbook();
+
+  wb.creator = `${json.configuration.program} v${version}`;
+  wb.created = new Date(json.configuration.generatedOnTimestamp);
+  wb.modified = new Date(json.configuration.generatedOnTimestamp);
+
+  const name = json.features[0].feature.name;
+  const ws = wb.addWorksheet(name);
+
+  let wscolumns = [
+    {header: name, key: 'name', width: 9}
+  ];
+
+  for (let i = 0; i < NUM_CONTENT_ROWS; i++) {
+    wscolumns.push({header: '', key: `content${i}`, width: 9});
+  }
+
+  ws.columns = wscolumns;
+  ws.getRow(1).font = styles.bold;
+  ws.addRow();
+
+  json.features[0].feature.featureElements.forEach(scenario => {
+    ws.addRow({content0: scenario.name});
+    ws.lastRow.font = styles.bold;
+
+    if (scenario.tags) {
+      ws.addRow({content0: 'Tags:', content1: scenario.tags.join(', ')});
+      ws.lastRow.font = styles.light;
+    }
+
+    ws.addRow();
+
+    scenario.steps.forEach(step => {
+      ws.addRow({content1: step.rawKeyword, content2: step.text});
+      ws.lastRow.getCell(3).font = styles.bold;
+      ws.lastRow.getCell(3).alignment = styles.keywordAlignment;
+    });
+
+    if (scenario.examples.length > 0) {
+      ws.addRow();
+    }
+
+    scenario.examples.forEach(example => {
+      genExampleTable(ws, example);
+    });
+
+    ws.addRow();
+  });
+
+  await wb.xlsx.writeFile('output.xlsx');
+};
+
+const genExampleTable = (ws, example) => {
+  ws.addRow({content0: 'Examples:'});
+  let headerRow = {};
+  let cellsToStyle = [];
+
+  for (let i = 0; i < example.tableHeader.cells.length; i++) {
+    headerRow[`content${i+2}`] = example.tableHeader.cells[i].value;
+    cellsToStyle.push(i+4);
+  }
+
+  ws.addRow(headerRow);
+  ws.lastRow.font = styles.boldItalic;
+
+  cellsToStyle.forEach(cellNum => {
+    ws.lastRow.getCell(cellNum).fill = styles.blueFill;
+    ws.lastRow.getCell(cellNum).border = styles.fullBorder;
+  });
+
+  example.tableBody.forEach(row => {
+    let cellsToStyle = [];
+    let contentRow = {};
+
+    for (let i = 0; i < row.cells.length; i++) {
+      contentRow[`content${i+2}`] = row.cells[i].value;
+      cellsToStyle.push(i+4);
+    }
+
+    ws.addRow(contentRow);
+
+    cellsToStyle.forEach(cellNum => {
+      ws.lastRow.getCell(cellNum).border = styles.fullBorder;
+    });
+  });
 };
