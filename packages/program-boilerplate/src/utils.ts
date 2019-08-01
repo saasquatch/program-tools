568bddc1a6e09878863897618573bde34c6ba4a8
Jayden Chan <jaydencn7@gmail.com>
2019-08-01 14:59:53 -0700

    JC: Add type inference function

--- packages/program-boilerplate/src/utils.ts
+++ packages/program-boilerplate/src/utils.ts
@@ -64,3 +64,45 @@ export function getGoalAnalyticTimestamp(trigger: any): number {
 
   return purchaseEvent ? purchaseEvent.dateTriggered - 1 : trigger.time;
 }
+
+/**
+ * Translates a string type into its proper JavaScript type
+ *
+ * @example
+ *
+ * "29" => 29
+ * "3.1415" => 3.1415
+ * "true" => true
+ * "asdf" => "asdf"
+ *
+ * @param {string} val The value to test
+ * @return {any} the computed value
+ */
+export function inferType(val: string): any {
+  if (/^(-|\+)?([0-9]+(\.[0-9]+)?)$/.test(val)) {
+    const asNum = Number(val);
+
+    if (!isNaN(asNum)) {
+      return asNum;
+    }
+  }
+
+  switch (val) {
+    case 'undefined':
+      return undefined;
+    case 'null':
+      return null;
+    case 'NaN':
+      return NaN;
+    case 'Infinity':
+      return Infinity;
+    case '-Infinity':
+      return -Infinity;
+    case 'true':
+      return true;
+    case 'false':
+      return false;
+    default:
+      return val;
+  }
+}
