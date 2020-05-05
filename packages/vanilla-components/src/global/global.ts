7ecaecbaacc256116a3ccb573a6a650571c78ff0
Johan Venter <johan.venter@referralsaasquatch.com>
2020-05-04 17:29:37 -0700

    Provide a polyfill for Intl.PluralRules

--- packages/vanilla-components/src/global/global.ts
+++ packages/vanilla-components/src/global/global.ts
@@ -1,2 +1,5 @@
 import "@formatjs/intl-relativetimeformat/polyfill";
 import "@formatjs/intl-relativetimeformat/polyfill-locales";
+
+import "@formatjs/intl-pluralrules/polyfill";
+import "@formatjs/intl-pluralrules/polyfill-locales";
