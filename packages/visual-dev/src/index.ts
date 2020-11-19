e961ae9e459df6ba7aee67ad08828de4b1002ca4
Noah Clarke <noah.clarke@referralsaasquatch.com>
2020-11-19 14:45:21 -0800

    Proper exports

--- packages/visual-dev/src/index.ts
+++ packages/visual-dev/src/index.ts
@@ -1,3 +1,8 @@
 import MyComponent from './components/MyComponent'
+import {H1, H2, H3, P} from './components/Typography'
+import { WidgetContainer } from './components/Widgets'
+import {TextLink as TextButton} from './components/TextButton'
+import {Link} from './components/Links'
+import Select from './components/Select'
 
-export { MyComponent }
+export { MyComponent, H1, H2, H3, P, WidgetContainer, TextButton, Link, Select}
