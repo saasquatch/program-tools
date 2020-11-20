4e286b99b0b925e8626b684c7bc06bc969469ada
Noah Clarke <noah.clarke@referralsaasquatch.com>
2020-11-20 11:01:26 -0800

    react-select style fix

--- packages/visual-dev/src/components/Select.tsx
+++ packages/visual-dev/src/components/Select.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import ArrowRenderer from "./ArrowRenderer";
 import DefaultSelect from "react-select";
 import { SelectComponentsProps } from "react-select/src/Select";
+import 'react-select/dist/react-select.css';
 
 
 const Select = (props: SelectComponentsProps) => (
