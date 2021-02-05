27cb0df29b0a98e3dd05cee126a357e4c3a8e30a
Johan Venter <johan@saasquatch.com>
2021-02-05 10:41:53 -0800

    JV: Add the inferType additions for objects/arrays from PR82

--- packages/program-boilerplate/src/utils.ts
+++ packages/program-boilerplate/src/utils.ts
@@ -1,7 +1,7 @@
-import {rewardScheduleQuery} from './queries';
-import {ProgramTriggerBody, TriggerType} from './types/rpc';
-import {User} from './types/saasquatch';
-import {loggers} from 'winston';
+import { rewardScheduleQuery } from "./queries";
+import { ProgramTriggerBody, TriggerType } from "./types/rpc";
+import { User } from "./types/saasquatch";
+import { loggers } from "winston";
 
 /**
  * Append a reward schedule to the template and return the new template
@@ -13,41 +13,41 @@ import {loggers} from 'winston';
  * @param {number} periodInHours     The refresh period
  */
 export function setRewardSchedule({
-  template,
-  expiryWarningDays,
-  key,
-  emailKey,
-  periodInHours,
+	template,
+	expiryWarningDays,
+	key,
+	emailKey,
+	periodInHours,
 }: {
-  template: any;
-  expiryWarningDays: number;
-  key: string;
-  emailKey: string;
-  periodInHours: number;
+	template: any;
+	expiryWarningDays: number;
+	key: string;
+	emailKey: string;
+	periodInHours: number;
 }): any {
-  if (expiryWarningDays) {
-    const dateExpires_timeframe = `next_${expiryWarningDays}_days`;
-    const rewardSchedule = {
-      key,
-      type: 'REWARD',
-      filter: {
-        dateExpires_timeframe,
-      },
-      query: rewardScheduleQuery(emailKey),
-      periodInHours,
-    };
-    const currentSchedules = template.schedules;
-    const schedules = currentSchedules
-      ? [...currentSchedules, rewardSchedule]
-      : [rewardSchedule];
-    const newTemplate = {
-      ...template,
-      schedules,
-    };
-    return newTemplate;
-  } else {
-    return template;
-  }
+	if (expiryWarningDays) {
+		const dateExpires_timeframe = `next_${expiryWarningDays}_days`;
+		const rewardSchedule = {
+			key,
+			type: "REWARD",
+			filter: {
+				dateExpires_timeframe,
+			},
+			query: rewardScheduleQuery(emailKey),
+			periodInHours,
+		};
+		const currentSchedules = template.schedules;
+		const schedules = currentSchedules
+			? [...currentSchedules, rewardSchedule]
+			: [rewardSchedule];
+		const newTemplate = {
+			...template,
+			schedules,
+		};
+		return newTemplate;
+	} else {
+		return template;
+	}
 }
 
 /**
@@ -61,11 +61,11 @@ export function setRewardSchedule({
  * @return {number} The timestamp
  */
 export function getGoalAnalyticTimestamp(trigger: any): number {
-  const purchaseEvent = trigger.events
-    ? trigger.events.find((e: any) => e.key === 'purchase')
-    : undefined;
+	const purchaseEvent = trigger.events
+		? trigger.events.find((e: any) => e.key === "purchase")
+		: undefined;
 
-  return purchaseEvent ? purchaseEvent.dateTriggered - 1 : trigger.time;
+	return purchaseEvent ? purchaseEvent.dateTriggered - 1 : trigger.time;
 }
 
 /**
@@ -85,37 +85,53 @@ export function getGoalAnalyticTimestamp(trigger: any): number {
  * NaN => NaN (number)
  * @example
  * undefined => undefined
+ * @example
+ * "[true, false]" => [true, false] (object)
+ * @example
+ * "{"key1":true,"key2":false}" => {"key1":true,"key2":false} (object)
+ * @example
  *
  * @param {string} val The value to test
  * @return {any} the computed value
  */
 export function inferType(val: string): any {
-  if (/^(-|\+)?([0-9]+(\.[0-9]+)?)$/.test(val)) {
-    const asNum = Number(val);
+	if (/^(-|\+)?([0-9]+(\.[0-9]+)?)$/.test(val)) {
+		const asNum = Number(val);
 
-    if (!isNaN(asNum)) {
-      return asNum;
-    }
-  }
+		if (!isNaN(asNum)) {
+			return asNum;
+		}
+	}
 
-  switch (val) {
-    case 'undefined':
-      return undefined;
-    case 'null':
-      return null;
-    case 'NaN':
-      return NaN;
-    case 'Infinity':
-      return Infinity;
-    case '-Infinity':
-      return -Infinity;
-    case 'true':
-      return true;
-    case 'false':
-      return false;
-    default:
-      return val;
-  }
+	if (/(^[\[].*[\]]$)|(^[\{].*[\}]$)/.test(val)) {
+		try {
+			const asObject = JSON.parse(val);
+			if (asObject instanceof Object) {
+				return asObject;
+			}
+		} catch (e) {
+			return undefined;
+		}
+	}
+
+	switch (val) {
+		case "undefined":
+			return undefined;
+		case "null":
+			return null;
+		case "NaN":
+			return NaN;
+		case "Infinity":
+			return Infinity;
+		case "-Infinity":
+			return -Infinity;
+		case "true":
+			return true;
+		case "false":
+			return false;
+		default:
+			return val;
+	}
 }
 
 /**
@@ -125,16 +141,16 @@ export function inferType(val: string): any {
  * @return {string} the string representation of the conversion operator
  */
 export function numToEquality(num: number): string {
-  switch (num) {
-    case 0:
-      return 'eq';
-    case 1:
-      return 'gte';
-    case 2:
-      return 'lte';
-    default:
-      return 'eq';
-  }
+	switch (num) {
+		case 0:
+			return "eq";
+		case 1:
+			return "gte";
+		case 2:
+			return "lte";
+		default:
+			return "eq";
+	}
 }
 
 /**
@@ -143,55 +159,55 @@ export function numToEquality(num: number): string {
  * @return object[] The tranformed data that is relavent for the trigger type
  */
 export function getTriggerSchema(body: ProgramTriggerBody): object[] {
-  const activeTrigger = body.activeTrigger;
-  const triggerType = activeTrigger.type as TriggerType;
-  const standardData = {
-    type: activeTrigger.type,
-    time: activeTrigger.time,
-    user: activeTrigger.user,
-  };
-  switch (triggerType) {
-    case 'AFTER_USER_CREATED_OR_UPDATED':
-      return [
-        {
-          ...standardData,
-          previous: activeTrigger.previous,
-        },
-      ];
-    case 'REFERRAL':
-      return [
-        {
-          ...standardData,
-          referral: activeTrigger.referral,
-        },
-      ];
-    case 'AFTER_USER_EVENT_PROCESSED':
-      let contexts: object[] = [];
-      activeTrigger.events.forEach((event: any) => {
-        contexts.push({
-          ...standardData,
-          event: {
-            key: event.key,
-            id: event.id,
-            dateTriggered: event.dateTriggered,
-            fields: event.fields,
-          },
-        });
-      });
-      return contexts;
-    case 'SCHEDULED':
-      return [
-        {
-          ...standardData,
-        },
-      ];
-    case 'REWARD_SCHEDULED':
-      return [
-        {
-          ...standardData,
-        },
-      ];
-    default:
-      throw new Error('Trigger type did not match expected options');
-  }
+	const activeTrigger = body.activeTrigger;
+	const triggerType = activeTrigger.type as TriggerType;
+	const standardData = {
+		type: activeTrigger.type,
+		time: activeTrigger.time,
+		user: activeTrigger.user,
+	};
+	switch (triggerType) {
+		case "AFTER_USER_CREATED_OR_UPDATED":
+			return [
+				{
+					...standardData,
+					previous: activeTrigger.previous,
+				},
+			];
+		case "REFERRAL":
+			return [
+				{
+					...standardData,
+					referral: activeTrigger.referral,
+				},
+			];
+		case "AFTER_USER_EVENT_PROCESSED":
+			let contexts: object[] = [];
+			activeTrigger.events.forEach((event: any) => {
+				contexts.push({
+					...standardData,
+					event: {
+						key: event.key,
+						id: event.id,
+						dateTriggered: event.dateTriggered,
+						fields: event.fields,
+					},
+				});
+			});
+			return contexts;
+		case "SCHEDULED":
+			return [
+				{
+					...standardData,
+				},
+			];
+		case "REWARD_SCHEDULED":
+			return [
+				{
+					...standardData,
+				},
+			];
+		default:
+			throw new Error("Trigger type did not match expected options");
+	}
 }
