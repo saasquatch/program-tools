8a3ba1d01c7baa0195999fa6de7cead849013480
Johan Venter <johan@saasquatch.com>
2021-02-22 15:26:50 -0800

    JV: Typo

--- packages/program-test-suite/src/steps/user.ts
+++ packages/program-test-suite/src/steps/user.ts
@@ -61,7 +61,7 @@ const userSteps: StepDefinitions = ({ given }) => {
     }
   );
 
-  given("the user has the following reward:", (data: any) => {
+  given("the (?:referred )?user has the following reward:", (data: any) => {
     const rewards = getWorld().state.current.user.rewards || {
       totalCount: 0,
       data: [],
@@ -78,7 +78,7 @@ const userSteps: StepDefinitions = ({ given }) => {
     });
   });
 
-  given("the referred user has the following reward:", (data: any) => {
+  given("the referrer user has the following reward:", (data: any) => {
     const rewards = getWorld().state.current.user.referredByReferral
       .referrerUser.rewards || {
       totalCount: 0,
