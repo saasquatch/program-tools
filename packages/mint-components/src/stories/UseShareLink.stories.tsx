87adb13ab1e606df00ebff3d2fc31aefd8848546
Malcolm Seyd <malcolm.seyd@referralsaasquatch.com>
2021-03-18 14:43:35 -0700

    better stories for share-link

--- packages/mint-components/src/stories/UseShareLink.stories.tsx
+++ packages/mint-components/src/stories/UseShareLink.stories.tsx
@@ -1,24 +1,24 @@
-import { h } from '@stencil/core';
-import { ShareLinkView } from '../components/share-link/share-link-view';
-import { useShareLink } from '../components/share-link/useShareLink';
+import { h } from "@stencil/core";
+import { ShareLinkView } from "../components/share-link/share-link-view";
+import { useShareLink } from "../components/share-link/useShareLink";
 
 export default {
-  title: 'Hooks / useShareLink',
+  title: "Hooks / useShareLink",
 };
 
 function setupGraphQL() {
-  const id = 'worried-camera@uexwltgh.mailosaur.net';
+  const id = "worried-camera@uexwltgh.mailosaur.net";
   const accountId = id;
 
   //@ts-ignore
   window.SquatchAndroid = true;
   //@ts-ignore
   window.widgetIdent = {
-    tenantAlias: 'test_as36zjtpfy7oo',
-    appDomain: 'https://staging.referralsaasquatch.com',
+    tenantAlias: "test_as36zjtpfy7oo",
+    appDomain: "https://staging.referralsaasquatch.com",
     token:
       // you have to change this if you change the id or accountId
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImFjY291bnRJZCI6IndvcnJpZWQtY2FtZXJhQHVleHdsdGdoLm1haWxvc2F1ci5uZXQiLCJpZCI6IndvcnJpZWQtY2FtZXJhQHVleHdsdGdoLm1haWxvc2F1ci5uZXQifX0.-WGV4_bzGCFp-OTIO-h-yp0MlgtkdufT_GgI4T691OY',
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImFjY291bnRJZCI6IndvcnJpZWQtY2FtZXJhQHVleHdsdGdoLm1haWxvc2F1ci5uZXQiLCJpZCI6IndvcnJpZWQtY2FtZXJhQHVleHdsdGdoLm1haWxvc2F1ci5uZXQifX0.-WGV4_bzGCFp-OTIO-h-yp0MlgtkdufT_GgI4T691OY",
     userId: id,
     accountId,
   };
@@ -27,15 +27,41 @@ function setupGraphQL() {
 
 export const BareBonesView = () => {
   setupGraphQL();
-  const res = useShareLink({ programId: 'a-referral-program' });
+  const res = useShareLink({ programId: "a-referral-program" });
   return (
     <div>
-      Sharelink: <code style={{ borderStyle: 'solid', borderWidth: '1px', padding: '2px' }}>{res.sharelink}</code>
+      Sharelink:{" "}
+      <code
+        style={{ borderStyle: "solid", borderWidth: "1px", padding: "2px" }}
+      >
+        {res.sharelink}
+      </code>
     </div>
   );
 };
 
 export const RegularView = () => {
   setupGraphQL();
-  return <ShareLinkView {...useShareLink({ programId: 'a-referral-program', tooltiptext: 'Copied to clipboard' })} />;
+  return (
+    <ShareLinkView
+      {...useShareLink({
+        programId: "a-referral-program",
+        tooltiptext: "Copied to clipboard",
+      })}
+    />
+  );
+};
+
+export const FastTooltip = () => {
+  setupGraphQL();
+  return (
+    <ShareLinkView
+      {...useShareLink({
+        programId: "a-referral-program",
+        tooltiptext:
+          "⠀⠀⠀⠀⠀⠀⠀⠀⠀HELLO THERE!!!\n⠀⠀⠀⡯⡯⡾⠝⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢊⠘⡮⣣⠪⠢⡑⡌\n⠀⠀⠀⠟⠝⠈⠀⠀⠀⠡⠀⠠⢈⠠⢐⢠⢂⢔⣐⢄⡂⢔⠀⡁⢉⠸⢨⢑⠕⡌\n⠀⠀⡀⠁⠀⠀⠀⡀⢂⠡⠈⡔⣕⢮⣳⢯⣿⣻⣟⣯⣯⢷⣫⣆⡂⠀⠀⢐⠑⡌\n⢀⠠⠐⠈⠀⢀⢂⠢⡂⠕⡁⣝⢮⣳⢽⡽⣾⣻⣿⣯⡯⣟⣞⢾⢜⢆⠀⡀⠀⠪\n⣬⠂⠀⠀⢀⢂⢪⠨⢂⠥⣺⡪⣗⢗⣽⢽⡯⣿⣽⣷⢿⡽⡾⡽⣝⢎⠀⠀⠀⢡\n⣿⠀⠀⠀⢂⠢⢂⢥⢱⡹⣪⢞⡵⣻⡪⡯⡯⣟⡾⣿⣻⡽⣯⡻⣪⠧⠑⠀⠁⢐\n⣿⠀⠀⠀⠢⢑⠠⠑⠕⡝⡎⡗⡝⡎⣞⢽⡹⣕⢯⢻⠹⡹⢚⠝⡷⡽⡨⠀⠀⢔\n⣿⡯⠀⢈⠈⢄⠂⠂⠐⠀⠌⠠⢑⠱⡱⡱⡑⢔⠁⠀⡀⠐⠐⠐⡡⡹⣪⠀⠀⢘\n⣿⣽⠀⡀⡊⠀⠐⠨⠈⡁⠂⢈⠠⡱⡽⣷⡑⠁⠠⠑⠀⢉⢇⣤⢘⣪⢽⠀⢌⢎\n⣿⢾⠀⢌⠌⠀⡁⠢⠂⠐⡀⠀⢀⢳⢽⣽⡺⣨⢄⣑⢉⢃⢭⡲⣕⡭⣹⠠⢐⢗\n⣿⡗⠀⠢⠡⡱⡸⣔⢵⢱⢸⠈⠀⡪⣳⣳⢹⢜⡵⣱⢱⡱⣳⡹⣵⣻⢔⢅⢬⡷\n⣷⡇⡂⠡⡑⢕⢕⠕⡑⠡⢂⢊⢐⢕⡝⡮⡧⡳⣝⢴⡐⣁⠃⡫⡒⣕⢏⡮⣷⡟\n⣷⣻⣅⠑⢌⠢⠁⢐⠠⠑⡐⠐⠌⡪⠮⡫⠪⡪⡪⣺⢸⠰⠡⠠⠐⢱⠨⡪⡪⡰\n⣯⢷⣟⣇⡂⡂⡌⡀⠀⠁⡂⠅⠂⠀⡑⡄⢇⠇⢝⡨⡠⡁⢐⠠⢀⢪⡐⡜⡪⡊\n⣿⢽⡾⢹⡄⠕⡅⢇⠂⠑⣴⡬⣬⣬⣆⢮⣦⣷⣵⣷⡗⢃⢮⠱⡸⢰⢱⢸⢨⢌\n⣯⢯⣟⠸⣳⡅⠜⠔⡌⡐⠈⠻⠟⣿⢿⣿⣿⠿⡻⣃⠢⣱⡳⡱⡩⢢⠣⡃⠢⠁\n⡯⣟⣞⡇⡿⣽⡪⡘⡰⠨⢐⢀⠢⢢⢄⢤⣰⠼⡾⢕⢕⡵⣝⠎⢌⢪⠪⡘⡌⠀\n⡯⣳⠯⠚⢊⠡⡂⢂⠨⠊⠔⡑⠬⡸⣘⢬⢪⣪⡺⡼⣕⢯⢞⢕⢝⠎⢻⢼⣀⠀\n⠁⡂⠔⡁⡢⠣⢀⠢⠀⠅⠱⡐⡱⡘⡔⡕⡕⣲⡹⣎⡮⡏⡑⢜⢼⡱⢩⣗⣯⣟\n⢀⢂⢑⠀⡂⡃⠅⠊⢄⢑⠠⠑⢕⢕⢝⢮⢺⢕⢟⢮⢊⢢⢱⢄⠃⣇⣞⢞⣞⢾\n⢀⠢⡑⡀⢂⢊⠠⠁⡂⡐⠀⠅⡈⠪⠪⠪⠣⠫⠑⡁⢔⠕⣜⣜⢦⡰⡎⡯⡾⡽",
+        tooltiplifespan: 500,
+      })}
+    />
+  );
 };
