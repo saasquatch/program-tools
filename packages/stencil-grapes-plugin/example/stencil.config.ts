1f3f096f13bcf4ecb2da1ddc86fa1a99a61fe10c
Logan Volkers <logan@saasquat.ch>
2021-03-19 09:38:55 -0700

    LV: Migrate GrapesJS plugin to external package

---
+++ packages/stencil-grapes-plugin/example/stencil.config.ts
@@ -0,0 +1,25 @@
+import { Config } from "@stencil/core";
+import { grapesJsOutput } from "../";
+export const config: Config = {
+  namespace: "grapes-test",
+  outputTargets: [
+    {
+      type: "dist",
+    },
+    {
+      type: "dist-custom-elements-bundle",
+    },
+    {
+      type: "docs-readme",
+    },
+    {
+      type: "docs-json",
+      file: "docs/docs.json",
+    },
+    grapesJsOutput({}),
+    {
+      type: "www",
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
