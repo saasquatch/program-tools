d7ecc5bc3bda8a230d3a83cff1806ef6d598f256
Noah Clarke <noah.clarke@referralsaasquatch.com>
2021-03-31 16:44:16 -0700

    portal frame and edit profile stories

---
+++ packages/mint-components/src/stories/EditProfileForm.stories.tsx
@@ -0,0 +1,69 @@
+import { h } from "@stencil/core";
+import EditProfileView from "../components/edit-profile/edit-profile-view";
+import { LeaderboardRankView } from "../components/leaderboard-rank/leaderboard-rank-view";
+
+export default {
+  title: "Edit Profile Form",
+};
+
+const defaultProps = {
+  states: {
+    loading: false,
+    submitDisabled: false,
+    formState: {
+      currentRegion: "Canada",
+      firstName: "Bill",
+      lastName: "Bob",
+      errors: {},
+      error: "An error string",
+    },
+    user: {
+      firstName: "Bill",
+      lastName: "Bob",
+      email: "billbob@example.com",
+    },
+    showEdit: false,
+    text: {
+      editprofileheader: "Edit Profile",
+      editprofiletext: "Update your profile.",
+      firstnametext: "Bill",
+      lastnametext: "Bob",
+      canceltext: "Cancel",
+      updatetext: "Update",
+      currentregiontext: "Canada",
+    },
+  },
+  callbacks: {
+    onSubmit: (props) => {
+      console.log(props);
+    },
+    resetForm: () => {
+      console.log("reset");
+    },
+    onChange: () => {
+      console.log("change");
+    },
+    setShowEdit: (props) => {
+      console.log(props);
+    },
+  },
+};
+
+export const FrameWithMenu = () => {
+  const props = defaultProps;
+  return <EditProfileView {...props} />;
+};
+
+export const FullStackFrameWithMenu = () => {
+  return (
+    <sqm-edit-profile
+      editprofileheader="Edit Profile"
+      editprofiletext="Update your profile."
+      firstnametext="Bill"
+      lastnametext="Bob"
+      canceltext="Cancel"
+      updatetext="Update"
+      currentregiontext="Canada"
+    ></sqm-edit-profile>
+  );
+};
