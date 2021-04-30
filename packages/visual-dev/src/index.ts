9c56da7e328eff05d237740c1ff602c80d73ef8b
Johan Venter <johan@saasquatch.com>
2021-04-29 18:15:57 -0700

    JV: Add a default object template

--- packages/visual-dev/src/index.ts
+++ packages/visual-dev/src/index.ts
@@ -1,15 +1,50 @@
 import MyComponent from "./components/MyComponent";
-import { H1, H2, H3, P, WidgetTitle, AttributeHeading, ErrorBlock } from "./components/Typography";
-import { ColumnContainer, RJSFContainer, RowContainer, StyledHR, WidgetContainer } from "./components/Layouts";
+import {
+  H1,
+  H2,
+  H3,
+  P,
+  WidgetTitle,
+  AttributeHeading,
+  ErrorBlock,
+} from "./components/Typography";
+import {
+  ColumnContainer,
+  RJSFContainer,
+  RowContainer,
+  StyledHR,
+  WidgetContainer,
+} from "./components/Layouts";
 import { TextLink as TextButton } from "./components/TextButton";
 import { Link } from "./components/Links";
-import {Select, CreatableSelect, SelectNarrow, CreatableSelectNarrow} from "./components/Select";
-import LoadingSpinner, { LoadingSpinnerLarge } from "./components/LoadingSpinner";
+import {
+  Select,
+  CreatableSelect,
+  SelectNarrow,
+  CreatableSelectNarrow,
+} from "./components/Select";
+import LoadingSpinner, {
+  LoadingSpinnerLarge,
+} from "./components/LoadingSpinner";
 import RadioCards from "./components/RadioCards";
-import {GenericInput, StyledSelect, PencilButton, EditPencil} from "./components/Inputs"
-import JSONataEditor from "./components/MapFieldsJsonata"
-import { PlatformSignInButton, PrimaryButton, PrimaryButtonNew, SecondaryButton } from "./components/Buttons";
-import { ActionsArrayTemplate, DefaultTemplate } from "./components/FormTemplates";
+import {
+  GenericInput,
+  StyledSelect,
+  PencilButton,
+  EditPencil,
+} from "./components/Inputs";
+import JSONataEditor from "./components/MapFieldsJsonata";
+import {
+  PlatformSignInButton,
+  PrimaryButton,
+  PrimaryButtonNew,
+  SecondaryButton,
+} from "./components/Buttons";
+import {
+  ActionsArrayTemplate,
+  DefaultTemplate,
+  DefaultObjectTemplate,
+} from "./components/FormTemplates";
 import { Icon } from "./components/Icons";
 import JSONataUtils from "./components/JSONata/JSONataUtils";
 
@@ -48,5 +83,6 @@ export {
   Icon,
   ErrorBlock,
   DefaultTemplate,
+  DefaultObjectTemplate,
   JSONataUtils,
 };
