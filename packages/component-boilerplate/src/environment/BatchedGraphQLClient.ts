0d23a48252e2c30c1dd2e0e37f53a7278309af9e
ian hitchcock <ian.hitchcock@referralsaasquatch.com>
2021-05-10 16:13:33 -0700

    IH: first pass at more in depth error handling

--- packages/component-boilerplate/src/environment/BatchedGraphQLClient.ts
+++ packages/component-boilerplate/src/environment/BatchedGraphQLClient.ts
@@ -1,10 +1,9 @@
-//TODO: rename this file?
-import { iif, Observable, Subject } from "rxjs";
-import { bufferTime, scan } from "rxjs/operators";
+import { Subject } from "rxjs";
+import { bufferTime } from "rxjs/operators";
 import { v4 as uuid } from "uuid";
-import { RequestDocument } from "graphql-request/dist/types";
+import { ClientError, RequestDocument } from "graphql-request/dist/types";
 import { GraphQLClient } from "graphql-request";
-import { print, parse, DocumentNode, getOperationAST } from "graphql";
+import { print, parse, DocumentNode } from "graphql";
 
 //TODO: replace this package with some util functions and remove.
 import combineQuery, {
@@ -58,7 +57,48 @@ export class BatchedGraphQLClient extends GraphQLClient {
         //resolve the results
         resolveMergedQueryResult(mergedQueryResult, mergedQueryAddedEvents);
       } catch (e) {
-        rejectAllQueryAddedEventsWithError(mergedQueryAddedEvents, e);
+        // So, there is possibly both data and errors here.
+        if (e instanceof ClientError) {
+          const { data, errors } = e.response;
+          const aliases = Object.keys(data);
+          let eventsToResolve = [...mergedQueryAddedEvents];
+          // reject all that errored:
+          for (const error of errors) {
+            let erroredEvent: QueryAddedEvent;
+            // todo: this needs testing, is path going to work??
+            error.path.find((key, index) => {
+              if (aliases.includes(key)) {
+                const erroredId = getIdFromAliasedField(key);
+                const indexOfErroredEvent = eventsToResolve.findIndex(
+                  (event) => event.id === erroredId
+                );
+                if (indexOfErroredEvent === -1) return false;
+                // remove from data
+                delete data[key];
+                // remove from aliases
+                aliases.splice(index, 1);
+                // remove from events
+                erroredEvent = eventsToResolve.splice(
+                  indexOfErroredEvent,
+                  1
+                )[0];
+                const { query, variables } = erroredEvent;
+
+                const errorResponse = { ...e.response };
+                errorResponse.error = [error];
+                const newError = new ClientError(errorResponse, {
+                  query: typeof query !== "string" ? print(query) : query,
+                  variables,
+                });
+                rejectQueryAddedEventWithError(erroredEvent, newError);
+                return true;
+              }
+              return false;
+            });
+          }
+          // resolve the rest:
+          resolveMergedQueryResult(data, eventsToResolve);
+        } else rejectAllQueryAddedEventsWithError(mergedQueryAddedEvents, e);
       }
     });
 
@@ -73,9 +113,45 @@ export class BatchedGraphQLClient extends GraphQLClient {
       }
     });
   }
+
   superRequest<T>(query: RequestDocument, variables?: any) {
-    return super.request(query, variables);
+    return super.request<T>(query, variables);
   }
+
+  // async superRequest<T = any>(
+  //   document: RequestDocument,
+  //   variables?: any
+  // ): Promise<T> {
+  //   let { headers, fetch: localFetch = crossFetch, ...others } = super.options;
+  //   const resolvedDoc = resolveRequestDocument(document);
+  //   const body = createRequestBody(resolvedDoc, variables);
+
+  //   const response = await localFetch(this.url, {
+  //     method: "POST",
+  //     headers: {
+  //       ...(typeof body === "string"
+  //         ? { "Content-Type": "application/json" }
+  //         : {}),
+  //       ...resolveHeaders(headers),
+  //     },
+  //     body,
+  //     ...others,
+  //   });
+
+  //   return await getResult(response);
+
+  //   // todo: move this logic to resolving logic
+  //   if (response.ok && !result.errors && result.data) {
+  //     return result.data;
+  //   } else {
+  //     const errorResult =
+  //       typeof result === "string" ? { error: result } : result;
+  //     throw new ClientError(
+  //       { ...errorResult, status: response.status, headers: response.headers },
+  //       { query: resolvedDoc, variables }
+  //     );
+  //   }
+  // }
   request<T>(query, variables) {
     return new Promise<T>((resolve, reject) => {
       const queryAddedEvent: QueryAddedEvent = {
@@ -116,7 +192,11 @@ const generateQueryAddedEventId = () => uuid().replace(/-/g, "");
 
 const aliasFieldOrVariableFn = (name, id) => `${name}_${id}`;
 
-const removeAliasFromField = (field: string, id) => field.replace(`_${id}`, "");
+const removeAliasFromField = (field: string, id: string) =>
+  field.replace(`_${id}`, "");
+
+const getIdFromAliasedField = (field: string): string =>
+  field.split("_")[1] || "";
 
 const mergeQueryAddedEvents = (
   events: QueryAddedEvent[]
@@ -157,39 +237,50 @@ const mergeQueryAddedEvents = (
   };
 };
 
+const removeAliasesFromDataResult = (
+  queryResult: any,
+  event: QueryAddedEvent
+) => {
+  const aliases = Object.keys(queryResult);
+  const { id } = event;
+
+  return aliases.reduce((data, key) => {
+    if (key.endsWith(id)) {
+      data = {
+        ...data,
+        [removeAliasFromField(key, id)]: queryResult[key],
+      };
+    }
+    return data;
+  }, {});
+};
+
 const resolveSingleQueryResult = (queryResult: any, event: QueryAddedEvent) => {
+  const data = removeAliasesFromDataResult(queryResult, event);
   const { resolve } = event;
-  resolve(queryResult);
+  resolve(data);
 };
 
 const resolveMergedQueryResult = (
   mergedQueryResult: any,
   events: QueryAddedEvent[]
 ): void => {
-  const aliases = Object.keys(mergedQueryResult);
   for (const event of events) {
     //figure out what data we need
-    const { id } = event;
-
-    const data = aliases.reduce((data, key) => {
-      if (key.endsWith(id)) {
-        data = {
-          ...data,
-          [removeAliasFromField(key, id)]: mergedQueryResult[key],
-        };
-      }
-      return data;
-    }, {});
+    const data = removeAliasesFromDataResult(mergedQueryResult, event);
     event.resolve(data);
   }
 };
 
-// error handling
+// error handling TODO: THIS NEED OVERHAUL - PROPER MAPPING NEEDS TO BE DONE
 
 const rejectQueryAddedEventWithError = (
   event: QueryAddedEvent,
   err: Error
 ): void => {
+  if (err instanceof ClientError) {
+    err.response.data = removeAliasesFromDataResult(err.response.data, event);
+  }
   const { reject } = event;
   reject(err);
 };
@@ -199,6 +290,9 @@ const rejectAllQueryAddedEventsWithError = (
   err: Error
 ): void => {
   for (const event of events) {
+    if (err instanceof ClientError) {
+      err.response.data = removeAliasesFromDataResult(err.response.data, event);
+    }
     const { reject } = event;
     reject(err);
   }
