2e2a3bf55c2b80ac0b497d54c27551586c65ce88
ian hitchcock <ian.hitchcock@referralsaasquatch.com>
2021-07-08 16:03:28 -0700

    Move types from SquatchPortal.ts to environment.ts

--- packages/component-boilerplate/src/environment/environment.ts
+++ packages/component-boilerplate/src/environment/environment.ts
@@ -1,4 +1,20 @@
-import { PortalEnv } from "./SquatchPortal";
+/**
+ * Environment provided by components hosted in a web component (`sqh-widget`)
+ */
+
+declare global {
+  interface Window {
+    SquatchPortal?: PortalEnv;
+  }
+}
+
+/**
+ * Portal env doesn't include User Id
+ */
+export type PortalEnv = Pick<
+  WidgetIdent,
+  "tenantAlias" | "appDomain" | "programId"
+> & { portalAuthUrl: string };
 
 /**
  * Program ID context helpers
