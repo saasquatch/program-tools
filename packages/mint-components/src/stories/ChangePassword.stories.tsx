8c073d91d602ac7ce49ca895def8db57e9665ed7
Zach Harrison <zharrison100@gmail.com>
2021-07-20 11:12:53 -0700

    Stories for change password

---
+++ packages/mint-components/src/stories/ChangePassword.stories.tsx
@@ -0,0 +1,34 @@
+import { h } from "@stencil/core";
+import { PortalChangePasswordView } from "../components/sqm-portal-change-password/sqm-portal-change-password-view";
+
+export default {
+  title: "Change Password",
+};
+
+const defaultProps = {
+  states: { open: false, error: "" },
+  callbacks: {
+    setOpen: (o) => console.log(o),
+    submit: (e) => console.log("Sumbit"),
+  },
+};
+
+const openProps = {
+  states: { open: true, error: "" },
+  callbacks: {
+    setOpen: (o) => console.log(o),
+    submit: (e) => console.log("Sumbit"),
+  },
+};
+
+const errorProps = {
+  states: { open: true, error: "Something went wrong. Please try again." },
+  callbacks: {
+    setOpen: (o) => console.log(o),
+    submit: (e) => console.log("Sumbit"),
+  },
+};
+
+export const Default = () => <PortalChangePasswordView {...defaultProps} />;
+export const Open = () => <PortalChangePasswordView {...openProps} />;
+export const Error = () => <PortalChangePasswordView {...errorProps} />;
