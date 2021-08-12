4b0decb3a1a2900c0025e47f25e08f45c2093296
00salmon <sam.beveridge@referralsaasquatch.com>
2021-08-12 16:20:41 -0700

    fancier validation

--- packages/mint-components/src/components/sqm-password-field/passwordValidation.tsx
+++ packages/mint-components/src/components/sqm-password-field/passwordValidation.tsx
@@ -16,31 +16,69 @@ export const validateNewPassword = (password: string) => {
   return message;
 };
 
-const getErrorMessage = (errorKeys: string[], password: string): VNode => {
-  const errorItems = errorKeys.map((key) => {
-    switch (key) {
-      case "min":
-        return <li>be a minimum of 8 characters</li>;
-      case "uppercase":
-        return <li>contain at least 1 uppercase character</li>;
-      case "lowercase":
-        return <li>contain at least 1 lowercase character</li>;
-      case "has":
-        return <li>contain at least 1 number or symbol</li>;
-      default:
-        return "";
-    }
-  });
-  if (!errorItems.length && password)
+const Valid = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+  >
+    <path
+      d="M6 0C2.688 0 0 2.688 0 6C0 9.312 2.688 12 6 12C9.312 12 12 9.312 12 6C12 2.688 9.312 0 6 0ZM4.8 9L1.8 6L2.646 5.154L4.8 7.302L9.354 2.748L10.2 3.6L4.8 9Z"
+      fill="#259053"
+    />
+  </svg>
+);
+
+const Invalid = () => (
+  <svg
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M6 0C2.688 0 0 2.688 0 6C0 9.312 2.688 12 6 12C9.312 12 12 9.312 12 6C12 2.688 9.312 0 6 0ZM6 10.8C3.354 10.8 1.2 8.646 1.2 6C1.2 3.354 3.354 1.2 6 1.2C8.646 1.2 10.8 3.354 10.8 6C10.8 8.646 8.646 10.8 6 10.8ZM8.754 3.348L4.8 7.302L3.246 5.754L2.4 6.6L4.8 9L9.6 4.2L8.754 3.348Z"
+      fill="#555555"
+    />
+  </svg>
+);
+
+const validationMessages = {
+  min: "be a minimum of 8 characters",
+  uppercase: "contain at least 1 uppercase character",
+  lowercase: "contain at least 1 lowercase character",
+  has: "contain at least 1 number or symbol",
+};
+
+const getErrorMessage = (errorKeys: string[], password: string): string => {
+  console.log("keys", errorKeys, validationMessages);
+
+  if (!errorKeys.length && password)
     return (
       <div>
-        <b style={{ color: "green" }}>✓</b> Password has met all requirements
+        <Valid /> Password has met all requirements
       </div>
     );
+
   return (
     <div>
       Password must meet the following requirements:
-      <ul>{errorItems.map((item) => item)}</ul>
+      <ul style={{ listStyleType: "none" }}>
+        {Object.keys(validationMessages).map((errorKey) =>
+          errorKeys.includes(errorKey) ? (
+            <li>
+              <Invalid /> {validationMessages[errorKey]}
+            </li>
+          ) : (
+            <li>
+              <Valid /> {validationMessages[errorKey]}
+            </li>
+          )
+        )}
+      </ul>
     </div>
   );
 };
