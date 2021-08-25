9c4dcb2bc77a513ad762229745576efd9feddc64
Zach Harrison <zharrison100@gmail.com>
2021-08-25 15:40:06 -0700

    Add getAssetPath to get image for asset card

--- packages/mint-components/src/components/sqm-asset-card/sqm-asset-card-view.tsx
+++ packages/mint-components/src/components/sqm-asset-card/sqm-asset-card-view.tsx
@@ -1,4 +1,4 @@
-import { h } from "@stencil/core";
+import { getAssetPath, h } from "@stencil/core";
 import jss from "jss";
 import preset from "jss-preset-default";
 
@@ -42,8 +42,8 @@ const style = {
   Icon: {
     "&:hover": {
       cursor: "pointer",
-    }
-  }
+    },
+  },
 };
 
 jss.setup(preset());
@@ -73,7 +73,7 @@ export const AssetCardView = (props: AssetCardViewProps) => {
           <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
         </svg>
       </div>
-      <img src={imgUrl} class={sheet.classes.CardImg} />
+      <img src={getAssetPath(imgUrl)} class={sheet.classes.CardImg} />
     </div>
   );
 };
