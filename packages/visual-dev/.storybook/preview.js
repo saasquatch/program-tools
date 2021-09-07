e338a17560ae6d6b0ee83302092e02a54f3877bd
Locrian24 <locrian24@gmail.com>
2021-09-07 11:14:02 -0700

    updated storybook version

---
+++ packages/visual-dev/.storybook/preview.js
@@ -0,0 +1,11 @@
+import { DocsPage, DocsContainer } from "@storybook/addon-docs"
+
+export const parameters = {
+  docs: {
+    container: DocsContainer,
+    page: DocsPage
+  },
+  options: {
+    enableShortcuts: false
+  }
+}
