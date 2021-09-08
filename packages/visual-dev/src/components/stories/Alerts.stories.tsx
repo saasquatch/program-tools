de0d4df300cf1c2a60cf9f7ead917020ca10f2fc
Kutay Cinar <kutaycinar@uvic.ca>
2021-09-08 14:38:26 -0700

    KC: Finalizing Alert component

--- packages/visual-dev/src/components/stories/Alerts.stories.tsx
+++ packages/visual-dev/src/components/stories/Alerts.stories.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { AlertComponent } from "../Alerts";
+import { Alert } from "../Alerts";
 
 
 export default {
   title: "Components / Alerts",
-  component: AlertComponent
+  component: Alert
 }
 
-export const critical = () => <AlertComponent type='critical' title='A critical alert'>An explanation of what this alert is.</AlertComponent>
-export const warning = () => <AlertComponent type='warning' title='A warning alert'>An explanation of what this alert is.</AlertComponent>
-export const success = () => <AlertComponent type='success' title='A success alert'>An explanation of what this alert is.</AlertComponent>
-export const info = () => <AlertComponent type='info' title='An info alert'>An explanation of what this alert is in multiple lines.</AlertComponent>
+export const critical = () => <Alert type="critical" title='A critical alert'>An explanation of what this alert is.</Alert>
+export const warning = () => <Alert type="warning" title='A warning alert'>An explanation of what this alert is.</Alert>
+export const success = () => <Alert type='success' title='A success alert'>An explanation of what this alert is.</Alert>
+export const info = () => <Alert type='info' title='An info alert'>An explanation of what this alert is in multiple lines.</Alert>
 
-export const noTitle = () => <AlertComponent type='critical'>No title test. An explanation of what this alert is in multiple multiple multiple multiple lines.</AlertComponent>
-export const noType = () => <AlertComponent>No type test. An explanation of what this alert is in multiple multiple multiple multiple lines.</AlertComponent>
+export const noTitle = () => <Alert type='critical'>No title test. An explanation of what this alert is in multiple multiple multiple multiple lines.</Alert>
+export const noType = () => <Alert>No type test. An explanation of what this alert is in multiple multiple multiple multiple lines.</Alert>
