e102560c2e23fcd494bbfb149ca1476ea5e7fbcd
Kutay Cinar <kutaycinar@uvic.ca>
2021-09-13 10:42:34 -0700

    KC: Icon component SVG style

--- packages/visual-dev/src/components/Icon/Icon.tsx
+++ packages/visual-dev/src/components/Icon/Icon.tsx
@@ -1,24 +1,83 @@
 import * as React from 'react';
 import styled from 'styled-components'
 
-import * as Styles from './Styles'
+import { custom } from './Styles.tsx'
 
-interface ExampleComponentProps {
-	status: "success" | "error" | "info"
-	children: string
-	size?: "small" | "medium" | "large"
+interface IconProps {
+	icon: string;
+	color?: string;
+	padding?: string;
+	margin?: string;
+	fontSize?: string;
+	fontWeight?: string;
 }
-export const ExampleComponent: React.FC<ExampleComponentProps> = ({
-	size = "medium",
-	status,
-	children
+
+export const StyledIcon = styled.i<{
+  color?: string;
+  padding?: string;
+  margin?: string;
+  fontSize?: string;
+  fontWeight?: string;
+}>`
+    color: ${(props) => (props.color ? props.color : "#7c7c7c")} ;
+    ${(props) => props.padding && `padding: ${props.padding};`}
+    ${(props) => props.margin && `margin: ${props.margin};`}
+    ${(props) => props.fontSize && `font-size: ${props.fontSize};`}
+    ${(props) => props.fontWeight && `font-size: ${props.fontWeight};`}
+
+    &:before{
+      color: ${(props) => (props.color ? props.color : "#7c7c7c")} ;
+    }
+	
+`;
+
+
+export const StyledSVG = styled.div<{
+	color?: string;
+	padding?: string;
+	margin?: string;
+	fontSize?: string;
+	fontWeight?: string;
+  }>`
+	  color: ${(props) => (props.color ? props.color : "#7c7c7c")} ;
+	  ${(props) => props.padding && `padding: ${props.padding};`}
+	  ${(props) => props.margin && `margin: ${props.margin};`}
+	  ${(props) => props.fontSize && `width: ${props.fontSize};`}
+	  ${(props) => props.fontSize && `height: ${props.fontSize};`}
+  
+	  &:before{
+		color: ${(props) => (props.color ? props.color : "#7c7c7c")} ;
+	  }	  
+  `;
+
+export const Icon: React.FC<IconProps> = ({
+	icon,
+	color,
+	padding,
+	margin,
+	fontSize,
+	fontWeight
 }) => {
-	return <Button status={status} size={size}>{children}</Button>
+	return (
+			icon.startsWith('sqh') ?
+				<StyledSVG
+					fontSize={fontSize}
+					padding={padding}
+					margin={margin}
+					color={color}
+					className={icon}
+					fontWeight={fontWeight}
+				>{custom[icon]}</StyledSVG>
+			:	
+				<StyledIcon
+					fontSize={fontSize}
+					padding={padding}
+					margin={margin}
+					color={color}
+					className={icon}
+					fontWeight={fontWeight}
+				></StyledIcon>
+	)
 }
 
 
-const Button = styled.button<Required<ExampleComponentProps>>`
-	${Styles.base}
-	${props => Styles[props.status]}
-	${props => Styles[props.size]}
-`
---
+++ packages/visual-dev/src/components/Icon/Styles.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export const custom = {
+	'sqh-alert':
+
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="currentColor"
+		viewBox="0 0 23 23"
+		>
+		<path
+			d="M10.375 14.875h2.25v2.25h-2.25v-2.25zm0-9h2.25v6.75h-2.25v-6.75zM11.489.25C5.279.25.25 5.29.25 11.5s5.029 11.25 11.239 11.25c6.221 0 11.261-5.04 11.261-11.25S17.71.25 11.489.25zM11.5 20.5c-4.973 0-9-4.027-9-9s4.027-9 9-9 9 4.027 9 9-4.027 9-9 9z"
+		></path>
+	</svg>
+}
