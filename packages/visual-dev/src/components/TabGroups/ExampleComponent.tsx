bbad81bc13dceba7ad788de23cc5ecb66818428f
Kutay Cinar <kutaycinar@uvic.ca>
2021-09-15 09:37:00 -0700

    KC: Tabs initial commit

---
+++ packages/visual-dev/src/components/TabGroups/ExampleComponent.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import styled from 'styled-components'
+
+import * as Styles from './Styles'
+
+interface ExampleComponentProps {
+  status: "success" | "error" | "info"
+  children: string
+  size?: "small" | "medium" | "large"
+}
+
+const Button = styled.button<Required<ExampleComponentProps>>`
+  ${Styles.base}
+  ${props => Styles[props.status]}
+  ${props => Styles[props.size]}
+`
+
+export const ExampleComponent: React.FC<ExampleComponentProps> = ({
+  size = "medium",
+  status,
+  children
+}) => {
+  return <Button status={status} size={size}>{children}</Button>
+}
