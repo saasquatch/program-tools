f0c5fcb0edb7f513feddf6821ec617dbada790a8
Kutay Cinar <kutaycinar@uvic.ca>
2021-09-16 16:41:56 -0700

    KC: major changes

--- packages/visual-dev/src/components/ExampleComponent/ExampleComponent.tsx
+++ packages/visual-dev/src/components/ExampleComponent/ExampleComponent.tsx
@@ -1,24 +1,33 @@
 import * as React from 'react';
-import styled from 'styled-components'
-
+import styled, { CSSProp } from 'styled-components'
 import * as Styles from './Styles'
 
-interface ExampleComponentProps {
+interface Subprops {
   status: "success" | "error" | "info"
   children: string
   size?: "small" | "medium" | "large"
 }
 
-const Button = styled.button<Required<ExampleComponentProps>>`
+type ExampleComponentProps = Props & React.ComponentProps<"button">
+type Props = Subprops & {css?: CSSProp}
+
+const Button = styled.button<Required<Props>>`
   ${Styles.base}
   ${props => Styles[props.status]}
   ${props => Styles[props.size]}
+  ${props => props.css}
 `
 
-export const ExampleComponent: React.FC<ExampleComponentProps> = ({
-  size = "medium",
-  status,
-  children
-}) => {
-  return <Button status={status} size={size}>{children}</Button>
-}
+export const ExampleComponent = React.forwardRef<React.ElementRef<'button'>, ExampleComponentProps>((props, forwardedRef) => {
+  const {
+    size = "medium",
+    status,
+    children,
+	  css = {},
+    ...rest
+	} = props;
+
+  return (
+		<Button {...rest} status={status} size={size} ref={forwardedRef} css={css}>{children}</Button>
+  )
+})
--- packages/visual-dev/src/components/Icon/Icon.tsx
+++ packages/visual-dev/src/components/Icon/Icon.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import styled from 'styled-components'
 import * as SVGs from './SVGs'
 
-type IconProps = OptionProps & StyleProps
+type IconProps = OptionProps & StyleProps & React.ComponentProps<"div">
 
 interface OptionProps {
 	icon: keyof typeof SVGs
@@ -19,7 +19,7 @@ export const default_size = {
 	"large": "36px"
 }
 
-export const StyledSVG = styled.div<Required<StyleProps>>`
+export const StyledSVG = styled.div<Required<StyleProps> `
 	color: ${(props) => props.color};
 	width: ${(props) => default_size.hasOwnProperty(props.size) ? default_size[props.size] : props.size};
 	height: ${(props) => default_size.hasOwnProperty(props.size) ? default_size[props.size] : props.size};
@@ -28,12 +28,14 @@ export const StyledSVG = styled.div<Required<StyleProps>>`
 export const Icon: React.FC<IconProps> = ({
 	icon,
 	color = "var(--sq-text-subdued)",
-	size = "medium"
+	size = "medium",
+	...rest
 }) => {
 	return (
 			<StyledSVG
 				size={size}
 				color={color}
+				{...rest}
 			>
 				{ Object.keys(SVGs).includes(icon) ? SVGs[icon] : SVGs['placeholder'] }
 			</StyledSVG>
