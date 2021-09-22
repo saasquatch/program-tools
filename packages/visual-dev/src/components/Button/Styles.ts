7eb1a571e2f89abfb89a233ca8b89bef474061d2
Kutay Cinar <kutaycinar@uvic.ca>
2021-09-22 11:01:47 -0700

    KC: Button component

---
+++ packages/visual-dev/src/components/Button/Styles.ts
@@ -0,0 +1,145 @@
+import { css } from 'styled-components'
+
+export const base = css`
+	cursor: pointer;
+	padding: 5px 14px;
+	font: 700 14px Helvetica;
+	line-height: 16px;
+	box-sizing: border-box;
+	border-radius: 4px;	
+`
+
+export const primary = css`
+	border: 1px solid transparent;
+	background: #f49c20;
+	color: #ffffff;
+	&:hover{
+		background: #DC8F32;
+	}
+`
+export const primary_small = css`
+	padding: 2.5px 7px;
+`
+export const primary_medium = css`
+	padding: 5px 14px;
+`
+export const primary_large = css`
+	padding: 10px 28px;
+`
+
+export const secondary = css`
+	border: 1px solid #A6B9BD;
+	background: #ffffff;
+	color: #575757;
+	&:hover{
+		color: #ffffff;
+		background: #A6B9BD;
+	}
+`
+export const secondary_small = css`
+	padding: 2.5px 7px;
+`
+export const secondary_medium = css`
+	padding: 5px 14px;
+`
+export const secondary_large = css`
+	padding: 10px 28px;
+`
+
+export const text = css`
+
+	border-radius: 0px;
+	margin: 0px 14px;
+	padding: 5px 0px;
+	border: none;
+	background: none;
+	border-bottom: 2px solid transparent;
+
+	color: #575757;
+	&:hover{
+		border-bottom: 2px solid #575757;
+	}
+`
+
+export const text_small = css`
+	font-size: 12px;
+`
+export const text_medium = css`
+	font-size: 14px;
+`
+export const text_large = css`
+	font-size: 18px;
+`
+
+export const circle = css`
+	border: 1px solid #A6B9BD;		
+	background: #ffffff;
+	color: #575757;
+	padding: 0px;
+	width:	40px;
+	height: 40px;
+	border-radius: 100%;
+	&:hover{
+		color: #ffffff;
+		background: #A6B9BD;
+	}
+`
+
+export const circle_small = css`
+	width:	36px;
+	height: 36px;
+`
+
+export const circle_medium = css`
+	width:	44px;
+	height: 44px;
+`
+
+export const circle_large = css`
+	width:	60px;
+	height: 60px;
+`
+
+export const pill = css`
+	padding: 5px 16px;
+	border-radius: 100px;
+`
+export const disable_primary = css`
+	user-select: none;
+	cursor: not-allowed;
+	pointer-events: none;
+	background-color: #ebebeb;
+`
+
+export const loading_primary = css`
+	background-color: #ebebeb;
+	user-select: none;
+	pointer-events: none;
+`
+
+export const loading_secondary = css`
+	color: #EBEBEB;
+	border-color: #EBEBEB;
+	user-select: none;
+	pointer-events: none;
+`
+
+export const disable_secondary = css`
+	color: #EBEBEB;
+	border-color: #EBEBEB;
+	user-select: none;
+	pointer-events: none;
+`
+
+export const danger = css`
+	background-color: #FE6666;
+	&:hover{
+		background: #CB0000;
+	}
+`
+export const success = css`
+	background-color: #57AC59;
+	&:hover{
+		background: #479449;
+	}
+`
---
+++ packages/visual-dev/src/components/stories/Button.stories.tsx
@@ -0,0 +1,169 @@
+import React from 'react'
+import { Button } from '../Button'
+
+export default {
+  title: 'Components / Button',
+  component: Button,
+}
+
+export const primary = () => <Button variant='primary'>Primary</Button>
+export const primarySmall = () => (
+  <Button variant='primary' size='small'>
+    Primary
+  </Button>
+)
+export const primaryMedium = () => (
+  <Button variant='primary' size='medium'>
+    Primary
+  </Button>
+)
+export const primaryLarge = () => (
+  <Button variant='primary' size='large'>
+    Primary
+  </Button>
+)
+export const loading = () => (
+  <Button variant='primary' loading>
+    Loading
+  </Button>
+)
+export const disabled = () => (
+  <Button variant='primary' disable>
+    Disabled
+  </Button>
+)
+
+export const danger = () => (
+  <Button variant='primary' danger>
+    Danger
+  </Button>
+)
+export const success = () => (
+  <Button variant='primary' success>
+    Success
+  </Button>
+)
+
+export const icon = () => <Button variant='primary' icon='settings'></Button>
+
+export const primaryPill = () => (
+  <Button variant='primary' pill>
+    Primary
+  </Button>
+)
+
+export const loadingPill = () => (
+  <Button variant='primary' loading pill>
+    Loading
+  </Button>
+)
+export const disabledPill = () => (
+  <Button variant='primary' disable pill>
+    Disabled
+  </Button>
+)
+
+export const dangerPill = () => (
+  <Button variant='primary' danger pill>
+    Danger
+  </Button>
+)
+export const successPill = () => (
+  <Button variant='primary' success pill>
+    Success
+  </Button>
+)
+
+export const iconPill = () => <Button variant='primary' icon='settings' pill></Button>
+
+export const secondary = () => <Button variant='secondary'>Secondary</Button>
+
+export const secondarySmall = () => (
+  <Button variant='secondary' size='small'>
+    Secondary
+  </Button>
+)
+export const secondaryMedium = () => (
+  <Button variant='secondary' size='medium'>
+    Secondary
+  </Button>
+)
+export const secondaryLarge = () => (
+  <Button variant='secondary' size='large'>
+    Secondary
+  </Button>
+)
+
+export const secondaryLoading = () => (
+  <Button variant='secondary' loading>
+    Loading
+  </Button>
+)
+
+export const secondaryDisabled = () => (
+  <Button variant='secondary' disable>
+    Disabled
+  </Button>
+)
+
+export const secondaryPill = () => (
+  <Button variant='secondary' pill>
+    Secondary
+  </Button>
+)
+
+export const secondaryLoadingPill = () => (
+  <Button variant='secondary' loading pill>
+    Loading
+  </Button>
+)
+
+export const secondaryDisabledPill = () => (
+  <Button variant='secondary' disable pill>
+    Disabled
+  </Button>
+)
+
+export const circleIconSmall = () => <Button variant='circle' icon='add' size='small' />
+export const circleIconMedium = () => <Button variant='circle' icon='add' size='medium' />
+export const circleIconLarge = () => <Button variant='circle' icon='add' size='large' />
+
+export const iconButton = () => (
+  <Button variant='secondary' icon='add'>
+    Icon Button
+  </Button>
+)
+
+export const textSmall = () => (
+  <Button variant='text' size='small'>
+    Text Button
+  </Button>
+)
+export const textMedium = () => (
+  <Button variant='text' size='medium'>
+    Text Button
+  </Button>
+)
+export const textLarge = () => (
+  <Button variant='text' size='large'>
+    Text Button
+  </Button>
+)
+
+export const iconTextSmall = () => (
+  <Button variant='text' icon='block' size='small'>
+    Text Button
+  </Button>
+)
+
+export const iconTextMedium = () => (
+  <Button variant='text' icon='block' size='medium'>
+    Text Button
+  </Button>
+)
+
+export const iconTextLarge = () => (
+  <Button variant='text' icon='block' size='large'>
+    Text Button
+  </Button>
+)
