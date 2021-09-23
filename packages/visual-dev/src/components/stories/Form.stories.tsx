decb6e8121260d037418aea33e3ffb87fef3d150
Kutay Cinar <kutaycinar@uvic.ca>
2021-09-23 14:14:53 -0700

    KC: console error fixes

--- packages/visual-dev/src/components/stories/Form.stories.tsx
+++ packages/visual-dev/src/components/stories/Form.stories.tsx
@@ -11,8 +11,8 @@ export const FunctionalInput = () => {
   return <TextBox value={value} onChange={(e) => setValue(e.target.value)} />
 }
 
-export const InputText = () => <TextBox value='Input Text' />
-export const InputDisabled = () => <TextBox value='Input Text' disabled />
+export const InputText = () => <TextBox value='Input Text' onChange={() => void 0} />
+export const InputDisabled = () => <TextBox value='Input Text' disabled onChange={() => void 0} />
 export const Placeholder = () => <TextBox placeholder='Placeholder Text' />
 export const PlaceholderDisabled = () => <TextBox placeholder='Placeholder Text' disabled />
 
@@ -27,7 +27,7 @@ export const radioChecked = () => {
   const options = {
     primary: 'Checked',
   }
-  return <R1 options={options} checked />
+  return <R1 options={options} checked onChange={() => void 0} />
 }
 
 export const RadioDescription = () => {
@@ -36,7 +36,7 @@ export const RadioDescription = () => {
     secondary: 'Addtional text describing conequences of this option',
   }
   const [value, setValue] = useState(false)
-  return <R2 options={options} checked={value} onClick={() => setValue(true)} />
+  return <R2 options={options} checked={value} onClick={() => setValue(true)} onChange={() => void 0} />
 }
 
 export const RadioDescriptionChecked = () => {
@@ -45,7 +45,7 @@ export const RadioDescriptionChecked = () => {
     secondary: 'Addtional text describing conequences of this option',
   }
   const [value, setValue] = useState(true)
-  return <R2 options={options} checked={value} onClick={() => setValue(true)} />
+  return <R2 options={options} checked={value} onClick={() => setValue(true)} onChange={() => void 0} />
 }
 
 export const checkbox = () => {
@@ -59,5 +59,5 @@ export const checkboxChecked = () => {
   const options = {
     primary: 'Checked',
   }
-  return <Checkbox options={options} checked />
+  return <Checkbox options={options} checked onChange={() => void 0} />
 }
