aca909b9051d9899f9e0917d8c55104c6207232d
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2021-09-24 10:32:30 -0700

    Add files via upload

---
+++ packages/visual-dev/src/components/stories/Badge.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Badge } from '../Badge'
+
+export default {
+  title: 'Components / Badge',
+  component: Badge,
+}
+
+export const defaultBadge = () => <Badge status='info'>Default Badge</Badge>
+export const defaultBadgeWithIcon = () => (
+  <Badge status='info' icon='gift'>
+    Badge with icon
+  </Badge>
+)
+export const successBadge = () => <Badge status='success'>Default Badge</Badge>
+export const successBadgeWithIcon = () => (
+  <Badge status='success' icon='gift'>
+    Badge with icon
+  </Badge>
+)
+export const activeBadge = () => <Badge status='active'>Default Badge</Badge>
+export const activeBadgeWithIcon = () => (
+  <Badge status='active' icon='gift'>
+    Badge with icon
+  </Badge>
+)
+export const criticalBadge = () => <Badge status='critical'>Default Badge</Badge>
+export const criticalBadgeWithIcon = () => (
+  <Badge status='critical' icon='gift'>
+    Badge with icon
+  </Badge>
+)
+export const warningBadge = () => <Badge status='warning'>Default Badge</Badge>
+export const warningBadgeWithIcon = () => (
+  <Badge status='warning' icon='gift'>
+    Badge with icon
+  </Badge>
+)
+export const defaultLongBadge = () => <Badge status='info'>A badge with a really really long name</Badge>
+export const defaultLongBadgeWithIcon = () => (
+  <Badge status='info' icon='gift'>
+    A badge with a really really long name
+  </Badge>
+)
+export const iconOnly = () => <Badge status='info' icon='trash' />
