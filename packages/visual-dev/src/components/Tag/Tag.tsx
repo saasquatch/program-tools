7555957ba69ebee2e0beb3485f1814a2f06b23c1
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2021-09-24 10:46:48 -0700

    Add files via upload

--- packages/visual-dev/src/components/Tag/Tag.tsx
+++ packages/visual-dev/src/components/Tag/Tag.tsx
@@ -1,41 +1,39 @@
-import * as React from "react";
-import styled from "styled-components";
-import * as Styles from "./Styles";
+import * as React from 'react'
+import styled, { CSSProp } from 'styled-components'
+import * as Styles from './Styles'
+import { Icon } from '../Icon'
 
-interface TagProps {
-  children?: any[] | string;
-  onClick?: (e: React.SyntheticEvent) => Promise<void>;
+type TagProps = OptionProps & StyleProps & React.ComponentProps<'div'>
+
+interface OptionProps {
+  onClickClose?: () => void
+  children: React.ReactNode
 }
 
-const XIcon = (
-  <svg
-    width="8"
-    height="10"
-    viewBox="0 0 8 10"
-    fill="inherit"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path d="M6.28571 3.15385L4.57143 5L6.28571 6.84615L5.71429 7.46154L4 5.61538L2.28571 7.46154L1.71429 6.84615L3.42857 5L1.71429 3.15385L2.28571 2.53846L4 4.38462L5.71429 2.53846L6.28571 3.15385Z" />
-  </svg>
-);
+interface StyleProps {
+  css?: CSSProp
+}
 
-const TagStyled = styled.span`
+const TagStyled = styled.div<StyleProps>`
   ${Styles.base}
-`;
-
-const TextSegmentStyled = styled.div`
-  ${Styles.textSegment}
-`;
+  ${(props) => props.css}
+`
 
 const IconSegmentStyled = styled.div`
   ${Styles.iconSegment}
-`;
+`
 
-export const Tag: React.FC<TagProps> = ({ children, onClick }) => {
+const TextSegmentStyled = styled.div`
+  ${Styles.textSegment}
+`
+
+export const Tag = React.forwardRef<React.ElementRef<'div'>, TagProps>((props, forwardedRef) => {
+  const { onClickClose, children, css = {}, ...rest } = props
+
   return (
-    <TagStyled>
-      <IconSegmentStyled onClick={onClick}>{XIcon}</IconSegmentStyled>
+    <TagStyled {...rest} ref={forwardedRef} css={css}>
+      <IconSegmentStyled onClick={onClickClose}>{<Icon size='10px' icon='close' />}</IconSegmentStyled>
       <TextSegmentStyled>{children}</TextSegmentStyled>
     </TagStyled>
-  );
-};
+  )
+})
