6ea5a9ac17b3770713d3933f0ec28df0376b7feb
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2021-09-27 14:39:26 -0700

    Add files via upload

---
+++ packages/visual-dev/src/components/stories/Radio.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import root from 'react-shadow/styled-components'
+import styled from 'styled-components'
+import { Radio, RadioGroup } from '../Radio'
+
+export default {
+  title: 'Components / Radio',
+  component: Radio,
+}
+
+const ShadowDom = styled(root.div)``
+
+export const Functional = () => {
+  return (
+    <RadioGroup>
+      <Radio options={{ text: 'Option 1' }} name='group1' />
+      <Radio options={{ text: 'Option 2' }} name='group1' />
+      <Radio options={{ text: 'Option 3' }} name='group1' />
+    </RadioGroup>
+  )
+}
+
+export const radio = () => {
+  const options = {
+    text: 'Radio',
+  }
+  return <Radio options={options} value={false} onChange={() => void 0} />
+}
+
+export const radioChecked = () => {
+  const options = {
+    text: 'Checked',
+  }
+  return <Radio options={options} value={true} onChange={() => void 0} />
+}
