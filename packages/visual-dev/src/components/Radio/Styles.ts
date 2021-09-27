81eef95eaa054b3964a2cf58f38abc17373a8f6e
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2021-09-27 14:39:46 -0700

    Add files via upload

---
+++ packages/visual-dev/src/components/Radio/Styles.ts
@@ -0,0 +1,52 @@
+import { css } from 'styled-components'
+
+export const RadioLabelStyle = css`
+  user-select: none;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  cursor: pointer;
+  margin-right: 10px;
+
+  color: #575757;
+  font-family: Helvetica;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 20px;
+`
+
+export const RadioInputStyle = css`
+  display: none;
+
+  &:checked + div {
+    border-color: #f49c20;
+  }
+
+  &:checked + div::after {
+    transform: scale(1);
+  }
+`
+
+export const RadioButtonStyle = css`
+  margin-top: 2.5px;
+  width: 14px;
+  height: 14px;
+  border: 1.5px solid #575757;
+  border-radius: 50%;
+  margin-right: 20px;
+  box-sizing: border-box;
+  padding: 1.5px;
+  flex-shrink: 0;
+
+  &::after {
+    content: '';
+    width: 8px;
+    height: 8px;
+    display: block;
+    background: #f49c20;
+    border-radius: 100%;
+    transform: scale(0);
+    transition: transform 0.15s;
+  }
+`
