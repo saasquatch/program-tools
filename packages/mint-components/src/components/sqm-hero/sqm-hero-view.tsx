90e4fec4b6542224c337b990c6d57881c4216a9f
Noah Clarke <noah.clarke@referralsaasquatch.com>
2021-10-01 10:17:56 -0700

    better hero component stories

--- packages/mint-components/src/components/sqm-hero/sqm-hero-view.tsx
+++ packages/mint-components/src/components/sqm-hero/sqm-hero-view.tsx
@@ -16,12 +16,14 @@ export interface HeroProps {
 export function HeroView(props: HeroProps, children: VNode) {
   const { states, content } = props;
 
+  console.log(states.backgroundImage);
+
   const column = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    minHeight: "100vh",
+    minHeight: "100%",
   };
 
   const style = {
@@ -32,7 +34,7 @@ export function HeroView(props: HeroProps, children: VNode) {
     SingleColumnContainer: {
       ...column,
       backgroundImage: `${
-        states.backgroundImage ? states.backgroundImage : ""
+        states.backgroundImage ? `url(${states.backgroundImage})` : ""
       }`,
       backgroundPosition: "center",
       backgroundRepeat: "no-repeat",
@@ -40,6 +42,7 @@ export function HeroView(props: HeroProps, children: VNode) {
     },
     Container: {
       width: "100%",
+      minHeight: "100%",
     },
   };
 
@@ -47,15 +50,31 @@ export function HeroView(props: HeroProps, children: VNode) {
   const sheet = jss.createStyleSheet(style);
   const styleString = sheet.toString();
 
+  const vanillaStyle = `
+  :host{    
+    height: 100%;
+  }
+  sqm-hero{    
+    height: 100%;
+  }
+  img{
+      max-width: 100%;
+      height: auto;
+  }
+`;
+
   return (
     <div class={sheet.classes.Container}>
-      <style type="text/css">{styleString}</style>
+      <style type="text/css">
+        {vanillaStyle}
+        {styleString}
+      </style>
       {states.columns == 1 ? (
         <div class={sheet.classes.SingleColumnContainer}>{children}</div>
       ) : (
         <div class={sheet.classes.TwoColumnContainer}>
           <div class={sheet.classes.ColumnWrapper}>{content.leftColumn}</div>
-          <div class={sheet.classes.ColumnWrapper}>{content.leftColumn}</div>
+          <div class={sheet.classes.ColumnWrapper}>{content.rightColumn}</div>
         </div>
       )}
     </div>
--- packages/mint-components/src/stories/Hero.stories.tsx
+++ packages/mint-components/src/stories/Hero.stories.tsx
@@ -8,7 +8,7 @@ export default {
   },
 };
 
-export const SimpleOneColumn = () => {
+export const LoginOneColumn = () => {
   const props = {
     states: {
       columns: 1 as const,
@@ -16,22 +16,84 @@ export const SimpleOneColumn = () => {
     content: {},
   };
   return (
-    <HeroView {...props}>
-      <div>test</div>
-    </HeroView>
+    <div style={{ height: "800px" }}>
+      <div>
+        <h1 style={{ textAlign: "center" }}>Get Referring!</h1>
+        <sqm-portal-login></sqm-portal-login>
+      </div>
+    </div>
   );
 };
 
-export const OneColumnWithImage = () => {
-  return <div></div>;
+export const LoginOneColumnWithImage = () => {
+  const props = {
+    states: {
+      columns: 1 as const,
+      backgroundImage:
+        "https://images.unsplash.com/photo-1599676821464-3555954838dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1939&q=80",
+    },
+    content: {},
+  };
+  return (
+    <div style={{ height: "800px" }}>
+      <HeroView {...props}>
+        <sqm-portal-login></sqm-portal-login>
+      </HeroView>
+    </div>
+  );
 };
 
-export const SimpleTwoColumn = () => {
+export const TwoColumnLogin = () => {
   const props = {
     states: {
       columns: 2 as const,
     },
-    content: { leftColumn: <div>test1</div>, rightColumn: <div>test2</div> },
+    content: {
+      leftColumn: (
+        <div style={{ width: "85%" }}>
+          <h1 style={{ textAlign: "center" }}>Get Referring!</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+        </div>
+      ),
+      rightColumn: (
+        <sqm-portal-login style={{ width: "85%" }}></sqm-portal-login>
+      ),
+    },
   };
-  return <HeroView {...props}></HeroView>;
+  return (
+    <div style={{ height: "800px" }}>
+      <HeroView {...props}></HeroView>
+    </div>
+  );
+};
+
+export const TwoColumnLoginWithImage = () => {
+  const props = {
+    states: {
+      columns: 2 as const,
+    },
+    content: {
+      leftColumn: (
+        <div style={{ width: "85%" }}>
+          <h1 style={{ textAlign: "center" }}>Get Referring!</h1>
+          <sqm-portal-login></sqm-portal-login>
+        </div>
+      ),
+      rightColumn: (
+        <img src="https://images.unsplash.com/photo-1629004021495-83fe9b730acb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80"></img>
+      ),
+    },
+  };
+  return (
+    <div style={{ height: "800px" }}>
+      <HeroView {...props}></HeroView>
+    </div>
+  );
 };
