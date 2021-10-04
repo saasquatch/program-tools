a64fc8189237ea83e5095929f0a782def54a59c9
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2021-10-04 13:30:21 -0700

    story cleanup

--- packages/mint-components/src/stories/PortalTemplates.stories.tsx
+++ packages/mint-components/src/stories/PortalTemplates.stories.tsx
@@ -1,4 +1,4 @@
-import { useState } from "@saasquatch/universal-hooks";
+import { useEffect, useState } from "@saasquatch/universal-hooks";
 import { h } from "@stencil/core";
 import { createHookStory } from "../components/sqm-stencilbook/HookStoryAddon";
 import portalTemplate from "../templates/Portal.html";
@@ -152,50 +152,43 @@ export const EmailVerified = createHookStory(() => {
   return <TemplateView states={states} callbacks={callbacks} />;
 });
 
-// function useGraphQL() {
-//   window.widgetIdent = {
-//     tenantAlias: "test_agvu4yg8zrkxt",
-//     appDomain: "https://app.referralsaasquatch.com",
-//     userId: "rfcdhX2WTcgyJUB061TxE1xwXvj1",
-//     accountId: "rfcdhX2WTcgyJUB061TxE1xwXvj1",
-//     token:
-//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImFjY291bnRJZCI6InJmY2RoWDJXVGNneUpVQjA2MVR4RTF4d1h2ajEiLCJpZCI6InJmY2RoWDJXVGNneUpVQjA2MVR4RTF4d1h2ajEifX0.lGUEN_cmRrSdw-y2fEz-BQ2R5COoN8tQTiKJGEMfCcI",
-//     programId: "Vacay-referral",
-//     engagementMedium: "EMBED",
-//   };
-// }
+function useGraphQL() {
+  window.widgetIdent = {
+    tenantAlias: "test_agvu4yg8zrkxt",
+    appDomain: "https://app.referralsaasquatch.com",
+    userId: "rfcdhX2WTcgyJUB061TxE1xwXvj1",
+    accountId: "rfcdhX2WTcgyJUB061TxE1xwXvj1",
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImFjY291bnRJZCI6InJmY2RoWDJXVGNneUpVQjA2MVR4RTF4d1h2ajEiLCJpZCI6InJmY2RoWDJXVGNneUpVQjA2MVR4RTF4d1h2ajEifX0.lGUEN_cmRrSdw-y2fEz-BQ2R5COoN8tQTiKJGEMfCcI",
+    programId: "Vacay-referral",
+    engagementMedium: "EMBED",
+  };
+}
 
 export const ProgramSwitch = createHookStory(() => {
-  // useGraphQL();
-  const [template, setTemplate] = useState("");
+  useGraphQL();
+
+  useEffect(() => {
+    //@ts-ignore
+    () => (window.widgetIdent = { env: "demo" });
+  }, []);
+
   return (
     <div>
-      <button
-        onClick={() =>
-          setTemplate(`
-            <sqb-program-section program-id="Vacay-referral">
-              <sqm-program-menu>
-                <sl-menu-item value="Vacay-referral">
-                  Vacay-referral
-                </sl-menu-item>
-                <sl-menu-item value="vacay-affiliates">
-                  vacay-affiliates
-                </sl-menu-item>
-              </sqm-program-menu>
-              <sqb-program-switch>
-                <template program-id="Vacay-referral">
-                  <sqb-widget widget-type="p/Vacay-referral/w/referrerWidget"></sqb-widget>
-                </template>
-                <template program-id="vacay-affiliates">
-                  <sqb-widget widget-type="p/vacay-affiliates/w/referrerWidget"></sqb-widget>
-                </template>
-              </sqb-program-switch>
-            </sqb-program-section>`)
-        }
-      >
-        Load Widget
-      </button>
-      <div innerHTML={template}></div>
+      <sqb-program-section program-id="Vacay-referral">
+        <sqm-program-menu>
+          <sl-menu-item value="Vacay-referral">Vacay-referral</sl-menu-item>
+          <sl-menu-item value="vacay-affiliates">vacay-affiliates</sl-menu-item>
+        </sqm-program-menu>
+        <sqb-program-switch>
+          <template program-id="Vacay-referral">
+            <sqb-widget widget-type="p/Vacay-referral/w/referrerWidget"></sqb-widget>
+          </template>
+          <template program-id="vacay-affiliates">
+            <sqb-widget widget-type="p/vacay-affiliates/w/referrerWidget"></sqb-widget>
+          </template>
+        </sqb-program-switch>
+      </sqb-program-section>
     </div>
   );
 });
