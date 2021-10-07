c2c21645d4ae178ac8c9e12bc4c1fb33c0aa9bc8
Noah Clarke <noah.clarke@referralsaasquatch.com>
2021-10-07 11:21:10 -0700

    fix hero unit height

--- packages/mint-components/src/components/sqm-hero/sqm-hero-view.tsx
+++ packages/mint-components/src/components/sqm-hero/sqm-hero-view.tsx
@@ -55,6 +55,7 @@ export function HeroView(props: HeroProps) {
     },
     Container: {
       width: "100%",
+      flex: 1,
       ...column,
       alignItems: "unset",
       background: `no-repeat center/cover ${parseBackground(
@@ -70,9 +71,11 @@ export function HeroView(props: HeroProps) {
   const vanillaStyle = `
   :host{    
     height: 100%;
+    display: contents;
   }
   sqm-hero{    
     height: 100%;
+    display: contents;
   }
   img{
       max-width: 100%;
