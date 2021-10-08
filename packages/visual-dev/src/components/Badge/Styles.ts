015ec50b030a1ff647f2745d28063e7f1e2cf342
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2021-10-08 15:02:31 -0700

    Update Styles.ts

--- packages/visual-dev/src/components/Badge/Styles.ts
+++ packages/visual-dev/src/components/Badge/Styles.ts
@@ -1,42 +1,42 @@
-import { css } from "styled-components";
+import { css } from 'styled-components'
 
 export const base = css`
   border-radius: 50px;
   padding: 4px 20px;
-  font-weight: 500;
-  font-size: 14px;
-  font-style: normal;
-  font-family: "Helvetica Neue", Helvetica, sans-serif;
-  line-height: 20px;
+  color: var(--sq-text);
+  font-family: 'Helvetica Neue', Helvetica, sans-serif;
+  font-weight: var(--sq-font-weight-regular);
+  font-size: var(--sq-font-size-regular);
+  line-height: var(--sq-line-height-regular);
   width: max-content;
-`;
+`
 
 export const info = css`
   background-color: var(--sq-border);
   border-color: var(--sq-border);
   color: var(--sq-text);
-`;
+`
 
 export const success = css`
   background-color: var(--sq-surface-success-subdued);
   border-color: var(--sq-surface-success-subdued);
   color: var(--sq-text-dark);
-`;
+`
 
 export const active = css`
   background-color: var(--sq-border-success);
   border-color: var(--sq-border-success);
   color: var(--sq-surface);
-`;
+`
 
 export const critical = css`
   background-color: var(--sq-surface-critical);
   border-color: var(--sq-surface-critical);
   color: var(--sq-surface);
-`;
+`
 
 export const warning = css`
   background-color: var(--sq-border-warning);
   border-color: var(--sq-border-warning);
   color: var(--sq-text-dark);
-`;
+`
