cfcfd2591da000fe51cd914e210bf1416f4bf809
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2021-10-20 14:00:19 -0700

    Update RadioCard.tsx

--- packages/visual-dev/src/components/RadioCard/RadioCard.tsx
+++ packages/visual-dev/src/components/RadioCard/RadioCard.tsx
@@ -17,7 +17,7 @@ const ShadowDom = styled(root.div)``
 
 const RadioLabel = styled.label<{ isChecked: boolean }>`
   ${Styles.RadioLabelStyle}
-  ${(props) => (props.isChecked ? 'border: 2px solid #DC8F32;' : '&:hover {border: 2px solid #858585;}')}
+  ${(props) => (props.isChecked ? 'border: 2px solid var(--sq-action-primary-hovered);' : '&:hover {border: 2px solid var(--sq-text-subdued);}')}
 `
 const RadioInput = styled.input`
   ${Styles.RadioInputStyle}
@@ -33,29 +33,33 @@ const RadioText = styled.div`
 
 const LeftSegment = styled.div<{ isChecked: boolean }>`
   ${Styles.LeftSegmentStyle}
-  ${(props) => (props.isChecked ? 'color: #DC8F32;' : '')}
+  ${(props) => (props.isChecked ? 'color: var(--sq-action-primary-hovered);' : '')}
 `
 
 export const RadioCard = React.forwardRef<React.ElementRef<'input'>, InputProps>((props, forwardedRef) => {
   const { value, onChange, options, icon = 'calendar', ...rest } = props
 
   let icon_color
-  value ? (icon_color = '#DC8F32') : ''
+  value ? (icon_color = 'var(--sq-action-primary-hovered)') : ''
 
   return (
-    <ShadowDom>
-      <RadioLabel htmlFor={rest.id} isChecked={value}>
-        <RadioInput type='radio' checked={value} {...rest} ref={forwardedRef} />
-        <LeftSegment isChecked={value}>
-          <Icon icon={icon} size='40px' color={icon_color} />
-        </LeftSegment>
-        <RightSegment>
-          <RadioText>
-            {options.title ? <div style={{ fontWeight: 'bold' }}> {options.title} </div> : ''}
-            {options.text ? <div style={{ color: '#858585', marginTop: 4 }}> {options.text} </div> : ''}
-          </RadioText>
-        </RightSegment>
-      </RadioLabel>
-    </ShadowDom>
+    <RadioLabel htmlFor={rest.id} isChecked={value}>
+      <RadioInput type='radio' checked={value} {...rest} ref={forwardedRef} />
+      <LeftSegment isChecked={value}>
+        <Icon icon={icon} size='40px' color={icon_color} />
+      </LeftSegment>
+      <RightSegment>
+        <RadioText>
+          {options.title ? <div style={{ fontWeight: 'bold' }}> {options.title} </div> : ''}
+          {options.text ? <div style={{ color: 'var(--sq-text-subdued)', marginTop: 4 }}> {options.text} </div> : ''}
+        </RadioText>
+      </RightSegment>
+    </RadioLabel>
   )
+})
+
+export const RadioCardGroup = React.forwardRef<React.ElementRef<'div'>, InputProps>((props) => {
+  const { children } = props
+
+  return <ShadowDom>{children}</ShadowDom>
 })
