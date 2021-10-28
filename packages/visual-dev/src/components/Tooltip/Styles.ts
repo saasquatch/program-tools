6a174995c65b60293051570e9ed1a39a03c91efc
Kutay Cinar <kutaycinar@uvic.ca>
2021-10-28 11:54:30 -0700

    KC: Applied Prettier to Tooltip

--- packages/visual-dev/src/components/Tooltip/Styles.ts
+++ packages/visual-dev/src/components/Tooltip/Styles.ts
@@ -1,86 +1,86 @@
-import { css } from 'styled-components'
+import { css } from "styled-components";
 
 export const base = css`
-	display: inline-block;
-	position: relative;
-`
+  display: inline-block;
+  position: relative;
+`;
 
 export const tooltip = css`
-	user-select: none;
-	position: absolute;
-	padding: 12px;
-	border-radius: 4px;
-	width: max-content;
-	max-width: 144px;
-	min-height: 20px;
-	color: var(--sq-surface);
-	background: rgba(35, 35, 35, 0.9);
-	font-family: Helvatica Neue, Arial;
-	font-size: 14px;
-	line-height: 20px;
-	
-	&::before {
-		content: " ";
-		border: solid transparent;
-		left: 50%;
-		height: 0;
-		width: 0;
-		position: absolute;
-		border-width: 5px;
-		margin-left: calc(5px*-1);
-	}
+  user-select: none;
+  position: absolute;
+  padding: 12px;
+  border-radius: 4px;
+  width: max-content;
+  max-width: 144px;
+  min-height: 20px;
+  color: var(--sq-surface);
+  background: rgba(35, 35, 35, 0.9);
+  font-family: Helvatica Neue, Arial;
+  font-size: 14px;
+  line-height: 20px;
 
-	&.right {
-		right: auto;
-		left: calc(100% + 10px);
-		top: 50%;
-		transform: translateX(0) translateY(-50%);
-	}
+  &::before {
+    content: " ";
+    border: solid transparent;
+    left: 50%;
+    height: 0;
+    width: 0;
+    position: absolute;
+    border-width: 5px;
+    margin-left: calc(5px * -1);
+  }
 
-	&.right::before {
-		left: calc(5px * -1);
-		top: 50%;
-		transform: translateX(0) translateY(-50%);
-		border-right-color: rgba(35, 35, 35, 0.9);
-	}
+  &.right {
+    right: auto;
+    left: calc(100% + 10px);
+    top: 50%;
+    transform: translateX(0) translateY(-50%);
+  }
 
-	&.left {
-		left: auto;
-		right: calc(100% + 10px);
-		top: 50%;
-		transform: translateX(0) translateY(-50%);
-	}
+  &.right::before {
+    left: calc(5px * -1);
+    top: 50%;
+    transform: translateX(0) translateY(-50%);
+    border-right-color: rgba(35, 35, 35, 0.9);
+  }
 
-	&.left::before {
-		left: auto;
-		right: calc(5px * -2);
-		top: 50%;
-		transform: translateX(0%) translateY(-50%);
-		border-left-color: rgba(35, 35, 35, 0.9);
-	}
+  &.left {
+    left: auto;
+    right: calc(100% + 10px);
+    top: 50%;
+    transform: translateX(0) translateY(-50%);
+  }
 
-	&.top {
-		bottom: calc(100% + 10px);
-		left: 50%;
-		transform: translateX(-50%);
-	}
+  &.left::before {
+    left: auto;
+    right: calc(5px * -2);
+    top: 50%;
+    transform: translateX(0%) translateY(-50%);
+    border-left-color: rgba(35, 35, 35, 0.9);
+  }
 
-	&.top::before {
-		top: 100%;
-		transform: translateX(5%);
-		border-top-color: rgba(35, 35, 35, 0.9);
-	}
+  &.top {
+    bottom: calc(100% + 10px);
+    left: 50%;
+    transform: translateX(-50%);
+  }
 
-	&.bottom {
-		top: calc(100% + 10px);
-		left: 50%;
-		transform: translateX(-50%);
-	}
-	
-	&.bottom::before {
-		bottom: calc(100%);
-		left: 50%;
-		transform: translateX(5%);
-		border-bottom-color: rgba(35, 35, 35, 0.9);
-	}
-`
+  &.top::before {
+    top: 100%;
+    transform: translateX(5%);
+    border-top-color: rgba(35, 35, 35, 0.9);
+  }
+
+  &.bottom {
+    top: calc(100% + 10px);
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  &.bottom::before {
+    bottom: calc(100%);
+    left: 50%;
+    transform: translateX(5%);
+    border-bottom-color: rgba(35, 35, 35, 0.9);
+  }
+`;
--- packages/visual-dev/src/components/Tooltip/Tooltip.tsx
+++ packages/visual-dev/src/components/Tooltip/Tooltip.tsx
@@ -1,37 +1,48 @@
-import React from "react"
-import styled, { CSSProp } from "styled-components"
+import React from "react";
+import styled, { CSSProp } from "styled-components";
 
-import * as Styles from "./Styles"
+import * as Styles from "./Styles";
 
-type TooltipProps = OptionProps & StyleProps & Omit<React.ComponentProps<"div">, "translate">
+type TooltipProps = OptionProps &
+  StyleProps &
+  Omit<React.ComponentProps<"div">, "translate">;
 
 interface OptionProps {
-  text?: string
-  direction?: "top" | "left" | "bottom" | "right"
-  showTooltip?: boolean
-  children?: React.ReactNode
+  text?: string;
+  direction?: "top" | "left" | "bottom" | "right";
+  showTooltip?: boolean;
+  children?: React.ReactNode;
 }
 
 interface StyleProps {
-  css?: CSSProp
+  css?: CSSProp;
 }
 
 const TooltipDiv = styled.div<Required<StyleProps>>`
   ${Styles.base}
   ${(props) => props.css}
-`
+`;
 
 const TooltipTip = styled.div`
   ${Styles.tooltip}
-`
+`;
 
-export const Tooltip = React.forwardRef<React.ElementRef<"div">, TooltipProps>((props, forwardedRef) => {
-  const { text = "", direction = "top", showTooltip = false, children, css = {}, ...rest } = props
+export const Tooltip = React.forwardRef<React.ElementRef<"div">, TooltipProps>(
+  (props, forwardedRef) => {
+    const {
+      text = "",
+      direction = "top",
+      showTooltip = false,
+      children,
+      css = {},
+      ...rest
+    } = props;
 
-  return (
-    <TooltipDiv {...rest} ref={forwardedRef} css={css}>
-      {children}
-      {showTooltip && <TooltipTip className={direction}>{text}</TooltipTip>}
-    </TooltipDiv>
-  )
-})
+    return (
+      <TooltipDiv {...rest} ref={forwardedRef} css={css}>
+        {children}
+        {showTooltip && <TooltipTip className={direction}>{text}</TooltipTip>}
+      </TooltipDiv>
+    );
+  }
+);
