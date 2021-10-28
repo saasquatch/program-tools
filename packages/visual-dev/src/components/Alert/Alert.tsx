8559c94904ab1bd34645e822d29bc12eb6478f62
Kutay Cinar <kutaycinar@uvic.ca>
2021-10-28 11:47:19 -0700

    KC: Applied Prettier to Alert

--- packages/visual-dev/src/components/Alert/Alert.tsx
+++ packages/visual-dev/src/components/Alert/Alert.tsx
@@ -1,42 +1,56 @@
-import * as React from "react"
-import styled, { CSSProp } from "styled-components"
-import { Icon } from "../Icon"
-import * as Styles from "./Styles"
+import * as React from "react";
+import styled, { CSSProp } from "styled-components";
+import { Icon } from "../Icon";
+import * as Styles from "./Styles";
 
-type AlertProps = OptionProps & StyleProps & Omit<React.ComponentProps<"div">, "translate">
+type AlertProps = OptionProps &
+  StyleProps &
+  Omit<React.ComponentProps<"div">, "translate">;
 
 interface OptionProps {
-  title: string
-  children: React.ReactNode
+  title: string;
+  children: React.ReactNode;
 }
 
 interface StyleProps {
-  type: "critical" | "warning" | "success" | "info"
-  css?: CSSProp
+  type: "critical" | "warning" | "success" | "info";
+  css?: CSSProp;
 }
 
 const AlertDiv = styled.div<Required<StyleProps>>`
   ${Styles.AlertDivStyle}
   ${(props) => Styles[props.type]}
-`
+`;
 
 const icons = {
-  critical: <Icon icon={"alert"} color="var(--sq-surface-critical)" size="23px" />,
-  warning: <Icon icon={"alert"} color="var(--sq-surface-warning)" size="23px" />,
-  success: <Icon icon={"checkmark_circle"} color="var(--sq-surface-success)" size="23px" />,
+  critical: (
+    <Icon icon={"alert"} color="var(--sq-surface-critical)" size="23px" />
+  ),
+  warning: (
+    <Icon icon={"alert"} color="var(--sq-surface-warning)" size="23px" />
+  ),
+  success: (
+    <Icon
+      icon={"checkmark_circle"}
+      color="var(--sq-surface-success)"
+      size="23px"
+    />
+  ),
   info: <Icon icon={"help"} color="var(--sq-text)" size="23px" />,
-}
+};
 
-export const Alert = React.forwardRef<React.ElementRef<"div">, AlertProps>((props, forwardedRef) => {
-  const { type: variant, title, children, css = {}, ...rest } = props
+export const Alert = React.forwardRef<React.ElementRef<"div">, AlertProps>(
+  (props, forwardedRef) => {
+    const { type: variant, title, children, css = {}, ...rest } = props;
 
-  return (
-    <AlertDiv {...rest} type={variant} ref={forwardedRef} css={css}>
-      {icons[variant]}
-      <div>
-        <strong>{title}</strong>
-        <div>{children}</div>
-      </div>
-    </AlertDiv>
-  )
-})
+    return (
+      <AlertDiv {...rest} type={variant} ref={forwardedRef} css={css}>
+        {icons[variant]}
+        <div>
+          <strong>{title}</strong>
+          <div>{children}</div>
+        </div>
+      </AlertDiv>
+    );
+  }
+);
--- packages/visual-dev/src/components/Alert/Styles.ts
+++ packages/visual-dev/src/components/Alert/Styles.ts
@@ -1,4 +1,4 @@
-import { css } from 'styled-components'
+import { css } from "styled-components";
 
 export const AlertDivStyle = css`
   display: flex;
@@ -9,31 +9,31 @@ export const AlertDivStyle = css`
   border-radius: 5px;
   box-sizing: border-box;
   color: var(--sq-text);
-  font-family: 'Helvetica Neue', Helvetica, sans-serif;
+  font-family: "Helvetica Neue", Helvetica, sans-serif;
   font-weight: var(--sq-font-weight-regular);
   font-size: var(--sq-font-size-regular);
   line-height: var(--sq-line-height-regular);
   div + div {
     padding-left: 16px;
   }
-`
+`;
 
 export const critical = css`
   background-color: var(--sq-surface-critical-subdued);
   border-color: var(--sq-border-critical);
-`
+`;
 
 export const warning = css`
   background-color: var(--sq-surface-warning-subdued);
   border-color: var(--sq-border-warning);
-`
+`;
 
 export const success = css`
   background-color: var(--sq-surface-success-subdued);
   border-color: var(--sq-border-success);
-`
+`;
 
 export const info = css`
   background-color: var(--sq-background);
   border-color: var(--sq-border);
-`
+`;
