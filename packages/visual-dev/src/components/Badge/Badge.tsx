328170e16812c5cb1a4d9cb9fab571af44c63421
Kutay Cinar <kutaycinar@uvic.ca>
2021-10-28 11:47:57 -0700

    KC: Applied Prettier to Badge

--- packages/visual-dev/src/components/Badge/Badge.tsx
+++ packages/visual-dev/src/components/Badge/Badge.tsx
@@ -1,18 +1,20 @@
-import * as React from "react"
-import styled, { CSSProp } from "styled-components"
-import * as Styles from "./Styles"
-import { IconKey, Icon } from "../Icon"
+import * as React from "react";
+import styled, { CSSProp } from "styled-components";
+import * as Styles from "./Styles";
+import { IconKey, Icon } from "../Icon";
 
-type BadgeProps = OptionProps & StyleProps & Omit<React.ComponentProps<"div">, "translate">
+type BadgeProps = OptionProps &
+  StyleProps &
+  Omit<React.ComponentProps<"div">, "translate">;
 
 interface OptionProps {
-  icon?: IconKey
+  icon?: IconKey;
 }
 
 interface StyleProps {
-  status: "info" | "success" | "critical" | "warning"
-  pill?: boolean
-  css?: CSSProp
+  status: "info" | "success" | "critical" | "warning";
+  pill?: boolean;
+  css?: CSSProp;
 }
 
 const BadgeStyled = styled.div<StyleProps>`
@@ -21,15 +23,31 @@ const BadgeStyled = styled.div<StyleProps>`
   padding: ${(props) => (props.pill ? "4px 20px" : "4px 12px")};
   border-radius: ${(props) => (props.pill ? "50px" : "4px")};
   ${(props) => props.css}
-`
+`;
 
-export const Badge = React.forwardRef<React.ElementRef<"div">, BadgeProps>((props, forwardedRef) => {
-  const { status, pill = false, icon, children, css = {}, ...rest } = props
+export const Badge = React.forwardRef<React.ElementRef<"div">, BadgeProps>(
+  (props, forwardedRef) => {
+    const { status, pill = false, icon, children, css = {}, ...rest } = props;
 
-  return (
-    <BadgeStyled {...rest} status={status} pill={pill} ref={forwardedRef} css={css}>
-      {icon && <Icon icon={icon} color="inherit" size="var(--sq-icon-size-badge)" style={{ margin: -2 }} css="+ span { margin-left: 5px; }" />}
-      {children && <span>{children}</span>}
-    </BadgeStyled>
-  )
-})
+    return (
+      <BadgeStyled
+        {...rest}
+        status={status}
+        pill={pill}
+        ref={forwardedRef}
+        css={css}
+      >
+        {icon && (
+          <Icon
+            icon={icon}
+            color="inherit"
+            size="var(--sq-icon-size-badge)"
+            style={{ margin: -2 }}
+            css="+ span { margin-left: 5px; }"
+          />
+        )}
+        {children && <span>{children}</span>}
+      </BadgeStyled>
+    );
+  }
+);
--- packages/visual-dev/src/components/Badge/Styles.ts
+++ packages/visual-dev/src/components/Badge/Styles.ts
@@ -1,42 +1,42 @@
-import { css } from 'styled-components'
+import { css } from "styled-components";
 
 export const base = css`
   padding: 4px 20px;
   color: var(--sq-text);
-  font-family: 'Helvetica Neue', Helvetica, sans-serif;
+  font-family: "Helvetica Neue", Helvetica, sans-serif;
   font-weight: var(--sq-font-weight-bold);
   font-size: var(--sq-font-size-regular);
   line-height: var(--sq-line-height-regular);
   width: max-content;
-`
+`;
 
 export const pill_false = css`
   border-radius: 4px;
-`
+`;
 export const pill_true = css`
   border-radius: 50px;
-`
+`;
 
 export const info = css`
   background-color: var(--sq-border);
   border-color: var(--sq-border);
   color: var(--sq-text);
-`
+`;
 
 export const success = css`
   background-color: var(--sq-surface-success);
   border-color: var(--sq-border-success);
   color: var(--sq-surface);
-`
+`;
 
 export const critical = css`
   background-color: var(--sq-surface-critical);
   border-color: var(--sq-surface-critical);
   color: var(--sq-surface);
-`
+`;
 
 export const warning = css`
   background-color: var(--sq-border-warning);
   border-color: var(--sq-border-warning);
   color: var(--sq-text-dark);
-`
+`;
