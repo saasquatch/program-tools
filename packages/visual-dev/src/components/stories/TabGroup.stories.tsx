1ab21b59348b0bfa281589b9955362fdac054ba4
Kutay Cinar <kutaycinar@uvic.ca>
2021-11-01 15:04:03 -0700

    KC: added a functional story

--- packages/visual-dev/src/components/stories/TabGroup.stories.tsx
+++ packages/visual-dev/src/components/stories/TabGroup.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tab, TabGroup } from '../TabGroup'
 
 export default {
@@ -6,6 +6,27 @@ export default {
   component: TabGroup,
 }
 
+export const Functional = () => {
+
+	const [count, setCount] = useState(0)
+
+	const states = {
+		0: false,
+		1: false,
+		2: false,
+	}
+
+  states[count] = true
+
+
+	return (
+	  <TabGroup>
+		<Tab selected={states[0]} onClick={() => setCount(0)}>Tab 1</Tab>
+		<Tab selected={states[1]} onClick={() => setCount(1)}>Tab 2</Tab>
+		<Tab selected={states[2]} onClick={() => setCount(2)}>Tab 3</Tab>
+	  </TabGroup>
+	)
+  }
 export const Primary = () => {
   return (
     <TabGroup>
