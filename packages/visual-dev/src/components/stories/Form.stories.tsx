db90872e8beac19cc019654bab62a14a6ff6b6f2
Noah Clarke <noah.clarke@referralsaasquatch.com>
2021-11-24 10:13:09 -0800

    fixed radio card widget bug

--- packages/visual-dev/src/components/stories/Form.stories.tsx
+++ packages/visual-dev/src/components/stories/Form.stories.tsx
@@ -1,128 +1,159 @@
-import { JSONSchema7 } from 'json-schema'
-import React from 'react'
-import { Button } from '../Button'
-import { Checkbox } from '../Checkbox'
-import { Radio, RSJFRadioWidget } from '../Radio'
-import { Input, RJSFCancellableInput, RJSFClearableInput, RJSFInput, RJSFLockableInput, RJSFNumericalInput } from '../Input'
-import Form from '@rjsf/core'
+import { JSONSchema7 } from "json-schema";
+import React from "react";
+import { Button } from "../Button";
+import { Checkbox } from "../Checkbox";
+import { RSJFRadioWidget } from "../Radio";
+import { RJSFRadioCardWidget } from "../RadioCard";
+import {
+  RJSFCancellableInput,
+  RJSFClearableInput,
+  RJSFInput,
+  RJSFLockableInput,
+  RJSFNumericalInput,
+} from "../Input";
+import Form from "@rjsf/core";
 
 export default {
-  title: 'Components / Form',
+  title: "Components / Form",
   component: Form,
-}
+};
 
-const log = (type) => console.log.bind(console, type)
+const log = (type) => console.log.bind(console, type);
 
 export const ExampleForm = () => {
   const uiSchema = {
-    'ui:widget': (props) => {
-      return <RJSFInput {...props} />
+    "ui:widget": (props) => {
+      return <RJSFInput {...props} />;
     },
-  }
+  };
 
   const schema: JSONSchema7 = {
-    type: 'string',
-  }
+    type: "string",
+  };
   return (
-    <div style={{ margin: '100px' }}>
-      <Form schema={schema} uiSchema={uiSchema} onChange={log('changed')} onSubmit={log('submitted')} onError={log('errors')}>
-        <Button buttonType='primary' style={{ marginTop: 15 }}>
+    <div style={{ margin: "100px" }}>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        onChange={log("changed")}
+        onSubmit={log("submitted")}
+        onError={log("errors")}
+      >
+        <Button buttonType="primary" style={{ marginTop: 15 }}>
           Submit
         </Button>
       </Form>
     </div>
-  )
-}
+  );
+};
 
 export const ExampleNumericalForm = () => {
   const uiSchema = {
-    'ui:widget': (props) => {
-      return <RJSFNumericalInput {...props} />
+    "ui:widget": (props) => {
+      return <RJSFNumericalInput {...props} />;
     },
-  }
+  };
 
   const schema: JSONSchema7 = {
-    type: 'string',
-  }
+    type: "string",
+  };
   return (
-    <div style={{ margin: '100px' }}>
-      <Form schema={schema} uiSchema={uiSchema} onChange={log('changed')} onSubmit={log('submitted')} onError={log('errors')}>
-        <Button buttonType='primary' style={{ marginTop: 15 }}>
+    <div style={{ margin: "100px" }}>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        onChange={log("changed")}
+        onSubmit={log("submitted")}
+        onError={log("errors")}
+      >
+        <Button buttonType="primary" style={{ marginTop: 15 }}>
           Submit
         </Button>
       </Form>
     </div>
-  )
-}
-
+  );
+};
 
 export const ExampleLockableForm = () => {
   const uiSchema = {
-    'ui:widget': (props) => {
-      return <RJSFLockableInput {...props} />
+    "ui:widget": (props) => {
+      return <RJSFLockableInput {...props} />;
     },
-  }
+  };
 
   const schema: JSONSchema7 = {
-    type: 'string',
-  }
+    type: "string",
+  };
   return (
-    <div style={{ margin: '100px' }}>
-      <Form schema={schema} uiSchema={uiSchema} onChange={log('changed')} onSubmit={log('submitted')} onError={log('errors')}>
-        <Button buttonType='primary' style={{ marginTop: 15 }}>
+    <div style={{ margin: "100px" }}>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        onChange={log("changed")}
+        onSubmit={log("submitted")}
+        onError={log("errors")}
+      >
+        <Button buttonType="primary" style={{ marginTop: 15 }}>
           Submit
         </Button>
       </Form>
     </div>
-  )
-}
-
-
+  );
+};
 
 export const ExampleClearableForm = () => {
   const uiSchema = {
-    'ui:widget': (props) => {
-      return <RJSFClearableInput {...props} />
+    "ui:widget": (props) => {
+      return <RJSFClearableInput {...props} />;
     },
-  }
+  };
 
   const schema: JSONSchema7 = {
-    type: 'string',
-  }
+    type: "string",
+  };
   return (
-    <div style={{ margin: '100px' }}>
-      <Form schema={schema} uiSchema={uiSchema} onChange={log('changed')} onSubmit={log('submitted')} onError={log('errors')}>
-        <Button buttonType='primary' style={{ marginTop: 15 }}>
+    <div style={{ margin: "100px" }}>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        onChange={log("changed")}
+        onSubmit={log("submitted")}
+        onError={log("errors")}
+      >
+        <Button buttonType="primary" style={{ marginTop: 15 }}>
           Submit
         </Button>
       </Form>
     </div>
-  )
-}
-
-
+  );
+};
 
 export const ExampleCancellableForm = () => {
   const uiSchema = {
-    'ui:widget': (props) => {
-      return <RJSFCancellableInput {...props} />
+    "ui:widget": (props) => {
+      return <RJSFCancellableInput {...props} />;
     },
-  }
+  };
 
   const schema: JSONSchema7 = {
-    type: 'string',
-  }
+    type: "string",
+  };
   return (
-    <div style={{ margin: '100px' }}>
-      <Form schema={schema} uiSchema={uiSchema} onChange={log('changed')} onSubmit={log('submitted')} onError={log('errors')}>
-        <Button buttonType='primary' style={{ marginTop: 15 }}>
+    <div style={{ margin: "100px" }}>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        onChange={log("changed")}
+        onSubmit={log("submitted")}
+        onError={log("errors")}
+      >
+        <Button buttonType="primary" style={{ marginTop: 15 }}>
           Submit
         </Button>
       </Form>
     </div>
-  )
-}
-
+  );
+};
 
 // // Checkboxes
 // export const CheckForm = () => {
@@ -186,33 +217,93 @@ export const ExampleCancellableForm = () => {
 
 export const RadioForm = () => {
   const schema: JSONSchema7 = {
-    "type": "object",
-    "properties": {
-      "numberEnumRadio": {
-        "type": "number",
-        "title": "Number enum",
-        "enum": [
-          1,
-          2,
-          3
-        ]
-      }
-    }
-  }
+    type: "object",
+    properties: {
+      numberEnumRadio: {
+        type: "number",
+        title: "Number enum",
+        enum: [1, 2, 3],
+      },
+    },
+  };
 
   const uiSchema = {
-    "numberEnumRadio": {
-        "ui:widget": RSJFRadioWidget
-      }
-  }
+    numberEnumRadio: {
+      "ui:widget": RSJFRadioWidget,
+    },
+  };
 
   return (
-    <div style={{ margin: '100px' }}>
-      <Form schema={schema} uiSchema={uiSchema} onChange={log('changed')} onSubmit={log('submitted')} onError={log('errors')}>
-        <Button buttonType='primary' style={{ marginTop: 15 }}>
+    <div style={{ margin: "100px" }}>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        onChange={log("changed")}
+        onSubmit={log("submitted")}
+        onError={log("errors")}
+      >
+        <Button buttonType="primary" style={{ marginTop: 15 }}>
           Submit
         </Button>
       </Form>
     </div>
-  )
-}
+  );
+};
+
+export const RadioCardForm = () => {
+  const schema: JSONSchema7 = {
+    type: "object",
+    properties: {
+      numberEnumRadio: {
+        type: "number",
+        title: "Number enum",
+        enum: [1, 2, 3],
+      },
+    },
+  };
+
+  const uiSchema = {
+    numberEnumRadio: {
+      "ui:widget": RJSFRadioCardWidget,
+      "ui:options": {
+        ruleOptions: [
+          {
+            key: 0,
+            icon: "icon-sqh-friends",
+            title: "Promoter and Referred Friend",
+            description:
+              "Retract rewards from both the promoter and the referred friend",
+          },
+          {
+            key: 1,
+            icon: "icon-sqh-referred_friend",
+            title: "Referred Friend",
+            description: "Only retract rewards from the Referred Friend",
+          },
+          {
+            key: 2,
+            icon: "icon-sqh-street-view",
+            title: "Promoter",
+            description: "Only retract rewards from the Promoter",
+          },
+        ],
+      },
+    },
+  };
+
+  return (
+    <div style={{ margin: "100px" }}>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        onChange={log("changed")}
+        onSubmit={log("submitted")}
+        onError={log("errors")}
+      >
+        <Button buttonType="primary" style={{ marginTop: 15 }}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
