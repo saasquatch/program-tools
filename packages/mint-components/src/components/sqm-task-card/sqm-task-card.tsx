59846f5cafe8e51a768ed0afc1b9cfb9cafdd0e1
Kutay Cinar <kutaycinar@uvic.ca>
2021-12-07 09:24:25 -0800

    KC: add reward duration onto grapesjs list

--- packages/mint-components/src/components/sqm-task-card/sqm-task-card.tsx
+++ packages/mint-components/src/components/sqm-task-card/sqm-task-card.tsx
@@ -8,7 +8,7 @@ import { TaskCardView } from "./sqm-task-card-view";
 
 /**
  * @uiName Task Card
- * @uiOrder ["reward-amount", "reward-unit", "card-title", "description", "repeatable", "show-progress-bar", "stat-type", "progress-source", "goal", "steps", "progress-bar-unit", "show-expiry", "date-expires", "button-text", "button-link", "open-new-tab"]
+ * @uiOrder ["reward-amount", "reward-unit", "card-title", "description", "repeatable", "show-progress-bar", "stat-type", "progress-source", "goal", "steps", "progress-bar-unit", "show-expiry", "reward-duration", "button-text", "button-link", "open-new-tab"]
  */
 @Component({
   tag: "sqm-task-card",
