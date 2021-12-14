a855691d02ea7199f91729fbcaa9b32988b92f34
Kutay Cinar <kutaycinar@uvic.ca>
2021-12-13 17:08:05 -0800

    KC: sqm-program-explainer and steps

--- packages/mint-components/src/components/sqm-program-explainer-step/sqm-program-explainer-step-view.tsx
+++ packages/mint-components/src/components/sqm-program-explainer-step/sqm-program-explainer-step-view.tsx
@@ -15,26 +15,42 @@ export function ProgramExplainerStepView(props: ProgramExplainerStepViewProps) {
   const style = {
     Container: {
       display: "flex",
+      flexDirection: "column",
       width: "100%",
-      height: "100%",
-      placeItems: "center",
-      placeContent: "center",
+      height: "444px",
       color: props.color,
       background: props.background,
+      padding: "24px",
+
+      "@media (max-width: 1024px)": {
+        flexDirection: "row",
+        width: "auto",
+        height: "auto",
+      },
     },
-    Logo: {
-      margin: "var(--sl-spacing-x-large)",
-    },
+    Logo: {},
     Text: {
-      margin: "var(--sl-spacing-x-large)",
+      "@media (max-width: 1024px)": {
+        marginLeft: "24px",
+      },
     },
     Title: {
+      fontSize: "16px",
+      fontWeight: "var(--sl-font-weight-bold)",
       textTransform: "uppercase",
-      fontSize: "var(--sl-font-size-large)",
-      fontWeight: "var(--sl-font-weight-semibold)",
+      lineHeight: "24px",
+      marginTop: "24px",
+      "@media (max-width: 1024px)": {
+        margin: "0",
+      },
     },
     Description: {
-      fontSize: "var(--sl-font-size-medium)",
+      fontSize: "16px",
+      lineHeight: "28px",
+      marginTop: "12px",
+      "@media (max-width: 1024px)": {
+        marginTop: "8px",
+      },
     },
   };
 
@@ -43,21 +59,19 @@ export function ProgramExplainerStepView(props: ProgramExplainerStepViewProps) {
   const styleString = sheet.toString();
 
   return (
-    <div
-      style={{
-        resize: "both",
-        overflow: "hidden",
-        height: "600px",
-        width: "500px",
-      }}
-    >
-      <div class={sheet.classes.Container}>
-        <style type="text/css">{styleString}</style>
-        <div class={sheet.classes.Logo}>
-          <div
-            style={{ width: "64px", height: "64px", background: "white" }}
-          ></div>
-        </div>
+    <div class={sheet.classes.Container}>
+      <style type="text/css">{styleString}</style>
+      <div class={sheet.classes.Logo}>
+        <div
+          style={{
+            width: "64px",
+            height: "64px",
+            borderRadius: "100%",
+            background: "white",
+          }}
+        ></div>
+      </div>
+      <div class={sheet.classes.Text}>
         <div class={sheet.classes.Title}>{props.title}</div>
         <div class={sheet.classes.Description}>{props.description}</div>
       </div>
--- packages/mint-components/src/components/sqm-program-explainer/ProgramExplainer.stories.tsx
+++ packages/mint-components/src/components/sqm-program-explainer/ProgramExplainer.stories.tsx
@@ -6,48 +6,49 @@ export default {
   title: "Components/Program Explainer",
 };
 
-export const DefaultProgramExplainerView = () => {
-  const barProps = {
-    data: {
-      programs: [
-        {
-          key: "program1",
-          label: "My Referral Program",
-        },
-        {
-          key: "program2",
-          label: "My Rewards Program",
-        },
-      ],
-    },
-  };
-  const item1Props = {
-	title: "hello",
-	description: "world"
-  };
-  const item2Props = {
-	title: "hello",
-	description: "world"
-  };
+const props1 = {
+  title: "Invite your friends to Klip",
+  description: "Earn up to $1200 in rewards for each referral",
+  color: "white",
+  background: "#5B3EDA",
+};
+const props2 = {
+  title: "If your friend signs up for a Klip Business plan",
+  description: "Get a $50 VISA giftcard when they pay for their first month",
+  color: "black",
+  background: "#F3CC66",
+};
+const props3 = {
+  title: "If your friend signs up for a Klip Enterprise plan",
+  description:
+    "Get up to $1200 in rewards - a $200 VISA giftcard when our sales team qualifies them as a good fit for Klip, and a $1000 VISA giftcard when they become a paying customer",
+  color: "black",
+  background: "#D3CCF5",
+};
 
-  const props1 = {
-    title: "Invite your friends to Klip",
-    description: "Earn up to $1200 in rewards for each referral",
-	color: "white",
-	background: "#5B3EDA"
-  };
-  const props2 = {
-    title: "Invite your friends to Klip",
-    description: "Earn up to $1200 in rewards for each referral",
-	color: "black",
-	background: "yellow"
-  };
-
-
+export const Step1 = () => {
   return (
-    <ProgramExplainerView {...barProps}>
+    <ProgramExplainerView>
+      <ProgramExplainerStepView {...props1} />
+    </ProgramExplainerView>
+  );
+};
+
+export const Step2 = () => {
+  return (
+    <ProgramExplainerView>
       <ProgramExplainerStepView {...props1} />
       <ProgramExplainerStepView {...props2} />
+    </ProgramExplainerView>
+  );
+};
+
+export const Step3 = () => {
+  return (
+    <ProgramExplainerView>
+      <ProgramExplainerStepView {...props1} />
+      <ProgramExplainerStepView {...props2} />
+      <ProgramExplainerStepView {...props3} />
     </ProgramExplainerView>
   );
 };
