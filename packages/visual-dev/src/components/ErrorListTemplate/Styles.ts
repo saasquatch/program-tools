735a4854a7407e42c67d31c6db8af03677be3dbf
Noah Clarke <noah.clarke@referralsaasquatch.com>
2022-01-13 14:49:42 -0800

    top margin on error list

--- packages/visual-dev/src/components/ErrorListTemplate/Styles.ts
+++ packages/visual-dev/src/components/ErrorListTemplate/Styles.ts
@@ -5,6 +5,7 @@ export const Container = css`
   font-weight: var(--sq-font-weight-regular);
   line-height: var(--sq-line-height-button-medium);
   font-family: var(--sq-font-family-sans);
+  margin: var(--sq-spacing-xx-large) 0 0;
 `;
 
 export const Errors = css`
