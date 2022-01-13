dd273dd1ee25272f22dfbe21b7f6341951aa5278
Noah Clarke <noah.clarke@referralsaasquatch.com>
2022-01-13 14:10:27 -0800

    unstyled array field template

--- packages/visual-dev/src/components/ArrayFieldTemplate/ArrayFieldTemplate.tsx
+++ packages/visual-dev/src/components/ArrayFieldTemplate/ArrayFieldTemplate.tsx
@@ -1,6 +1,7 @@
 import { ArrayFieldTemplateProps } from "@rjsf/core";
 import React from "react";
 import styled from "styled-components";
+import { IconButton } from "../Button";
 import * as Styles from "./Styles";
 
 export const Container = styled.div`
@@ -23,7 +24,83 @@ export const FrontMatterContainer = styled.div`
   ${Styles.FrontMatterContainer}
 `;
 
+const RequiredLabel = styled.span`
+  ${Styles.RequiredLabel}
+`;
+
+const ItemContainer = styled.div``;
+const ItemContent = styled.div``;
+const ItemButtons = styled.div``;
+const ArrayContainer = styled.div``;
+
+// Used in the two templates
+const DefaultArrayItem = (props: any) => {
+  return (
+    <ItemContainer key={props.key}>
+      <ItemContent>{props.children}</ItemContent>
+      <ItemButtons>
+        {props.hasToolbar && (
+          <>
+            {(props.hasMoveUp || props.hasMoveDown) && (
+              <IconButton
+                icon="chevron_up"
+                disabled={props.disabled || props.readonly || !props.hasMoveUp}
+                onClick={props.onReorderClick(props.index, props.index - 1)}
+              />
+            )}
+
+            {(props.hasMoveUp || props.hasMoveDown) && (
+              <IconButton
+                icon="chevron_down"
+                disabled={
+                  props.disabled || props.readonly || !props.hasMoveDown
+                }
+                onClick={props.onReorderClick(props.index, props.index + 1)}
+              />
+            )}
+
+            {props.hasRemove && (
+              <IconButton
+                icon="trash"
+                disabled={props.disabled || props.readonly}
+                onClick={props.onDropIndexClick(props.index)}
+              />
+            )}
+          </>
+        )}
+      </ItemButtons>
+    </ItemContainer>
+  );
+};
+
 export const ArrayFieldTemplate = (props: ArrayFieldTemplateProps) => {
-  console.log(props);
-  return <Container></Container>;
+  return (
+    <Container>
+      <FrontMatterContainer>
+        <Title key={`array-field-title-${props.idSchema.$id}`}>
+          {props.TitleField || props.uiSchema["ui:title"] || props.title}
+          {props.required ? <RequiredLabel> (required)</RequiredLabel> : null}
+        </Title>
+
+        {(props.uiSchema["ui:description"] || props.schema.description) && (
+          <Description key={`array-field-description-${props.idSchema.$id}`}>
+            {props.DescriptionField ||
+              props.uiSchema["ui:description"] ||
+              props.schema.description}
+          </Description>
+        )}
+      </FrontMatterContainer>
+      <ArrayContainer key={`array-item-list-${props.idSchema.$id}`}>
+        {props.items && props.items.map((p) => DefaultArrayItem(p))}
+
+        {props.canAdd && (
+          <IconButton
+            icon="add"
+            onClick={props.onAddClick}
+            disabled={props.disabled || props.readonly}
+          />
+        )}
+      </ArrayContainer>
+    </Container>
+  );
 };
