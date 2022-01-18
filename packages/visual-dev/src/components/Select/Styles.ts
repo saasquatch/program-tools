9e964817b384e86f30e62f776c6aaa64aab358e4
Noah Clarke <noah.clarke@referralsaasquatch.com>
2022-01-17 16:22:05 -0800

    fix hidden arrows on select

--- packages/visual-dev/src/components/Select/Styles.ts
+++ packages/visual-dev/src/components/Select/Styles.ts
@@ -9,6 +9,7 @@ export const Item = css`
   font-weight: var(--sq-font-weight-regular);
   line-height: var(--sq-line-height-regular);
   border-radius: inherit;
+  box-sizing: content-box;
   &:hover {
     background: var(--sq-surface-hover);
   }
