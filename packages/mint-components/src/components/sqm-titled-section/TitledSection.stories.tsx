402db9d7842a2a1ae73f307ca72dff34b7c48ff3
Kutay Cinar <kutaycinar@uvic.ca>
2022-01-26 10:25:09 -0800

    KC: simplify titled section stories

--- packages/mint-components/src/components/sqm-titled-section/TitledSection.stories.tsx
+++ packages/mint-components/src/components/sqm-titled-section/TitledSection.stories.tsx
@@ -1,59 +1,56 @@
 import { h } from "@stencil/core";
-import { MatrixStory } from "../sqm-stencilbook/Matrix";
 
 export default {
   title: "Components/Titled Section",
 };
 
-export const Align = () => {
+export const AlignLeft = () => {
   return (
-    <div>
-      <h3>Left</h3>
-      <sqm-titled-section align="left">
-        <sqm-text slot="label">
-          <h3>Earn more rewards</h3>
-        </sqm-text>
-        <sqm-text slot="content">
-          <p>
-            Get points while using Klip. Use those points to redeem rewards like
-            one free month of Klip Enterprise or two plane tickets to anywhere
-            in North America
-          </p>
-        </sqm-text>
-      </sqm-titled-section>
-      <br />
-      <hr />
-      <br />
-      <h3>Center</h3>
-      <sqm-titled-section align="center">
-        <sqm-text slot="label">
-          <h3>Earn more rewards</h3>
-        </sqm-text>
-        <sqm-text slot="content">
-          <p>
-            Get points while using Klip. Use those points to redeem rewards like
-            one free month of Klip Enterprise or two plane tickets to anywhere
-            in North America
-          </p>
-        </sqm-text>
-      </sqm-titled-section>
-      <br />
-      <hr />
-      <br />
-      <h3>Right</h3>
-      <sqm-titled-section align="right">
-        <sqm-text slot="label">
-          <h3>Earn more rewards</h3>
-        </sqm-text>
-        <sqm-text slot="content">
-          <p>
-            Get points while using Klip. Use those points to redeem rewards like
-            one free month of Klip Enterprise or two plane tickets to anywhere
-            in North America
-          </p>
-        </sqm-text>
-      </sqm-titled-section>
-    </div>
+    <sqm-titled-section align="left">
+      <sqm-text slot="label">
+        <h3>Earn more rewards</h3>
+      </sqm-text>
+      <sqm-text slot="content">
+        <p>
+          Get points while using Klip. Use those points to redeem rewards like
+          one free month of Klip Enterprise or two plane tickets to anywhere in
+          North America
+        </p>
+      </sqm-text>
+    </sqm-titled-section>
+  );
+};
+
+export const AlignCenter = () => {
+  return (
+    <sqm-titled-section align="center">
+      <sqm-text slot="label">
+        <h3>Earn more rewards</h3>
+      </sqm-text>
+      <sqm-text slot="content">
+        <p>
+          Get points while using Klip. Use those points to redeem rewards like
+          one free month of Klip Enterprise or two plane tickets to anywhere in
+          North America
+        </p>
+      </sqm-text>
+    </sqm-titled-section>
+  );
+};
+export const AlignRight = () => {
+  return (
+    <sqm-titled-section align="right">
+      <sqm-text slot="label">
+        <h3>Earn more rewards</h3>
+      </sqm-text>
+      <sqm-text slot="content">
+        <p>
+          Get points while using Klip. Use those points to redeem rewards like
+          one free month of Klip Enterprise or two plane tickets to anywhere in
+          North America
+        </p>
+      </sqm-text>
+    </sqm-titled-section>
   );
 };
 
@@ -238,182 +235,181 @@ export const Padding = () => {
 };
 
 export const LabelMargin = () => {
-	return (
-	  <div>
-		<h3>None</h3>
-		<sqm-titled-section label-margin="none">
-		  <sqm-text slot="label">
-			<h3>Earn more rewards</h3>
-		  </sqm-text>
-		  <sqm-text slot="content">
-			<p>
-			  Get points while using Klip. Use those points to redeem rewards like
-			  one free month of Klip Enterprise or two plane tickets to anywhere
-			  in North America
-			</p>
-		  </sqm-text>
-		</sqm-titled-section>
-		<br />
-		<hr />
-		<br />
-		<h3>xxx-small</h3>
-		<sqm-titled-section label-margin="xxx-small">
-		  <sqm-text slot="label">
-			<h3>Earn more rewards</h3>
-		  </sqm-text>
-		  <sqm-text slot="content">
-			<p>
-			  Get points while using Klip. Use those points to redeem rewards like
-			  one free month of Klip Enterprise or two plane tickets to anywhere
-			  in North America
-			</p>
-		  </sqm-text>
-		</sqm-titled-section>
-		<br />
-		<hr />
-		<br />
-		<h3>xx-small</h3>
-		<sqm-titled-section label-margin="xx-small">
-		  <sqm-text slot="label">
-			<h3>Earn more rewards</h3>
-		  </sqm-text>
-		  <sqm-text slot="content">
-			<p>
-			  Get points while using Klip. Use those points to redeem rewards like
-			  one free month of Klip Enterprise or two plane tickets to anywhere
-			  in North America
-			</p>
-		  </sqm-text>
-		</sqm-titled-section>
-		<br />
-		<hr />
-		<br />
-		<h3>x-small</h3>
-		<sqm-titled-section label-margin="x-small">
-		  <sqm-text slot="label">
-			<h3>Earn more rewards</h3>
-		  </sqm-text>
-		  <sqm-text slot="content">
-			<p>
-			  Get points while using Klip. Use those points to redeem rewards like
-			  one free month of Klip Enterprise or two plane tickets to anywhere
-			  in North America
-			</p>
-		  </sqm-text>
-		</sqm-titled-section>
-		<br />
-		<hr />
-		<br />
-		<h3>small</h3>
-		<sqm-titled-section label-margin="small">
-		  <sqm-text slot="label">
-			<h3>Earn more rewards</h3>
-		  </sqm-text>
-		  <sqm-text slot="content">
-			<p>
-			  Get points while using Klip. Use those points to redeem rewards like
-			  one free month of Klip Enterprise or two plane tickets to anywhere
-			  in North America
-			</p>
-		  </sqm-text>
-		</sqm-titled-section>
-		<br />
-		<hr />
-		<br />
-		<h3>medium</h3>
-		<sqm-titled-section label-margin="medium">
-		  <sqm-text slot="label">
-			<h3>Earn more rewards</h3>
-		  </sqm-text>
-		  <sqm-text slot="content">
-			<p>
-			  Get points while using Klip. Use those points to redeem rewards like
-			  one free month of Klip Enterprise or two plane tickets to anywhere
-			  in North America
-			</p>
-		  </sqm-text>
-		</sqm-titled-section>
-		<br />
-		<hr />
-		<br />
-		<h3>large</h3>
-		<sqm-titled-section label-margin="large">
-		  <sqm-text slot="label">
-			<h3>Earn more rewards</h3>
-		  </sqm-text>
-		  <sqm-text slot="content">
-			<p>
-			  Get points while using Klip. Use those points to redeem rewards like
-			  one free month of Klip Enterprise or two plane tickets to anywhere
-			  in North America
-			</p>
-		  </sqm-text>
-		</sqm-titled-section>
-		<br />
-		<hr />
-		<br />
-		<h3>x-large</h3>
-		<sqm-titled-section label-margin="x-large">
-		  <sqm-text slot="label">
-			<h3>Earn more rewards</h3>
-		  </sqm-text>
-		  <sqm-text slot="content">
-			<p>
-			  Get points while using Klip. Use those points to redeem rewards like
-			  one free month of Klip Enterprise or two plane tickets to anywhere
-			  in North America
-			</p>
-		  </sqm-text>
-		</sqm-titled-section>
-		<br />
-		<hr />
-		<br />
-		<h3>xx-large</h3>
-		<sqm-titled-section label-margin="xx-large">
-		  <sqm-text slot="label">
-			<h3>Earn more rewards</h3>
-		  </sqm-text>
-		  <sqm-text slot="content">
-			<p>
-			  Get points while using Klip. Use those points to redeem rewards like
-			  one free month of Klip Enterprise or two plane tickets to anywhere
-			  in North America
-			</p>
-		  </sqm-text>
-		</sqm-titled-section>
-		<br />
-		<hr />
-		<br />
-		<h3>xxx-large</h3>
-		<sqm-titled-section label-margin="xxx-large">
-		  <sqm-text slot="label">
-			<h3>Earn more rewards</h3>
-		  </sqm-text>
-		  <sqm-text slot="content">
-			<p>
-			  Get points while using Klip. Use those points to redeem rewards like
-			  one free month of Klip Enterprise or two plane tickets to anywhere
-			  in North America
-			</p>
-		  </sqm-text>
-		</sqm-titled-section>
-		<br />
-		<hr />
-		<br />
-		<h3>xxxx-large</h3>
-		<sqm-titled-section label-margin="xxxx-large">
-		  <sqm-text slot="label">
-			<h3>Earn more rewards</h3>
-		  </sqm-text>
-		  <sqm-text slot="content">
-			<p>
-			  Get points while using Klip. Use those points to redeem rewards like
-			  one free month of Klip Enterprise or two plane tickets to anywhere
-			  in North America
-			</p>
-		  </sqm-text>
-		</sqm-titled-section>
-	  </div>
-	);
-  };
-  
+  return (
+    <div>
+      <h3>None</h3>
+      <sqm-titled-section label-margin="none">
+        <sqm-text slot="label">
+          <h3>Earn more rewards</h3>
+        </sqm-text>
+        <sqm-text slot="content">
+          <p>
+            Get points while using Klip. Use those points to redeem rewards like
+            one free month of Klip Enterprise or two plane tickets to anywhere
+            in North America
+          </p>
+        </sqm-text>
+      </sqm-titled-section>
+      <br />
+      <hr />
+      <br />
+      <h3>xxx-small</h3>
+      <sqm-titled-section label-margin="xxx-small">
+        <sqm-text slot="label">
+          <h3>Earn more rewards</h3>
+        </sqm-text>
+        <sqm-text slot="content">
+          <p>
+            Get points while using Klip. Use those points to redeem rewards like
+            one free month of Klip Enterprise or two plane tickets to anywhere
+            in North America
+          </p>
+        </sqm-text>
+      </sqm-titled-section>
+      <br />
+      <hr />
+      <br />
+      <h3>xx-small</h3>
+      <sqm-titled-section label-margin="xx-small">
+        <sqm-text slot="label">
+          <h3>Earn more rewards</h3>
+        </sqm-text>
+        <sqm-text slot="content">
+          <p>
+            Get points while using Klip. Use those points to redeem rewards like
+            one free month of Klip Enterprise or two plane tickets to anywhere
+            in North America
+          </p>
+        </sqm-text>
+      </sqm-titled-section>
+      <br />
+      <hr />
+      <br />
+      <h3>x-small</h3>
+      <sqm-titled-section label-margin="x-small">
+        <sqm-text slot="label">
+          <h3>Earn more rewards</h3>
+        </sqm-text>
+        <sqm-text slot="content">
+          <p>
+            Get points while using Klip. Use those points to redeem rewards like
+            one free month of Klip Enterprise or two plane tickets to anywhere
+            in North America
+          </p>
+        </sqm-text>
+      </sqm-titled-section>
+      <br />
+      <hr />
+      <br />
+      <h3>small</h3>
+      <sqm-titled-section label-margin="small">
+        <sqm-text slot="label">
+          <h3>Earn more rewards</h3>
+        </sqm-text>
+        <sqm-text slot="content">
+          <p>
+            Get points while using Klip. Use those points to redeem rewards like
+            one free month of Klip Enterprise or two plane tickets to anywhere
+            in North America
+          </p>
+        </sqm-text>
+      </sqm-titled-section>
+      <br />
+      <hr />
+      <br />
+      <h3>medium</h3>
+      <sqm-titled-section label-margin="medium">
+        <sqm-text slot="label">
+          <h3>Earn more rewards</h3>
+        </sqm-text>
+        <sqm-text slot="content">
+          <p>
+            Get points while using Klip. Use those points to redeem rewards like
+            one free month of Klip Enterprise or two plane tickets to anywhere
+            in North America
+          </p>
+        </sqm-text>
+      </sqm-titled-section>
+      <br />
+      <hr />
+      <br />
+      <h3>large</h3>
+      <sqm-titled-section label-margin="large">
+        <sqm-text slot="label">
+          <h3>Earn more rewards</h3>
+        </sqm-text>
+        <sqm-text slot="content">
+          <p>
+            Get points while using Klip. Use those points to redeem rewards like
+            one free month of Klip Enterprise or two plane tickets to anywhere
+            in North America
+          </p>
+        </sqm-text>
+      </sqm-titled-section>
+      <br />
+      <hr />
+      <br />
+      <h3>x-large</h3>
+      <sqm-titled-section label-margin="x-large">
+        <sqm-text slot="label">
+          <h3>Earn more rewards</h3>
+        </sqm-text>
+        <sqm-text slot="content">
+          <p>
+            Get points while using Klip. Use those points to redeem rewards like
+            one free month of Klip Enterprise or two plane tickets to anywhere
+            in North America
+          </p>
+        </sqm-text>
+      </sqm-titled-section>
+      <br />
+      <hr />
+      <br />
+      <h3>xx-large</h3>
+      <sqm-titled-section label-margin="xx-large">
+        <sqm-text slot="label">
+          <h3>Earn more rewards</h3>
+        </sqm-text>
+        <sqm-text slot="content">
+          <p>
+            Get points while using Klip. Use those points to redeem rewards like
+            one free month of Klip Enterprise or two plane tickets to anywhere
+            in North America
+          </p>
+        </sqm-text>
+      </sqm-titled-section>
+      <br />
+      <hr />
+      <br />
+      <h3>xxx-large</h3>
+      <sqm-titled-section label-margin="xxx-large">
+        <sqm-text slot="label">
+          <h3>Earn more rewards</h3>
+        </sqm-text>
+        <sqm-text slot="content">
+          <p>
+            Get points while using Klip. Use those points to redeem rewards like
+            one free month of Klip Enterprise or two plane tickets to anywhere
+            in North America
+          </p>
+        </sqm-text>
+      </sqm-titled-section>
+      <br />
+      <hr />
+      <br />
+      <h3>xxxx-large</h3>
+      <sqm-titled-section label-margin="xxxx-large">
+        <sqm-text slot="label">
+          <h3>Earn more rewards</h3>
+        </sqm-text>
+        <sqm-text slot="content">
+          <p>
+            Get points while using Klip. Use those points to redeem rewards like
+            one free month of Klip Enterprise or two plane tickets to anywhere
+            in North America
+          </p>
+        </sqm-text>
+      </sqm-titled-section>
+    </div>
+  );
+};
