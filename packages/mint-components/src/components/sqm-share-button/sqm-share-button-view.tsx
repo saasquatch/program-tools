bd586393d6dd92e790277dd15cfb471f23a0f3e5
Kutay Cinar <kutaycinar@uvic.ca>
2022-01-26 16:23:30 -0800

    KC: T_84 share button colors and outlines

--- packages/mint-components/src/components/sqm-share-button/sqm-share-button-view.tsx
+++ packages/mint-components/src/components/sqm-share-button/sqm-share-button-view.tsx
@@ -44,21 +44,44 @@ export interface ShareButtonViewProps {
 const medium = {
   facebook: "#1877f2",
   twitter: "#1da1f2",
-  email: "#f8cc07",
-  direct: "#cd3292",
+  email: "#666666",
+  direct: "",
   linkedin: "#0077b5",
-  sms: "#e68523",
+  sms: "#34DA50",
   fbmessenger: "#0084ff",
   whatsapp: "#25d366",
-  linemessenger: "#00C72A",
+  linemessenger: "#00B300",
   pinterest: "#e60023",
-  reminder: "#b1a24a",
-  unknown: "#33482c",
+  reminder: "",
+  unknown: "",
+};
+
+const medium_text = {
+  facebook: "#fff",
+  twitter: "#fff",
+  email: "#fff",
+  direct: "",
+  linkedin: "#fff",
+  sms: "#fff",
+  fbmessenger: "#fff",
+  whatsapp: "#fff",
+  linemessenger: "#fff",
+  pinterest: "#fff",
+  reminder: "",
+  unknown: "",
 };
 
 export function ShareButtonView(props: ShareButtonViewProps, children: VNode) {
   const vanillaStyle = `
     *::part(base) {
+			--sl-focus-ring-color-primary: ${
+        props.backgroundcolor
+          ? props.backgroundcolor
+          : props.medium
+          ? medium[props.medium]
+          : ""
+      }80!important;
+
       background: ${
         props.backgroundcolor
           ? props.backgroundcolor
@@ -66,13 +89,34 @@ export function ShareButtonView(props: ShareButtonViewProps, children: VNode) {
           ? medium[props.medium]
           : ""
       };
-      color: ${props.textcolor ? props.textcolor : props.medium ? "#fff" : ""};
+      color: ${
+        props.textcolor
+          ? props.textcolor
+          : props.medium
+          ? medium_text[props.medium]
+          : ""
+      };
       border-radius: ${props.borderradius ? props.borderradius + "px" : ""};
     }
-    *::part(label) {
-    //   position: relative;
-	//   top: 5%;
-    }
+
+	*::part(base):hover {
+		border-color: ${
+      props.backgroundcolor
+        ? props.backgroundcolor
+        : props.medium
+        ? medium[props.medium]
+        : ""
+    }D1!important;
+	}
+	
+	*::part(base):focus-visible {
+		border: none;
+	}
+
+	*::part(label) {
+		position: relative;
+		top: 2%;
+	}
   `;
 
   const style = {
