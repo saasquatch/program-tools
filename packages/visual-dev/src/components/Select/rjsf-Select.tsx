9126ac2c663f1d92d5576708cfec5bd4d69c071d
Noah Clarke <noah.clarke@referralsaasquatch.com>
2022-01-31 15:42:39 -0800

    rjsf select dev

--- packages/visual-dev/src/components/Select/rjsf-Select.tsx
+++ packages/visual-dev/src/components/Select/rjsf-Select.tsx
@@ -4,10 +4,6 @@ import React from "react";
 import { Select } from "./Select";
 
 export function RJSFSelect(props: WidgetProps) {
-  if (!props.schema?.enum) {
-    return <></>;
-  }
-  const items = Object.values(props.schema?.enum);
   const functional = useSelect({ items });
   const options = props.uiSchema["ui:options"];
   const selectProps = { options, items, functional };
