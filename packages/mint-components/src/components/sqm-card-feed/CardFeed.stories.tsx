1c1406836c70f399af8b523b07d5eb613d339846
Kutay Cinar <kutaycinar@uvic.ca>
2022-02-02 11:24:52 -0800

    KC: resizer default width 100%

--- packages/mint-components/src/components/sqm-card-feed/CardFeed.stories.tsx
+++ packages/mint-components/src/components/sqm-card-feed/CardFeed.stories.tsx
@@ -1,11 +1,6 @@
 import { h } from "@stencil/core";
 import { CardFeedView } from "./sqm-card-feed-view";
 import { TaskCardView } from "../sqm-task-card/sqm-task-card-view";
-import {
-  coupleActions,
-  manyActions,
-  oneAction,
-} from "../sqm-task-card/TaskCard.stories";
 import scenario from "./sqm-card-feed.feature";
 
 export default {
@@ -13,6 +8,105 @@ export default {
   parameters: {
     scenario,
   },
+};
+
+const oneAction = {
+  callbacks: {
+    sendEvent: () => void 0,
+    onClick: () => void 0,
+  },
+  content: {
+    rewardAmount: "20",
+    rewardUnit: "SaaSquatch Points",
+    cardTitle: "Complete a survey",
+    description:
+      "Description of action and reward. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget quisque commodo leo.",
+    repeatable: false,
+    showProgressBar: false,
+    steps: false,
+    buttonText: "Take survey",
+    buttonLink: "https://example.com/",
+    openNewTab: false,
+    rewardDuration: null,
+    completedText:
+      "Completed {finite, select, 0 {{count, plural, =1 {{count} time} other {{count} times}}} other {{count}/{finite} times}}",
+    expiryMessage: "Ends {endDate}",
+    startsOnMessage: "Starts {startDate}",
+    endedMessage: "Ended {endDate}",
+    finite: 0,
+    goal: 1,
+    locale: "en",
+  },
+  states: {
+    progress: 0,
+    loading: false,
+    loadingEvent: false,
+  },
+};
+
+const coupleActions = {
+  callbacks: {
+    sendEvent: () => void 0,
+    onClick: () => void 0,
+  },
+  content: {
+    rewardAmount: "40",
+    rewardUnit: "SaaSquatch Points",
+    cardTitle: "Comment on 5 articles",
+    description:
+      "Description of action and reward. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget quisque commodo leo.",
+    repeatable: false,
+    showProgressBar: true,
+    steps: true,
+    buttonText: "Take survey",
+    buttonLink: "https://example.com/",
+    openNewTab: false,
+    completedText:
+      "Completed {finite, select, 0 {{count, plural, =1 {{count} time} other {{count} times}}} other {{count}/{finite} times}}",
+    rewardDuration: null,
+    expiryMessage: "Ends {endDate}",
+    startsOnMessage: "Starts {startDate}",
+    endedMessage: "Ended {endDate}",
+    finite: 0,
+    goal: 5,
+    locale: "en",
+  },
+  states: {
+    progress: 1,
+    loading: false,
+    loadingEvent: false,
+  },
+};
+
+const manyActions = {
+  callbacks: {
+    sendEvent: () => void 0,
+    onClick: () => void 0,
+  },
+  content: {
+    rewardAmount: "150",
+    rewardUnit: "SaaSquatch Points",
+    cardTitle: "Spend $500 at our Store",
+    description:
+      "Description of action and reward. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget quisque commodo leo.",
+    repeatable: false,
+    completedText:
+      "Completed {finite, select, 0 {{count, plural, =1 {{count} time} other {{count} times}}} other {{count}/{finite} times}}",
+    showProgressBar: true,
+    steps: false,
+    progressBarUnit: "$",
+    buttonText: "Take survey",
+    buttonLink: "https://example.com/",
+    openNewTab: false,
+    rewardDuration: null,
+    expiryMessage: "Ends {endDate}",
+    startsOnMessage: "Starts {startDate}",
+    endedMessage: "Ended {endDate}",
+    finite: 0,
+    goal: 500,
+    locale: "en",
+  },
+  states: { progress: 100, loading: false, loadingEvent: false },
 };
 
 export const TaskCardGrid = () => {
