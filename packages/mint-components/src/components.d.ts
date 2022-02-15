e39c9a4aae4c306689e881e6602060eb8c2946a8
Kutay Cinar <kutaycinar@uvic.ca>
2022-02-15 11:41:13 -0800

    KC: unused buttonText prop removed from rewardExchangeList

--- packages/mint-components/src/components.d.ts
+++ packages/mint-components/src/components.d.ts
@@ -1154,10 +1154,6 @@ export namespace Components {
          */
         "backText": string;
         /**
-          * @uiName Exchange button text
-         */
-        "buttonText": string;
-        /**
           * @uiName Cancel Button Text
          */
         "cancelText": string;
@@ -1178,6 +1174,7 @@ export namespace Components {
          */
         "continueText": string;
         /**
+          * Displayed on the reward selection page.
           * @uiName Continue to Confirmation Button Text
          */
         "continueToConfirmationText": string;
@@ -1208,6 +1205,7 @@ export namespace Components {
          */
         "promoCode": string;
         /**
+          * Shown if an error occurs when loading the reward exchange.
           * @uiName Reward List Error Message
          */
         "queryError": string;
@@ -1216,14 +1214,17 @@ export namespace Components {
          */
         "redeemText": string;
         /**
+          * Displayed as the header for the confirmation page.
           * @uiName Confirmation Title Text
          */
         "redeemTitle": string;
         /**
+          * Shown if an error occurs during the reward exchange.
           * @uiName Redemption Error Message
          */
         "redemptionError": string;
         /**
+          * Displayed on the success screen when a user successfully exchanges for a reward.
           * @uiName Redemption Success Message
          */
         "redemptionSuccessText": string;
@@ -1255,6 +1256,7 @@ export namespace Components {
          */
         "skeletonCardNum": number;
         /**
+          * Displayed on the reward exchange cards.
           * @uiName Reward Exchange Amount Text
          */
         "sourceAmountMessage": string;
@@ -3573,10 +3575,6 @@ declare namespace LocalJSX {
          */
         "backText"?: string;
         /**
-          * @uiName Exchange button text
-         */
-        "buttonText"?: string;
-        /**
           * @uiName Cancel Button Text
          */
         "cancelText"?: string;
@@ -3597,6 +3595,7 @@ declare namespace LocalJSX {
          */
         "continueText"?: string;
         /**
+          * Displayed on the reward selection page.
           * @uiName Continue to Confirmation Button Text
          */
         "continueToConfirmationText"?: string;
@@ -3627,6 +3626,7 @@ declare namespace LocalJSX {
          */
         "promoCode"?: string;
         /**
+          * Shown if an error occurs when loading the reward exchange.
           * @uiName Reward List Error Message
          */
         "queryError"?: string;
@@ -3635,14 +3635,17 @@ declare namespace LocalJSX {
          */
         "redeemText"?: string;
         /**
+          * Displayed as the header for the confirmation page.
           * @uiName Confirmation Title Text
          */
         "redeemTitle"?: string;
         /**
+          * Shown if an error occurs during the reward exchange.
           * @uiName Redemption Error Message
          */
         "redemptionError"?: string;
         /**
+          * Displayed on the success screen when a user successfully exchanges for a reward.
           * @uiName Redemption Success Message
          */
         "redemptionSuccessText"?: string;
@@ -3674,6 +3677,7 @@ declare namespace LocalJSX {
          */
         "skeletonCardNum"?: number;
         /**
+          * Displayed on the reward exchange cards.
           * @uiName Reward Exchange Amount Text
          */
         "sourceAmountMessage"?: string;
