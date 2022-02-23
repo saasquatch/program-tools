d010213483a9d9baa88aafe550e3a70c75d3ed98
Noah Clarke <noah.clarke@referralsaasquatch.com>
2022-02-23 13:14:01 -0800

    CopyWrapper Tooltip story

--- packages/visual-dev/src/components/CopyWrapper/CopyWrapper.stories.tsx
+++ packages/visual-dev/src/components/CopyWrapper/CopyWrapper.stories.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CopyWrapper } from ".";
 import { Icon } from "../..";
+import { Tooltip } from "../Tooltip";
 
 export default {
   title: "Components / Copy Wrapper",
@@ -19,6 +20,36 @@ export const AroundIcon = () => {
       </CopyWrapper>
       <br></br>
       <span>Copy success value: {copySuccess ? "true" : "false"}</span>
+    </>
+  );
+};
+
+export const WithTooltip = () => {
+  const [copySuccess, setCopySuccess] = useState(false);
+
+  useEffect(() => {
+    console.log("test");
+    const timer = setTimeout(() => {
+      setCopySuccess(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [copySuccess]);
+
+  return (
+    <>
+      <CopyWrapper
+        copyCallback={setCopySuccess}
+        copyContent="This is test clipboard content"
+      >
+        <Tooltip
+          showTooltip={copySuccess}
+          text={`Copied successfully? ${copySuccess ? "true" : "false"}`}
+          direction={"right"}
+        >
+          <Icon icon="copy" color="var(--sq-action-primary)" />
+        </Tooltip>
+      </CopyWrapper>
+      <br></br>
     </>
   );
 };
