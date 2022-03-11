90d3ffc5e14a75f387f8d6a83aff2c30385147b1
Noah Clarke <noah.clarke@referralsaasquatch.com>
2022-03-11 10:21:57 -0800

    add tag stories

--- packages/visual-dev/src/components/Tag/Tag.stories.tsx
+++ packages/visual-dev/src/components/Tag/Tag.stories.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import { Tag } from '.'
+import React from "react";
+import { Tag } from ".";
 
 export default {
-  title: 'Components / Tag',
+  title: "Components / Tag",
   component: Tag,
-}
+};
 
 const fake = async () => {
-  Promise.resolve(alert('You clicked a tag.'))
-}
+  Promise.resolve(alert("You clicked a tag."));
+};
 
-export const shortTag = () => <Tag onClickClose={fake}>newSegment</Tag>
-export const longTag = () => <Tag onClickClose={fake}>A really really long segment</Tag>
+export const shortTag = () => <Tag onClickClose={fake}>New Segment</Tag>;
+export const longTag = () => (
+  <Tag onClickClose={fake}>A really really long tag with lots of text</Tag>
+);
+export const customCSS = () => (
+  <Tag
+    customCSS={{ background: "lightblue", color: "white", borderColor: "blue" }}
+    onClickClose={fake}
+  >
+    A long segment
+  </Tag>
+);
+export const multipleLines = () => (
+  <Tag onClickClose={fake}>
+    A multi- <br></br> line tag
+  </Tag>
+);
