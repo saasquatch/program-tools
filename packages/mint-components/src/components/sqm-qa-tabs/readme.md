28a95758aba79afe42454dedf08591774c66b72c
Derek Siemens <derekgsiemens@gmail.com>
2022-05-17 15:02:34 -0700

    DS: Add more qa components

---
+++ packages/mint-components/src/components/sqm-qa-tabs/readme.md
@@ -0,0 +1,17 @@
+# sqm-tabs
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property    | Attribute   | Description                               | Type                                     | Default     |
+| ----------- | ----------- | ----------------------------------------- | ---------------------------------------- | ----------- |
+| `placement` | `placement` | Used to specify the placement of the tabs | `"bottom" \| "left" \| "right" \| "top"` | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
---
+++ packages/mint-components/src/components/sqm-qa-tabs/sqm-tabs-view.tsx
@@ -0,0 +1,63 @@
+import { useHost } from "@saasquatch/stencil-hooks";
+import { FunctionalComponent, h, Host } from "@stencil/core";
+import { TabElement } from "./useTabs";
+
+export type TabsViewProps = {
+  placement: "left" | "right" | "bottom" | "top";
+  content: {
+    tabs: TabElement[];
+  };
+};
+
+export const TabsView: FunctionalComponent<TabsViewProps> = ({
+  content,
+  placement,
+}) => {
+  // Vertical padding for top & bottom placement, horizontal for left & right.
+  const padding =
+    !placement || placement === "top" || placement === "bottom"
+      ? "var(--sl-spacing-x-large) 0;"
+      : "0 var(--sl-spacing-xx-large);";
+
+  const vanillaStyle = `
+    sl-tab-panel::part(base) {
+      padding: ${padding};
+    }
+  `;
+  const host = useHost();
+  return (
+    <Host>
+      <style type="text/css">{vanillaStyle}</style>
+      <sl-tab-group placement={placement}>
+        {content.tabs.map((tab: TabElement) => {
+          const slotName = tab.getAttribute("slot");
+          if (tab.tagName === "RAISINS-PLOP-TARGET") {
+            return (
+              <sl-tab slot="nav">
+                <div style={{ marginTop: "10px" }}>
+                  <slot name={slotName} />
+                </div>
+              </sl-tab>
+            );
+          }
+          return [
+            <sl-tab
+              slot="nav"
+              panel={slotName}
+              id={tab.getAttribute("id") || slotName}
+            >
+              {tab.getAttribute("header")}
+            </sl-tab>,
+            <sl-tab-panel name={slotName}>
+              <slot name={slotName} />
+            </sl-tab-panel>,
+          ];
+        })}
+      </sl-tab-group>
+
+      <div style={{ display: "none" }}>
+        <slot />
+      </div>
+    </Host>
+  );
+};
