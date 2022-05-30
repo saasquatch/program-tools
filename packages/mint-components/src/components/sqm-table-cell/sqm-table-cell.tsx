45cf81e0cfa119be05055c50361f15f7eb74462e
Derek Siemens <derekgsiemens@gmail.com>
2022-05-30 14:19:22 -0700

    DS: update sqm-table-row and table-cell

--- packages/mint-components/src/components/sqm-table-cell/sqm-table-cell.tsx
+++ packages/mint-components/src/components/sqm-table-cell/sqm-table-cell.tsx
@@ -1,11 +1,17 @@
 import { Component, h, Prop, Host } from "@stencil/core";
 
+/**
+ * @uiName Table Cell
+ * @slots [{"name":"", "title":"Cell Content"}]
+ */
 @Component({
   tag: "sqm-table-cell",
   shadow: true,
 })
 export class TableCell {
+  /** @uiName Column Span  */
   @Prop() colspan: number = 1;
+  /** @uiName Padding} */
   @Prop() padding: string = "15px 10px";
   render() {
     return (
