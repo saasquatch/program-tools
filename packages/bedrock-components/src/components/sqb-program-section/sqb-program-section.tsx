7b1dec887964250f7a4355501cb9e80299019f40
Johan Venter <johan@saasquatch.com>
2022-07-20 09:36:47 -0700

    Set up context provider in sqb-program-section constructor

--- packages/bedrock-components/src/components/sqb-program-section/sqb-program-section.tsx
+++ packages/bedrock-components/src/components/sqb-program-section/sqb-program-section.tsx
@@ -1,6 +1,12 @@
 import { withHooks } from '@saasquatch/stencil-hooks';
-import { Component, Host, h, Prop, State } from '@stencil/core';
-import { useProgramProvider } from './PROGRAM_CONTEXT';
+import { Component, Host, h, Prop, State, Element } from '@stencil/core';
+import { ContextProvider } from 'dom-context';
+import { useEffect } from '@saasquatch/universal-hooks';
+
+/**
+ * Matches @saasquatch/component-environment
+ */
+const PROGRAM_CONTEXT = 'sq:program-id';
 
 /**
  * Use this with other components like share buttons, referral lists
@@ -26,15 +32,30 @@ export class SqbProgramSection {
   @Prop()
   programId: string;
 
+  @Element() el: HTMLElement;
+  provider: ContextProvider<string | undefined>;
+
   constructor() {
+    this.provider = new ContextProvider({
+      contextName: PROGRAM_CONTEXT,
+      element: this.el,
+      initialState: this.programId,
+    });
+    this.provider.start();
+    this.el.addEventListener('sq:update-program-id', (e: CustomEvent) => (this.provider.context = e.detail));
     withHooks(this);
   }
 
   disconnectedCallback() {}
 
   render() {
-    const { programId } = this;
-    useProgramProvider(programId);
+    const { programId, provider } = this;
+
+    useEffect(() => {
+      if (provider.context !== programId) {
+        provider.context = programId;
+      }
+    }, [programId]);
 
     return (
       <Host style={{ display: 'contents' }}>
