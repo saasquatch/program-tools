7ad3405de1cd3e39ab7c009922077ff2ca98b356
ian hitchcock <ian.hitchcock@referralsaasquatch.com>
2022-08-19 13:48:35 -0700

    IH: back to tests

--- packages/jsonata-paths-extractor/test/paths.test.ts
+++ packages/jsonata-paths-extractor/test/paths.test.ts
@@ -1,46 +1,38 @@
-// import { loadFeature, autoBindSteps, StepDefinitions } from "jest-cucumber";
-// import extractJSONataPaths from "../src";
-// import regressionTests from "./tests.json";
+import { loadFeature, autoBindSteps, StepDefinitions } from "jest-cucumber";
+import extractJSONataPaths from "../src";
+import jsonataSuiteTests from "./tests.json";
+import programTests from "./programTests.json";
 
-// const feature = loadFeature("test/paths.feature");
+const feature = loadFeature("test/paths.feature");
 
-// const stepDefinitions: StepDefinitions[] = [
-//   ({ given, when, then }) => {
-//     let currentExpression: string | null;
-//     let currentResult: string[] = [];
-//     beforeEach(() => {
-//       currentExpression = null;
-//       currentResult = [];
-//     });
-//     given(/^the JSONata expression "(.*)"$/, expression => {
-//       try {
-//         currentExpression = expression;
-//       } catch (e) {}
-//     });
+const stepDefinitions: StepDefinitions[] = [
+  ({ given, when, then }) => {
+    let currentExpression: string | null;
+    let currentResult: string[] = [];
+    beforeEach(() => {
+      currentExpression = null;
+      currentResult = [];
+    });
+    given(/^the JSONata expression "(.*)"$/, expression => {
+      try {
+        currentExpression = expression;
+      } catch (e) {}
+    });
 
-//     when("the paths are extracted", () => {
-//       currentResult = extractJSONataPaths(currentExpression as string);
-//     });
+    when("the paths are extracted", () => {
+      currentResult = extractJSONataPaths(currentExpression as string);
+    });
 
-//     then(/^the result is (.*)$/, result => {
-//       expect(currentResult).toStrictEqual(JSON.parse(result));
-//     });
-//   }
-// ];
+    then(/^the result is (.*)$/, result => {
+      expect(currentResult).toStrictEqual(JSON.parse(result));
+    });
+  }
+];
 
-// let count = 0;
-// for (const test of regressionTests) {
-//   if (count < 100)
-//     // && test.input === "$[].content.origin.$lowercase(name)")
-//     it(`${test.input}\nOutput:${JSON.stringify(
-//       test.expected,
-//       null,
-//       2
-//     )}`, () => {
-//       // console.log(JSON.stringify(extractJSONataPaths(test.input)));
-//       expect(extractJSONataPaths(test.input)).toStrictEqual(test.expected);
-//     });
-//   count++;
-// }
+for (const test of jsonataSuiteTests.concat(programTests)) {
+  it(`${test.input}\nOutput:${JSON.stringify(test.expected, null, 2)}`, () => {
+    expect(extractJSONataPaths(test.input)).toStrictEqual(test.expected);
+  });
+}
 
-// autoBindSteps([feature], stepDefinitions);
+autoBindSteps([feature], stepDefinitions);
