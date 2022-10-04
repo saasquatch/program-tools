1dc3017976626ae0b8b79528b832c628a1d67a78
Jayden Chan <jaydencn7@gmail.com>
2022-10-04 11:56:08 -0700

    fix: properly export isLoggerInitialized

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -14,6 +14,7 @@ import {
   getLogger,
   info,
   initializeLogger,
+  isLoggerInitialized,
   log,
   LOG_TYPE_MARKER,
   warn,
@@ -36,6 +37,7 @@ export {
   httpLogMiddleware,
   info,
   initializeLogger,
+  isLoggerInitialized,
   log,
   warn,
 };
