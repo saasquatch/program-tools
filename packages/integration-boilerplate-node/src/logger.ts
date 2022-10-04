c3a0ee24950532d6797394294781c53abae51356
Jayden Chan <jaydencn7@gmail.com>
2022-10-04 11:58:10 -0700

    fix: fix logging config in integration-boilerplate

--- packages/integration-boilerplate-node/src/logger.ts
+++ packages/integration-boilerplate-node/src/logger.ts
@@ -4,13 +4,23 @@ import {
   LOG_LEVELS,
   Transport,
 } from "@saasquatch/logger";
+import { getLogger, isLoggerInitialized } from "@saasquatch/logger";
 import winston from "winston";
 import { BaseConfig } from "./config";
+
+const LOGGER_NAME = "integration-boilerplate";
 
 export function createLogger<Config extends BaseConfig>(
   config: Config
 ): winston.Logger {
-  const transports: Transport[] = [{ type: "console" }];
+  if (isLoggerInitialized(LOGGER_NAME)) {
+    return getLogger(LOGGER_NAME);
+  }
+
+  const transports: Transport[] = [
+    { type: "console", options: { silent: config.serverLogLevel === "none" } },
+  ];
+
   if (config.serverLogFile !== "") {
     transports.push({
       type: "file",
@@ -20,13 +30,16 @@ export function createLogger<Config extends BaseConfig>(
     });
   }
 
-  if (!LOG_LEVELS.includes(config.serverLogLevel as LogLevel)) {
+  if (
+    config.serverLogLevel !== "none" &&
+    !LOG_LEVELS.includes(config.serverLogLevel as LogLevel)
+  ) {
     throw new Error(
       `Invalid value for server log level: "${config.serverLogLevel}"`
     );
   }
 
-  const logger = initializeLogger("integration-boilerplate", {
+  const logger = initializeLogger(LOGGER_NAME, {
     logLevel: config.serverLogLevel as LogLevel,
     transports,
   });
