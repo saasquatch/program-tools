f5b4f6ef312ce6ecb76a24e5ebf9ceca1f7c8b49
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2022-10-07 16:22:18 -0700

    remove demo user from hook

--- packages/paypal-components/src/components/sqp-account-details/useAccountDetails.ts
+++ packages/paypal-components/src/components/sqp-account-details/useAccountDetails.ts
@@ -32,19 +32,6 @@ const SUBMIT_ACCOUNT = gql`
 `;
 
 export function useAccountDetails(props) {
-  // hacking in a user for ez development
-  useEffect(() => {
-    setUserIdentity({
-      jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2MzQwNmY4MWUzMWUzOTM2ZjkwOTQxZDAiLCJpYXQiOjE2NjUxNjcyMzMsImV4cCI6MTY2NTI1MzYzMywic3ViIjoiYzJGdEt6SXdRSE5oWVhOeGRXRjBMbU5vOmMyRnRLekl3UUhOaFlYTnhkV0YwTG1Ob0B0ZXN0X2E4YjQxam90ZjhhMXY6dXNlcnMiLCJ1c2VyIjp7ImlkIjoic2FtKzIwQHNhYXNxdWF0LmNoIiwiYWNjb3VudElkIjoic2FtKzIwQHNhYXNxdWF0LmNoIiwiZGF0ZUJsb2NrZWQiOm51bGx9fQ.ODn5NEs5iU2WpzarxLycTy0J2eZoA0c9OwrloTGJ2wk",
-      id: "sam+20@saasquat.ch",
-      accountId: "sam+20@saasquat.ch",
-      managedIdentity: {
-        email: "sam+20@saasquat.ch",
-        emailVerified: true,
-        sessionData: {},
-      },
-    });
-  }, []);
   const formRef = useRef<HTMLFormElement>(null);
   const [error, setError] = useState("");
   const [open, setOpen] = useState(false);
