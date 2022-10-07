c6fd50e77513032bcf2df06d73f24fac0c1b1bb9
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2022-10-07 15:00:20 -0700

    fix issues and deleted random incorrect prettier config

--- packages/paypal-components/src/components/sqp-account-details/sqp-account-details.tsx
+++ packages/paypal-components/src/components/sqp-account-details/sqp-account-details.tsx
@@ -1,13 +1,16 @@
-import { isDemo } from '@saasquatch/component-boilerplate';
-import { withHooks } from '@saasquatch/stencil-hooks';
-import { useState } from '@saasquatch/universal-hooks';
-import { Component, h, Host, Prop, State } from '@stencil/core';
-import { getProps } from '../../utils/utils';
-import { AccountDetailsView, AccountDetailsViewProps } from './sqp-account-details-view';
-import { AccountFormView, AccountFormViewProps } from './sqp-account-form-view';
-import { useAccountDetails } from './useAccountDetails';
-import deepmerge from 'deepmerge';
-import { DemoData } from '../../global/demo';
+import { isDemo } from "@saasquatch/component-boilerplate";
+import { withHooks } from "@saasquatch/stencil-hooks";
+import { useState } from "@saasquatch/universal-hooks";
+import { Component, h, Host, Prop, State } from "@stencil/core";
+import { getProps } from "../../utils/utils";
+import {
+  AccountDetailsView,
+  AccountDetailsViewProps,
+} from "./sqp-account-details-view";
+import { AccountFormView, AccountFormViewProps } from "./sqp-account-form-view";
+import { useAccountDetails } from "./useAccountDetails";
+import deepmerge from "deepmerge";
+import { DemoData } from "../../global/demo";
 /**
  * @uiName Paypal Account Details
  * @slots [{"name":"","title":"Form Slot"}]
@@ -15,7 +18,7 @@ import { DemoData } from '../../global/demo';
  * @example Paypal Account Details - <sqp-account-details header-text="PayPal account" account-label="Account" recent-payment-label="Recent payment" next-payment-label="Next payment" edit-text="Edit" modal-connect-pay-pal-account-header="Connect PayPal account" cancel-text="Cancel" connect-pay-pal-account-button-text="Connect account" pay-pal-email-label="PayPal email" pay-pal-email-label-help-text="Enter the email used for your PayPal account." confirm-pay-pal-email-label="Confirm email" success-message="Success!" pay-pal-account-header-text="PayPal account" connect-pay-pal-description-text="Connect your PayPal account to automatically receive payments/cash rewards."></sqp-account-details>
  */
 @Component({
-  tag: 'sqp-account-details',
+  tag: "sqp-account-details",
   shadow: true,
 })
 export class PaypalAccountDetails {
@@ -26,72 +29,74 @@ export class PaypalAccountDetails {
    * @uiName Header Text
    * @uiGroup Details
    */
-  @Prop() headerText: string = 'PayPal account';
+  @Prop() headerText: string = "PayPal account";
   /**
    * @uiName Account Label
    * @uiGroup Details
    */
-  @Prop() accountLabel: string = 'Account';
+  @Prop() accountLabel: string = "Account";
   /**
    * @uiName Recent Payment Label
    * @uiGroup Details
    */
-  @Prop() recentPaymentLabel: string = 'Recent payment';
+  @Prop() recentPaymentLabel: string = "Recent payment";
   /**
    * @uiName Next Payment Label
    * @uiGroup Details
    */
-  @Prop() nextPaymentLabel: string = 'Next payment';
+  @Prop() nextPaymentLabel: string = "Next payment";
   /**
    * @uiName Edit Text
    * @uiGroup Details
    */
-  @Prop() editText: string = 'Edit';
+  @Prop() editText: string = "Edit";
   /**
    * @uiName Modal Header
    * @uiGroup Form
    */
-  @Prop() modalConnectPayPalAccountHeader: string = 'Connect PayPal account';
+  @Prop() modalConnectPayPalAccountHeader: string = "Connect PayPal account";
   /**
    * @uiName Cancel Text
    * @uiGroup Form
    */
-  @Prop() cancelText: string = 'Cancel';
+  @Prop() cancelText: string = "Cancel";
   /**
    * @uiName Connect Button Text
    * @uiGroup Form
    */
-  @Prop() connectPayPalAccountButtonText: string = 'Connect account';
+  @Prop() connectPayPalAccountButtonText: string = "Connect account";
   /**
    * @uiName Paypal Email Label
    * @uiGroup Form
    */
-  @Prop() payPalEmailLabel: string = 'PayPal email';
+  @Prop() payPalEmailLabel: string = "PayPal email";
   /**
    * @uiName Email Help Text
    * @uiGroup Form
    */
-  @Prop() payPalEmailLabelHelpText: string = 'Enter the email used for your PayPal account.';
+  @Prop() payPalEmailLabelHelpText: string =
+    "Enter the email used for your PayPal account.";
   /**
    * @uiName Confirmation Email Text
    * @uiGroup Form
    */
-  @Prop() confirmPayPalEmailLabel: string = 'Confirm email';
+  @Prop() confirmPayPalEmailLabel: string = "Confirm email";
   /**
    * @uiName Success Message
    * @uiGroup Form
    */
-  @Prop() successMessage: string = 'Success!';
+  @Prop() successMessage: string = "Success!";
   /**
    * @uiName Header Text
    * @uiGroup Form
    */
-  @Prop() payPalAccountHeaderText: string = 'PayPal account';
+  @Prop() payPalAccountHeaderText: string = "PayPal account";
   /**
    * @uiName Subheader Text
    * @uiGroup Form
    */
-  @Prop() connectPayPalDescriptionText: string = 'Connect your PayPal account to automatically receive payments/cash rewards.';
+  @Prop() connectPayPalDescriptionText: string =
+    "Connect your PayPal account to automatically receive payments/cash rewards.";
 
   /**
    * @undocumented
@@ -104,11 +109,19 @@ export class PaypalAccountDetails {
   }
   disconnectedCallback() {}
   render() {
-    const props = isDemo() ? useAccountDetailsDemo(getProps(this)) : useAccountDetails(getProps(this));
+    const props = isDemo()
+      ? useAccountDetailsDemo(getProps(this))
+      : useAccountDetails(getProps(this));
 
     return (
       <Host>
-        <AccountFormView formRef={props.formRef} states={props.states} callbacks={props.callbacks} formContent={props.formContent} hasAccount={props.hasAccount} />
+        <AccountFormView
+          formRef={props.formRef}
+          states={props.states}
+          callbacks={props.callbacks}
+          formContent={props.formContent}
+          hasAccount={props.hasAccount}
+        />
         <AccountDetailsView
           hasAccount={props.hasAccount}
           accountDetails={props.accountDetails}
@@ -128,7 +141,7 @@ function useAccountDetailsDemo(props: PaypalAccountDetails) {
       setupAccount: () => {},
       hasAccount: false,
       accountDetails: {
-        email: 'test@example.com',
+        email: "test@example.com",
         recentPayment: { amount: 10000, date: 12345678900 },
         nextPayment: {
           date: 12345678900,
@@ -161,6 +174,6 @@ function useAccountDetailsDemo(props: PaypalAccountDetails) {
       },
     },
     props.demoData || {},
-    { arrayMerge: (_, a) => a },
+    { arrayMerge: (_, a) => a }
   );
 }
