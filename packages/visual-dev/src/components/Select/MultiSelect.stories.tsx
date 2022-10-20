41e9a1649212d470f7b8d3a67023531fa30347a1
Zach Harrison <zharrison100@gmail.com>
2022-10-20 16:46:33 -0700

    Fix styles, stories & publish pre release

--- packages/visual-dev/src/components/Select/MultiSelect.stories.tsx
+++ packages/visual-dev/src/components/Select/MultiSelect.stories.tsx
@@ -8,7 +8,7 @@ export default {
   component: SelectView,
 };
 
-export function MultipleSelectExample() {
+export function MultipleSelect() {
   const books = [
     "To Kill a Mockingbird",
     "War and Peace",
@@ -30,8 +30,8 @@ export function MultipleSelectExample() {
     removeSelectedItem,
     selectedItems,
   } = useMultipleSelection({ initialSelectedItems: [books[0], books[1]] });
+
   const items = books.filter(getBooksFilter(selectedItems));
-  // const functional = useSelect({ items });
   const functional = useSelect({
     selectedItem: null,
     defaultHighlightedIndex: 0, // after selection, highlight the first item.
@@ -64,41 +64,147 @@ export function MultipleSelectExample() {
     },
   });
 
-  console.log("current selection", selectedItems);
+  const tagsSlot = (
+    <>
+      {selectedItems.map(function renderSelectedItem(
+        selectedItemForRender,
+        index
+      ) {
+        return (
+          <TagView
+            key={`selected-item-${index}`}
+            {...getSelectedItemProps({
+              selectedItem: selectedItemForRender,
+              index,
+            })}
+            onClick={(e) => {
+              e.stopPropagation();
+              removeSelectedItem(selectedItemForRender);
+            }}
+          >
+            {selectedItemForRender}
+          </TagView>
+        );
+      })}
+    </>
+  );
+
+  const props = {
+    getSelectedItemProps,
+    getDropdownProps,
+    addSelectedItem,
+    removeSelectedItem,
+    functional,
+    items,
+    tagsSlot,
+  };
 
   return (
-    <SelectView.ContainerView limitWidth={false}>
-      <SelectView.HandleView
-        tagsSlot={
-          <>
-            {selectedItems.map(function renderSelectedItem(
-              selectedItemForRender,
-              index
-            ) {
-              return (
-                <TagView
-                  key={`selected-item-${index}`}
-                  {...getSelectedItemProps({
-                    selectedItem: selectedItemForRender,
-                    index,
-                  })}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    removeSelectedItem(selectedItemForRender);
-                  }}
-                >
-                  {selectedItemForRender}
-                </TagView>
-              );
+    <SelectView.ContainerView>
+      <SelectView.HandleView {...props} />
+      <SelectView.ListView {...props} />
+    </SelectView.ContainerView>
+  );
+}
+
+export function MultipleSelectFullWidth() {
+  const books = [
+    "To Kill a Mockingbird",
+    "War and Peace",
+    "The Idiot",
+    "A Picture of Dorian Gray",
+    "1984",
+    "Pride and Prejudice",
+    "Meditations",
+  ];
+  function getBooksFilter(selectedItems: string[]) {
+    return function booksFilter(book: string) {
+      return selectedItems.indexOf(book) < 0;
+    };
+  }
+  const {
+    getSelectedItemProps,
+    getDropdownProps,
+    addSelectedItem,
+    removeSelectedItem,
+    selectedItems,
+  } = useMultipleSelection({ initialSelectedItems: [books[0], books[1]] });
+
+  const items = books.filter(getBooksFilter(selectedItems));
+  const functional = useSelect({
+    selectedItem: null,
+    defaultHighlightedIndex: 0, // after selection, highlight the first item.
+    items,
+    stateReducer: (state, actionAndChanges) => {
+      const { changes, type } = actionAndChanges;
+      switch (type) {
+        case useSelect.stateChangeTypes.MenuKeyDownEnter:
+        case useSelect.stateChangeTypes.MenuKeyDownSpaceButton:
+        case useSelect.stateChangeTypes.ItemClick:
+          return {
+            ...changes,
+            isOpen: true, // keep the menu open after selection.
+          };
+      }
+      return changes;
+    },
+    onStateChange: ({ type, selectedItem }) => {
+      switch (type) {
+        case useSelect.stateChangeTypes.MenuKeyDownEnter:
+        case useSelect.stateChangeTypes.MenuKeyDownSpaceButton:
+        case useSelect.stateChangeTypes.ItemClick:
+          if (selectedItem) {
+            addSelectedItem(selectedItem);
+          }
+          break;
+        default:
+          break;
+      }
+    },
+  });
+
+  const tagsSlot = (
+    <>
+      {selectedItems.map(function renderSelectedItem(
+        selectedItemForRender,
+        index
+      ) {
+        return (
+          <TagView
+            key={`selected-item-${index}`}
+            {...getSelectedItemProps({
+              selectedItem: selectedItemForRender,
+              index,
             })}
-          </>
-        }
-        functional={functional}
-        {...functional.getToggleButtonProps(
-          getDropdownProps({ preventKeyAction: functional.isOpen })
-        )}
-      />
-      <SelectView.ListView items={items} functional={functional} />
+            onClick={(e) => {
+              e.stopPropagation();
+              removeSelectedItem(selectedItemForRender);
+            }}
+          >
+            {selectedItemForRender}
+          </TagView>
+        );
+      })}
+    </>
+  );
+
+  const limitWidth = false;
+
+  const props = {
+    getSelectedItemProps,
+    getDropdownProps,
+    addSelectedItem,
+    removeSelectedItem,
+    functional,
+    items,
+    tagsSlot,
+    limitWidth,
+  };
+
+  return (
+    <SelectView.ContainerView {...props}>
+      <SelectView.HandleView {...props} />
+      <SelectView.ListView {...props} />
     </SelectView.ContainerView>
   );
 }
