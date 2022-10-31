0f64dcd96e7a8612cc5b7849e5648b47fab90ff4
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2022-10-31 16:01:10 -0700

    bundle paypal components together

--- packages/paypal-components/stencil.config.ts
+++ packages/paypal-components/stencil.config.ts
@@ -1,99 +1,106 @@
-import { Config } from '@stencil/core';
-import { sass } from '@stencil/sass';
-import createDocxGenerator from 'stencil-docx-docs';
-import alias from '@rollup/plugin-alias';
-import copy from 'rollup-plugin-copy';
-import css from 'rollup-plugin-css-only';
-import path from 'path';
-import { OutputTarget } from '@stencil/core/internal';
-import { grapesJsOutput } from '@saasquatch/stencil-grapes-plugin';
-import { ShoelaceComponents } from './shoelace-definitions';
-import { string } from 'rollup-plugin-string';
-import plugin from '@raisins/stencil-docs-target';
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
+import createDocxGenerator from "stencil-docx-docs";
+import alias from "@rollup/plugin-alias";
+import copy from "rollup-plugin-copy";
+import css from "rollup-plugin-css-only";
+import path from "path";
+import { OutputTarget } from "@stencil/core/internal";
+import { grapesJsOutput } from "@saasquatch/stencil-grapes-plugin";
+import { ShoelaceComponents } from "./shoelace-definitions";
+import { string } from "rollup-plugin-string";
+import plugin from "@raisins/stencil-docs-target";
 
-const useDocx: OutputTarget = {
-  type: 'docs-custom',
-  generator: createDocxGenerator({
-    outDir: 'docs',
-    textFont: 'Calibri',
-    excludeTags: ['undocumented'],
-    title: 'Mint Components',
-    author: 'SaaSquatch',
-  }),
-} as const;
 const useGrapesjs: OutputTarget = grapesJsOutput({
-  outDir: 'grapesjs',
+  outDir: "grapesjs",
   components: ShoelaceComponents,
 });
 
 export const config: Config = {
-  namespace: 'paypal-components',
+  namespace: "paypal-components",
   devServer: {
     // startupTimeout: 0,
   },
-  globalScript: 'src/global/global.ts',
-  globalStyle: 'src/global/global.css',
+  globalScript: "src/global/global.ts",
+  globalStyle: "src/global/global.css",
   buildEs5: true,
+  bundles: [
+    {
+      components: [
+        "sqp-status-column",
+        "sqp-status-cell",
+        "sqp-rewards-column",
+        "sqp-rewards-cell",
+        "sqp-account-details",
+      ],
+    },
+  ],
   outputTargets:
     //@ts-ignore
-    process.env.NODE_ENV === 'dev'
+    process.env.NODE_ENV === "dev"
       ? [
           {
-            type: 'dist',
+            type: "dist",
           },
           {
-            type: 'www',
+            type: "www",
             serviceWorker: null, // disable service workers
-            copy: [{ src: 'global/styles.ts' }],
+            copy: [{ src: "global/styles.ts" }],
           },
-          useDocx,
           useGrapesjs,
           plugin({
-            outDir: 'docs',
+            outDir: "docs",
           }),
         ]
       : [
           {
-            type: 'dist',
-            copy: [{ src: 'global/styles.ts' }],
+            type: "dist",
+            copy: [{ src: "global/styles.ts" }],
           },
           {
-            type: 'stats',
-            file: 'docs/stats.json', // optional
+            type: "stats",
+            file: "docs/stats.json", // optional
           },
           plugin({
-            outDir: 'docs',
+            outDir: "docs",
           }),
-          useDocx,
           useGrapesjs,
         ],
-  plugins: [sass({ injectGlobalPaths: ['src/global/mixins.scss'] }), string({ include: '**/*.feature' }), string({ include: '**/*.md' })],
+  plugins: [
+    sass({ injectGlobalPaths: ["src/global/mixins.scss"] }),
+    string({ include: "**/*.feature" }),
+    string({ include: "**/*.md" }),
+  ],
   rollupPlugins: {
     before: [
       alias({
         entries: [
           {
-            find: '@saasquatch/universal-hooks',
-            replacement: path.resolve(__dirname, 'node_modules', '@saasquatch/stencil-hooks'),
+            find: "@saasquatch/universal-hooks",
+            replacement: path.resolve(
+              __dirname,
+              "node_modules",
+              "@saasquatch/stencil-hooks"
+            ),
           },
         ],
       }),
       css({
-        output: 'bundle.css',
+        output: "bundle.css",
       }),
       copy({
         targets: [
           {
-            src: 'node_modules/@shoelace-style/shoelace/dist/assets',
-            dest: 'shoelace',
+            src: "node_modules/@shoelace-style/shoelace/dist/assets",
+            dest: "shoelace",
           },
           {
-            src: 'node_modules/@shoelace-style/shoelace/dist/themes',
-            dest: 'shoelace/themes',
+            src: "node_modules/@shoelace-style/shoelace/dist/themes",
+            dest: "shoelace/themes",
           },
         ],
       }),
-      string({ include: 'src/templates/*.html' }),
+      string({ include: "src/templates/*.html" }),
     ],
   },
   extras: {
