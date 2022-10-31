0fb5ab4c48882b9fea2e7e6de2a29c12b50d219c
Zach Harrison <zharrison100@gmail.com>
2022-10-31 15:45:17 -0700

    add missing story for multi select

--- packages/visual-dev/src/components/Select/MultiSelect.stories.tsx
+++ packages/visual-dev/src/components/Select/MultiSelect.stories.tsx
@@ -215,3 +215,130 @@ export function MultipleSelectFullWidth() {
     </SelectView.ContainerView>
   );
 }
+
+export function MultiSelectWithManyItems() {
+  const books = [
+    "To Kill a Mockingbird",
+    "War and Peace",
+    "The Idiot",
+    "A Picture of Dorian Gray",
+    "1984",
+    "Pride and Prejudice",
+    "Meditations",
+    "To Kill a Mockingbird",
+    "War and Peace",
+    "The Idiot",
+    "A Picture of Dorian Gray",
+    "1984",
+    "Pride and Prejudice",
+    "Meditations",
+    "To Kill a Mockingbird",
+    "War and Peace",
+    "The Idiot",
+    "A Picture of Dorian Gray",
+    "1984",
+    "Pride and Prejudice",
+    "Meditations",
+    "To Kill a Mockingbird",
+    "War and Peace",
+    "The Idiot",
+    "A Picture of Dorian Gray",
+    "1984",
+    "Pride and Prejudice",
+    "Meditations",
+  ];
+  function getBooksFilter(selectedItems: string[]) {
+    return function booksFilter(book: string) {
+      return selectedItems.indexOf(book) < 0;
+    };
+  }
+  const {
+    getSelectedItemProps,
+    getDropdownProps,
+    addSelectedItem,
+    removeSelectedItem,
+    selectedItems,
+  } = useMultipleSelection({ initialSelectedItems: [books[0], books[1]] });
+
+  const items = books.filter(getBooksFilter(selectedItems));
+  const functional = useSelect({
+    selectedItem: null,
+    defaultHighlightedIndex: 0, // after selection, highlight the first item.
+    items,
+    stateReducer: (state, actionAndChanges) => {
+      const { changes, type } = actionAndChanges;
+      switch (type) {
+        case useSelect.stateChangeTypes.MenuKeyDownEnter:
+        case useSelect.stateChangeTypes.MenuKeyDownSpaceButton:
+        case useSelect.stateChangeTypes.ItemClick:
+          return {
+            ...changes,
+            isOpen: true, // keep the menu open after selection.
+          };
+      }
+      return changes;
+    },
+    onStateChange: ({ type, selectedItem }) => {
+      switch (type) {
+        case useSelect.stateChangeTypes.MenuKeyDownEnter:
+        case useSelect.stateChangeTypes.MenuKeyDownSpaceButton:
+        case useSelect.stateChangeTypes.ItemClick:
+          if (selectedItem) {
+            addSelectedItem(selectedItem);
+          }
+          break;
+        default:
+          break;
+      }
+    },
+  });
+
+  const tagsSlot = (
+    <>
+      {selectedItems.map(function renderSelectedItem(
+        selectedItemForRender,
+        index
+      ) {
+        return (
+          <TagView
+            key={`selected-item-${index}`}
+            {...getSelectedItemProps({
+              selectedItem: selectedItemForRender,
+              index,
+            })}
+            onClick={(e) => {
+              e.stopPropagation();
+              removeSelectedItem(selectedItemForRender);
+            }}
+          >
+            {selectedItemForRender}
+          </TagView>
+        );
+      })}
+    </>
+  );
+
+  const limitWidth = false;
+
+  const props = {
+    getSelectedItemProps,
+    getDropdownProps,
+    addSelectedItem,
+    removeSelectedItem,
+    functional,
+    items,
+    tagsSlot,
+    limitWidth,
+  };
+
+  return (
+    <SelectView.ContainerView {...props}>
+      <SelectView.HandleView {...props} />
+      <SelectView.ListView
+        limitHeight={true}
+        {...props}
+        empty={!items.length}
+      />
+    </SelectView.ContainerView>
+  );
+}
