c6187f6e9e6a00e263b2c561370fa8d5d6591993
Noah Clarke <noah.clarke@referralsaasquatch.com>
2022-12-06 17:19:28 -0800

    sample data shape for grouped select

---
+++ packages/visual-dev/src/components/Select/groupedDataShape.ts
@@ -0,0 +1,14 @@
+const groupedItems = {
+    group: "Test title",
+    items:[
+        {...item1Object}
+        {...item2Object}
+        {...item3Object}
+    ]
+}
+
+items:[
+    {...item1Object}
+    {...item2Object}
+    {...item3Object}
+]
