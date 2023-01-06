df51952521121c6c406ca292c19aff066d5c6f6c
Noah Clarke <noah.clarke@referralsaasquatch.com>
2023-01-06 15:27:05 -0800

    remove extra package files

--- packages/visual-dev/package 2/src/components/List/Styles.ts
+++
@@ -1,9 +0,0 @@
-import { css } from 'styled-components'
-
-export const ListStyles = css`
-  color: var(--sq-text);
-  font-family: "Helvetica Neue", Helvetica, sans-serif;
-  font-weight: var(--sq-font-weight-regular);
-  font-size: var(--sq-font-size-regular);
-  line-height: var(--sq-line-height-regular);
-`
--- packages/visual-dev/package 2/src/components/Paragraph/Styles.ts
+++
@@ -1,5 +0,0 @@
-import { css } from 'styled-components'
-
-export const base = css`
-  line-height: 20px;
-`
