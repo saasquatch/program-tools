03fb0776258c6e6c8ea7e7788fd538440ba7a00d
Noah Clarke <noah.clarke@referralsaasquatch.com>
2023-01-18 11:46:15 -0800

    fix cursor and disabled radio card

--- packages/visual-dev/src/components/RadioCard/RadioCard.tsx
+++ packages/visual-dev/src/components/RadioCard/RadioCard.tsx
@@ -58,6 +58,7 @@ const RadioLabel = styled.label<RabelLabelProps>`
     ${(props) =>
     props.disabled &&
     css`
+      cursor: default;
       background-color: var(--sq-border);
       & * {
         color: var(--sq-text-subdued);
