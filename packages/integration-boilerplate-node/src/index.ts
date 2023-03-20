b616c7b7e6dbe09009d23f843149a84c2fdb63ba
Jayden Chan <jayden@jayden.codes>
2023-03-20 11:24:04 -0700

    feat: export auth module

--- packages/integration-boilerplate-node/src/index.ts
+++ packages/integration-boilerplate-node/src/index.ts
@@ -1,7 +1,8 @@
 // Convenient re-export for the noop gql tag
 export { default as gql } from "noop-tag";
 
-export * from "./types";
+export * from "./auth";
 export * from "./config";
-export * from "./service";
 export * from "./metrics";
+export * from "./service";
+export * from "./types";
