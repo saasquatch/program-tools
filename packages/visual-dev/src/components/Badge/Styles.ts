98d6b4fbb8c29529abe62d18fa641ceade41f451
Zach Harrison <zharrison100@gmail.com>
2023-04-11 13:31:43 -0700

    Update badge styling & add new css vars

--- packages/visual-dev/src/components/Badge/Styles.ts
+++ packages/visual-dev/src/components/Badge/Styles.ts
@@ -1,7 +1,7 @@
 import { css } from "styled-components";
 
 export const base = css`
-  padding: var(--sq-spacing-xxx-small) var(--sq-spacing-medium);
+  padding: var(--sq-spacing-x-small) var(--sq-spacing-small);
   color: var(--sq-text);
   font-family: "Helvetica Neue", Helvetica, sans-serif;
   font-weight: var(--sq-font-weight-bold);
@@ -17,31 +17,37 @@ export const pill_true = css`
   border-radius: var(--sq-border-radius-pill);
 `;
 
-export const info = css`
+export const default_style = css`
   background-color: var(--sq-border);
-  border-color: var(--sq-border);
+  border: 1px solid var(--sq-text);
   color: var(--sq-text);
 `;
 
 export const success = css`
-  background-color: var(--sq-surface-success);
-  border-color: var(--sq-border-success);
-  color: var(--sq-surface);
+  background-color: var(--sq-surface-success-subdued);
+  border: 1px solid var(--sq-badge-text-success);
+  color: var(--sq-badge-text-success);
 `;
 
 export const critical = css`
-  background-color: var(--sq-surface-critical);
-  border-color: var(--sq-surface-critical);
-  color: var(--sq-surface);
+  background-color: var(--sq-surface-critical-subdued);
+  border: 1px solid var(--sq-badge-text-critical);
+  color: var(--sq-badge-text-critical);
 `;
 
 export const warning = css`
-  background-color: var(--sq-badge-surface-warning);
-  border-color: var(--sq-badge-surface-warning);
-  color: var(--sq-text);
+  background-color: var(--sq-surface-warning-subdued);
+  border: 1px solid var(--sq-surface-warning);
+  color: var(--sq-badge-text-warning);
+`;
+
+export const info = css`
+  background-color: var(--sq-badge-surface-info);
+  border: 1px solid var(--sq-border-info);
+  color: var(--sq-badge-text-info);
 `;
 
 export const small = css`
   padding: var(--sq-spacing-xxx-small) var(--sq-spacing-xx-small);
   font-weight: var(--sq-font-weight-regular);
-`
+`;
