6ff2c738850bb2a641317ee16f2462ff04da68c5
Truman <truman.gao@referralsaasquatch.com>
2023-04-20 10:01:09 -0700

    update x to use svg

--- packages/mint-components/src/components/sqm-close-button/sqm-close-button-view.tsx
+++ packages/mint-components/src/components/sqm-close-button/sqm-close-button-view.tsx
@@ -27,7 +27,18 @@ export function CloseButtonView(props: CloseButtonViewProps) {
     <div>
       <style type="text/css">{styleString}</style>
       <span class={sheet.classes.icon} onClick={onClick}>
-        ×
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 56 56"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M56 5.64L50.36 0L28 22.36L5.64 0L0 5.64L22.36 28L0 50.36L5.64 56L28 33.64L50.36 56L56 50.36L33.64 28L56 5.64Z"
+            fill="black"
+          />
+        </svg>
       </span>
     </div>
   );
