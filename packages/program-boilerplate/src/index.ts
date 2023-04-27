149307833b798b1189ec1549cc58f0d9420ad92c
Jayden Chan <jayden@jayden.codes>
2023-04-27 10:46:28 -0700

    chore: remove "enforce https" middleware

--- packages/program-boilerplate/src/index.ts
+++ packages/program-boilerplate/src/index.ts
@@ -81,24 +81,28 @@ export function webtask(program: Program = {}): express.Application {
   app.use(compression());
   app.use(httpLogMiddleware(logger));
 
-  // Enforce HTTPS. The server does not redirect http -> https
-  // because OWASP advises not to
-  app.use((req, res, next) => {
-    if (
-      process.env.NODE_ENV === "production" &&
-      req.header("X-Forwarded-Proto") !== "https"
-    ) {
-      return res.status(403).send({ message: "SSL required" });
-    }
+  app.get("/*", (_req, res) => {
+    return res.status(200).contentType("text/html;charset=utf8").send(`
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8"/>
 
-    // allow the request to continue if https is used
-    next();
+          <title>Program Boilerplate</title>
+          <meta name="author" content="SaaSquatch"/>
+        </head>
+
+        <body>
+          <h1>
+            Hello World
+          </h1>
+        </body>
+      </html>`);
   });
 
-  app.post("/*", (context, res) => {
-    const { json, code } = triggerProgram(context.body, program);
-
-    res.status(code).json(json);
+  app.post("/*", (req, res) => {
+    const { json, code } = triggerProgram(req.body, program);
+    return res.status(code).json(json);
   });
 
   return app;
