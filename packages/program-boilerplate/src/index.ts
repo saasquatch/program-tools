bb639318b7aef9f08387c7e075190ab3a15e2f31
Jayden Chan <jayden@jayden.codes>
2023-05-11 16:46:42 -0700

    feat: Kubernetes health check

--- packages/program-boilerplate/src/index.ts
+++ packages/program-boilerplate/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Response, Request } from "express";
 
 import {
   meetCustomFieldRules,
@@ -80,6 +80,27 @@ export function webtask(program: Program = {}): express.Application {
   app.use(express.json({ limit: process.env.MAX_PAYLOAD_SIZE || "1mb" }));
   app.use(compression());
   app.use(httpLogMiddleware(logger));
+
+  // Enforce HTTPS. The server does not redirect http -> https
+  // because OWASP advises not to
+  app.use((req, res, next) => {
+    if (
+      process.env.NODE_ENV === "production" &&
+      req.header("X-Forwarded-Proto") !== "https"
+    ) {
+      return res.status(403).send({ message: "SSL required" });
+    }
+
+    // allow the request to continue if https is used
+    next();
+  });
+
+  const healthCheck = (_req: Request, res: Response) =>
+    res.status(200).json({ status: "OK" });
+
+  app.get("/healthz", healthCheck);
+  app.get("/livez", healthCheck);
+  app.get("/readyz", healthCheck);
 
   app.post("/*", (req, res) => {
     const { json, code } = triggerProgram(req.body, program);
