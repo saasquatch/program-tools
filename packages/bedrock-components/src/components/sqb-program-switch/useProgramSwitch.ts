55dc0a188f9d06a44fb2bd89016bcd97763f53bc
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2023-05-15 13:19:05 -0700

    cleanup

--- packages/bedrock-components/src/components/sqb-program-switch/useProgramSwitch.ts
+++ packages/bedrock-components/src/components/sqb-program-switch/useProgramSwitch.ts
@@ -1,5 +1,5 @@
 import { useProgramId } from '@saasquatch/component-boilerplate';
-import { useEffect, useState } from '@saasquatch/universal-hooks';
+import { useCallback, useEffect, useState } from '@saasquatch/universal-hooks';
 import debugFn from 'debug';
 import { useTemplateChildren } from '../../utils/useTemplateChildren';
 import { useChildElements } from '../../utils/useChildElements';
@@ -16,54 +16,54 @@ export function useProgramSwitch() {
   const [container, setContainer] = useState<HTMLDivElement>(undefined);
   const templates = useChildElements<HTMLTemplateElement>();
 
+  const updateTemplates = useCallback(() => {
+    // <template>
+    const templates = slot.querySelectorAll<HTMLTemplateElement & ProgramTemplate>(`template`);
+    const templatesArray = Array.from(templates);
+
+    const template = templatesArray.find(template => {
+      const templateProgramId = template.attributes?.['program-id']?.nodeValue;
+      if (programId === templateProgramId) return template;
+    });
+
+    //@ts-ignore - can't access template attributes directly
+    const templateProgramId = template?.attributes?.['program-id']?.nodeValue;
+
+    debug({
+      template,
+      containerDatasetProgramId: container.dataset['program-id'],
+      templateProgramId,
+      programId,
+    });
+
+    // if no routes found, and the old route doesn't match the new route
+    if (!template) {
+      // No matching page, display nothing
+      debug('No matching programId Template found for ', programId, ', so render nothing');
+      container.innerHTML = '';
+      container.dataset.programId = programId;
+      return;
+    }
+
+    debug('Program selection updated to ', programId, template);
+    // use outerHTML if template's innerHTML is unset (only happens in Stencilbook)
+    const newContent = template.innerHTML || template.firstElementChild.outerHTML;
+
+    // if template contents are an exact match
+    if (newContent === container.innerHTML) {
+      debug("don't rerender");
+      // Same page, do not re-render
+      // Reduces dom mutations, speeds up page speed
+    } else if (template) {
+      container.innerHTML = newContent;
+      container.dataset.programId = templateProgramId;
+    }
+  }, [container, slot]);
+
   useEffect(() => {
     if (!container || !slot) {
       debug('DOM not ready for program rendering on:', programId);
       return;
-    }
-
-    function updateTemplates() {
-      // <template>
-      const templates = slot.querySelectorAll<HTMLTemplateElement & ProgramTemplate>(`template`);
-      const templatesArray = Array.from(templates);
-
-      const template = templatesArray.find(template => {
-        const templateProgramId = template.attributes?.['program-id']?.nodeValue;
-        if (programId === templateProgramId) return template;
-      });
-
-      //@ts-ignore - can't access template attributes directly
-      const templateProgramId = template?.attributes?.['program-id']?.nodeValue;
-
-      debug({
-        template,
-        containerDatasetProgramId: container.dataset['program-id'],
-        templateProgramId,
-        programId,
-      });
-
-      // if no routes found, and the old route doesn't match the new route
-      if (!template) {
-        // No matching page, display nothing
-        debug('No matching programId Template found for ', programId, ', so render nothing');
-        container.innerHTML = '';
-        container.dataset.programId = programId;
-        return;
-      }
-
-      debug('Program selection updated to ', programId, template);
-      // use outerHTML if template's innerHTML is unset (only happens in Stencilbook)
-      const newContent = template.innerHTML || template.firstElementChild.outerHTML;
-
-      // if template contents are an exact match
-      if (newContent === container.innerHTML) {
-        debug("don't rerender");
-        // Same page, do not re-render
-        // Reduces dom mutations, speeds up page speed
-      } else if (template) {
-        container.innerHTML = newContent;
-        container.dataset.programId = templateProgramId;
-      }
     }
 
     updateTemplates();
