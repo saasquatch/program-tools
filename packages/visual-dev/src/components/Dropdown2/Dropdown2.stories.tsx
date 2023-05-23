eff2928b9f423d6328b48f3e6c65d51e06cf3586
Truman <truman.gao@referralsaasquatch.com>
2023-05-23 16:14:24 -0700

    add stories

--- packages/visual-dev/src/components/Dropdown2/Dropdown2.stories.tsx
+++ packages/visual-dev/src/components/Dropdown2/Dropdown2.stories.tsx
@@ -48,3 +48,220 @@ export const Functional = () => {
     </div>
   );
 };
+
+export const FunctionalUpwards = () => {
+  const [enabled, setEnable] = useState(true);
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={enabled}
+        handleSlot={
+          <Dropdown2View.HandleView onClickDropdown={() => setEnable(!enabled)}>
+            Test handle
+          </Dropdown2View.HandleView>
+        }
+      >
+        {defaultItems}
+      </Dropdown2View>
+    </div>
+  );
+};
+
+export const FunctionalIcon = () => {
+  const [enabled, setEnable] = useState(true);
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={enabled}
+        handleSlot={
+          <Dropdown2View.HandleView
+            icon="calendar"
+            onClickDropdown={() => setEnable(!enabled)}
+          >
+            Test handle
+          </Dropdown2View.HandleView>
+        }
+      >
+        {defaultItems}
+      </Dropdown2View>
+    </div>
+  );
+};
+
+export const DefaultClosed = () => {
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={false}
+        handleSlot={
+          <Dropdown2View.HandleView onClickDropdown={exampleOnClick}>
+            Test handle
+          </Dropdown2View.HandleView>
+        }
+      >
+        {defaultItems}
+      </Dropdown2View>
+    </div>
+  );
+};
+
+export const DefaultOpened = () => {
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={true}
+        handleSlot={
+          <Dropdown2View.HandleView onClickDropdown={exampleOnClick}>
+            Test handle
+          </Dropdown2View.HandleView>
+        }
+      >
+        {defaultItems}
+      </Dropdown2View>
+    </div>
+  );
+};
+
+export const Disabled = () => {
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={false}
+        disabled
+        handleSlot={
+          <Dropdown2View.HandleView onClickDropdown={exampleOnClick}>
+            Test handle
+          </Dropdown2View.HandleView>
+        }
+      >
+        {defaultItems}
+      </Dropdown2View>
+    </div>
+  );
+};
+
+// todo - make story
+export const Description = () => {
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={true}
+        handleSlot={
+          <Dropdown2View.HandleView onClickDropdown={exampleOnClick}>
+            Test handle
+          </Dropdown2View.HandleView>
+        }
+      >
+        {defaultItems}
+      </Dropdown2View>
+    </div>
+  );
+};
+
+// todo - make story
+export const SideDescription = () => {
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={true}
+        handleSlot={
+          <Dropdown2View.HandleView onClickDropdown={exampleOnClick}>
+            Test handle
+          </Dropdown2View.HandleView>
+        }
+      >
+        {defaultItems}
+      </Dropdown2View>
+    </div>
+  );
+};
+
+// todo - make story
+export const BothDescriptions = () => {
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={true}
+        handleSlot={
+          <Dropdown2View.HandleView onClickDropdown={exampleOnClick}>
+            Test handle
+          </Dropdown2View.HandleView>
+        }
+      >
+        {defaultItems}
+      </Dropdown2View>
+    </div>
+  );
+};
+
+// todo - make story
+export const Checked = () => {
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={true}
+        handleSlot={
+          <Dropdown2View.HandleView onClickDropdown={exampleOnClick}>
+            Test handle
+          </Dropdown2View.HandleView>
+        }
+      >
+        {defaultItems}
+      </Dropdown2View>
+    </div>
+  );
+};
+
+export const PlaceholderWithoutText = () => {
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={false}
+        handleSlot={
+          <Dropdown2View.HandleView
+            onClickDropdown={exampleOnClick}
+            placeholder="Test placeholder"
+          ></Dropdown2View.HandleView>
+        }
+      >
+        {defaultItems}
+      </Dropdown2View>
+    </div>
+  );
+};
+export const PlaceholderWithText = () => {
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={false}
+        handleSlot={
+          <Dropdown2View.HandleView
+            onClickDropdown={exampleOnClick}
+            placeholder="Test placeholder"
+          >
+            Menu options
+          </Dropdown2View.HandleView>
+        }
+      >
+        {defaultItems}
+      </Dropdown2View>
+    </div>
+  );
+};
+export const NoItems = () => {
+  return (
+    <div style={{ height: 280 }}>
+      <Dropdown2View
+        showMenu={true}
+        handleSlot={
+          <Dropdown2View.HandleView
+            onClickDropdown={exampleOnClick}
+            placeholder="Test placeholder"
+          >
+            Menu options
+          </Dropdown2View.HandleView>
+        }
+      ></Dropdown2View>
+    </div>
+  );
+};
