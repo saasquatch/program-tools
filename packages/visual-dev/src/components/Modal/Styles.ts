535e2c4979a2ef8d4f4b065dbb60411d158837cb
Noah Clarke <noah.clarke@referralsaasquatch.com>
2023-05-25 15:19:14 -0700

    fix modal corner

--- packages/visual-dev/src/components/Modal/Styles.ts
+++ packages/visual-dev/src/components/Modal/Styles.ts
@@ -19,7 +19,6 @@ export const ModalDivStyle = css`
   min-width: 558px;
   background-color: var(--sq-surface);
   border-radius: 8px;
-  overflow: hidden;
   margin: auto;
   padding: 0;
   display: grid;
@@ -30,6 +29,8 @@ export const ModalHeaderStyle = css`
   user-select: none;
   padding: var(--sq-spacing-large);
   background: var(--sq-nav-surface-primary);
+  margin-top: -1px;
+  border-radius: 8px 8px 0px 0px;
   box-sizing: border-box;
   // border-radius: 8px 8px 0 0;
   font-family: var(--sq-font-family-sans);
