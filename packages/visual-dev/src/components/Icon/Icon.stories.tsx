1cb150ea766024f1406b4ead72a2d053660b4815
Truman <truman.gao@referralsaasquatch.com>
2023-07-12 16:19:28 -0700

    add icon

--- packages/visual-dev/src/components/Icon/Icon.stories.tsx
+++ packages/visual-dev/src/components/Icon/Icon.stories.tsx
@@ -197,3 +197,4 @@ export const checkmark_circle_filled = () => (
 export const globe = () => <IconView icon={"globe"} />;
 export const lock = () => <IconView icon={"lock"} />;
 export const unlock = () => <IconView icon={"unlock"} />;
+export const cookie = () => <IconView icon={"cookie"} />;
