2631304b71f37a22e6467be382e464b722ead9fa
Noah Clarke <noah.clarke@referralsaasquatch.com>
2023-09-25 16:40:56 -0700

    delete visual-dev

--- packages/visual-dev/src/components/Slider/Slider.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { SliderView } from "./Slider";
-
-export default {
-  title: "Components / Slider",
-  component: SliderView,
-};
-
-export const InputSlider = () => <SliderView />;
--- packages/visual-dev/src/utlis.ts
+++
@@ -1,4 +0,0 @@
-import { StyleProps as BadgeStyleProps } from "./components/Badge";
-
-export const dashToSnakeCase = (str: BadgeStyleProps["status"]) =>
-  str.replace("-", "_");
