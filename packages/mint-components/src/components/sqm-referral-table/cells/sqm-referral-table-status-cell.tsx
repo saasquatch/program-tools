d4a60e16eac59ae3b756fad16076682f8bc718dc
Zach Harrison <zharrison100@gmail.com>
2023-11-29 10:41:56 -0800

    style subtext

--- packages/mint-components/src/components/sqm-referral-table/cells/sqm-referral-table-status-cell.tsx
+++ packages/mint-components/src/components/sqm-referral-table/cells/sqm-referral-table-status-cell.tsx
@@ -9,6 +9,11 @@ const style = {
       whiteSpace: "pre-line",
     },
   },
+  SubText: {
+    fontSize: "var(--sl-font-size-small)",
+    color: "var(--sl-color-neutral-500)",
+    margin: "0",
+  },
 };
 
 const sheet = createStyleSheet(style);
@@ -54,7 +59,7 @@ export class ReferralTableStatusCell {
           {this.statusText}
         </sl-badge>
         {this.fraudStatus === "PENDING_REVIEW" ? (
-          <p style={{ margin: "0" }}>Awaiting approval</p>
+          <p class={sheet.classes.SubText}>Awaiting review</p>
         ) : null}
       </div>
     );
