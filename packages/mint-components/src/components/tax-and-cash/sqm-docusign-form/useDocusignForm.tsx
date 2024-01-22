c7eb038954abcc94ae26d49ffd848bd574fd5572
Locrian24 <locrian24@gmail.com>
2024-01-22 10:13:27 -0800

    useDocusignForm

--- packages/mint-components/src/components/tax-and-cash/sqm-docusign-form/useDocusignForm.tsx
+++ packages/mint-components/src/components/tax-and-cash/sqm-docusign-form/useDocusignForm.tsx
@@ -1,12 +1,42 @@
+import { gql } from "graphql-request";
 import { useParent } from "../../../utils/useParentState";
 import { TAX_CONTEXT_NAMESPACE } from "../sqm-tax-and-cash/useTaxAndCash";
 import { DocusignForm } from "./sqm-docusign-form";
+import { useQuery, useUserIdentity } from "@saasquatch/component-boilerplate";
+
+const GET_USER_TAX_INFO = gql`
+  query getUserTaxInfo($id: String!, $accountId: String!) {
+    id
+    accountId
+    firstName
+    lastName
+    email
+    countryCode
+  }
+`;
+
+const SUBMIT_TAX_INFO = gql`
+  mutation submitTaxInfo ($vars: SubmitTaxInfoInput) {
+
+  }
+`;
 
 export function useDocusignForm(props: DocusignForm) {
+  const user = useUserIdentity();
   const [step, setStep] = useParent<string>(TAX_CONTEXT_NAMESPACE);
+
+  const variables = {
+    id: user.id,
+    accountId: user.accountId,
+  };
+  const { data, loading } = useQuery(GET_USER_TAX_INFO, variables);
+
+  const countryCode = data?.getUserTaxInfo.countryCode;
+  const locationRadio = countryCode === "US" ? "US" : "OUTSIDE_US";
 
   return {
     step: step,
+    locationRadio,
     setStep: setStep,
     text: { ...props },
   };
