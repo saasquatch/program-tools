1f3937f6ba1431cdfdc6ac2c5c02664d53f19954
Locrian24 <locrian24@gmail.com>
2024-02-23 13:22:22 -0800

    fix objectIsFull

--- packages/mint-components/src/components/tax-and-cash/utils.ts
+++ packages/mint-components/src/components/tax-and-cash/utils.ts
@@ -1,7 +1,3 @@
 export const objectIsFull = (obj: Record<string, unknown>) => {
-  const hasUndefined = Object.keys(obj).reduce((prev, k) => {
-    return obj[k] == undefined;
-  }, false);
-
-  return !hasUndefined;
+  return !Object.keys(obj).find((k) => obj[k] == undefined);
 };
