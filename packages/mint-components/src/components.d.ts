2545efe9908736da58239feeb3e5e7fed34d9dca
Noah Clarke <noahwc@users.noreply.github.com>
2024-03-12 13:48:14 -0700

    fix column width

--- packages/mint-components/src/components.d.ts
+++ packages/mint-components/src/components.d.ts
@@ -1317,7 +1317,7 @@ export namespace Components {
          */
         "columnTitle": string;
         "renderCell": (_: Invoice) => Promise<any>;
-        "renderLabel": () => Promise<string>;
+        "renderLabel": () => Promise<any>;
     }
     interface SqmLeaderboard {
         /**
