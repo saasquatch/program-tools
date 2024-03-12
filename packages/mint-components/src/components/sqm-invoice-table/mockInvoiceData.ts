4c30b5b95fdae164090bf1edf764d5b402234527
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2024-03-12 13:58:32 -0700

    added randomized mocked invoice data

--- packages/mint-components/src/components/sqm-invoice-table/mockInvoiceData.ts
+++ packages/mint-components/src/components/sqm-invoice-table/mockInvoiceData.ts
@@ -8,20 +8,19 @@ export default (count = 4) => {
 const getMockData = (): Invoice => {
   const today = DateTime.now();
 
-  //  set random data
-  //   const randomUnitNumber = Math.floor(Math.random() * 3);
-  //   const randomStatus = status || statuses[Math.floor(Math.random() * 5)];
-  //   const unit = units[randomUnitNumber];
-  //   const prettyValue = prettyValues[randomUnitNumber];
-  //   const randomValue = Math.floor(Math.random() * 100) + 2;
+  const invoiceId = Math.random().toString(36).slice(2, 12);
+
+  const earnings = Math.floor(Math.random() * 100) + 1;
+
+  const taxedAmount = Math.round(earnings * 0.2);
 
   return {
     downloadURL: "https://example.com",
     dateCreated: today.toMillis(),
-    invoiceId: "asdfg",
+    invoiceId,
     program: "my-program",
-    earnings: "$20.00",
-    taxedAmount: "$2.00",
-    netEarnings: "$18.00",
+    earnings: `$${earnings}.00`,
+    taxedAmount: `$${taxedAmount}.00`,
+    netEarnings: `$${earnings - taxedAmount}.00`,
   };
 };
