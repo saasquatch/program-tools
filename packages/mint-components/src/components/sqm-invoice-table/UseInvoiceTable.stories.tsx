8e0e55593bb9d1758a389176421f80214657f805
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2024-03-12 14:19:51 -0700

    wire up api for invoice table

--- packages/mint-components/src/components/sqm-invoice-table/UseInvoiceTable.stories.tsx
+++ packages/mint-components/src/components/sqm-invoice-table/UseInvoiceTable.stories.tsx
@@ -1,11 +1,9 @@
-import { setProgramId } from "@saasquatch/component-boilerplate";
 import {
+  setProgramId,
   setUserIdentity,
-  useLazyQuery,
 } from "@saasquatch/component-boilerplate";
-import { useEffect, useState } from "@saasquatch/universal-hooks";
+import { useEffect } from "@saasquatch/universal-hooks";
 import { h } from "@stencil/core";
-import { gql } from "graphql-request";
 import { createHookStory } from "../sqm-stencilbook/HookStoryAddon";
 
 export default {
@@ -13,20 +11,24 @@ export default {
 };
 
 function setupGraphQL() {
-  const id = "testestest";
-  const accountId = id;
+  const id = "07bc01a0f123d20342b89ed8296946a747f770cc8bbd239d7f142fe629f826ac";
+  const accountId =
+    "07bc01a0f123d20342b89ed8296946a747f770cc8bbd239d7f142fe629f826ac";
+
+  const programId = "22999";
 
   //@ts-ignore
   window.widgetIdent = {
-    tenantAlias: "test_a8b41jotf8a1v",
-    appDomain: "https://staging.invoicesaasquatch.com",
-    // programId,
+    tenantAlias: "aprh0cfq6y8tk",
+    appDomain: "https://staging.referralsaasquatch.com",
+    programId,
   };
+
   useEffect(() => {
     setUserIdentity({
       accountId,
       id,
-      jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImFjY291bnRJZCI6InRlc3Rlc3Rlc3QiLCJpZCI6InRlc3Rlc3Rlc3QifX0.qYnU5hNeIj9C_G3NogfG7btgCPGZC7JRXY0MG6a63zs",
+      jwt: "eyJraWQiOiJJUk1Yc1l5NllZcXE0Njk0MzdtRzhFUlF0OFFvS0ZCYUcxIiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJqdGkiOiI2NWVmYTdiMzY0NWJlNjQ2OGVjOGYwMDMiLCJpYXQiOjE3MTAyMDQ4NTEsImV4cCI6MTcxMDI5MTI1MSwic3ViIjoiTURkaVl6QXhZVEJtTVRJelpESXdNelF5WWpnNVpXUTRNamsyT1RRMllUYzBOMlkzTnpCall6aGlZbVF5TXpsa04yWXhOREptWlRZeU9XWTRNalpoWXc9PTpNRGRpWXpBeFlUQm1NVEl6WkRJd016UXlZamc1WldRNE1qazJPVFEyWVRjME4yWTNOekJqWXpoaVltUXlNemxrTjJZeE5ESm1aVFl5T1dZNE1qWmhZdz09QGFwcmgwY2ZxNnk4dGs6dXNlcnMiLCJ1c2VyIjp7ImlkIjoiMDdiYzAxYTBmMTIzZDIwMzQyYjg5ZWQ4Mjk2OTQ2YTc0N2Y3NzBjYzhiYmQyMzlkN2YxNDJmZTYyOWY4MjZhYyIsImFjY291bnRJZCI6IjA3YmMwMWEwZjEyM2QyMDM0MmI4OWVkODI5Njk0NmE3NDdmNzcwY2M4YmJkMjM5ZDdmMTQyZmU2MjlmODI2YWMiLCJkYXRlQmxvY2tlZCI6bnVsbH19.cYhNPaaBwffl3PxUZXuwQKmTd_3OgDGppxrBkMmJxNA",
     });
     return () => {
       window.widgetIdent = undefined;
@@ -37,33 +39,9 @@ function setupGraphQL() {
   return { id, accountId };
 }
 
-function setupGraphQLKlip({ token, id }) {
-  const accountId = id;
-  // const programId = "klip-invoice-program";
-
-  //@ts-ignore
-  window.widgetIdent = {
-    tenantAlias: "test_a74miwdpofztj",
-    appDomain: "https://staging.invoicesaasquatch.com",
-    // programId,
-  };
-  useEffect(() => {
-    setUserIdentity({
-      accountId,
-      id,
-      jwt: token,
-    });
-    return () => {
-      window.widgetIdent = undefined;
-      setUserIdentity(undefined);
-    };
-  }, []);
-
-  return { id, accountId };
-}
 export const InvoiceTable = createHookStory(() => {
   setupGraphQL();
-  setProgramId("sam-partner-test-2");
+  setProgramId("22999");
   const props = {
     listType: "",
     render: () => {},
@@ -72,10 +50,24 @@ export const InvoiceTable = createHookStory(() => {
   };
   return (
     <sqm-invoice-table>
-      <sqm-invoice-table-user-column></sqm-invoice-table-user-column>
-      <sqm-invoice-table-status-column></sqm-invoice-table-status-column>
-      <sqm-invoice-table-date-column></sqm-invoice-table-date-column>
-      <sqm-invoice-table-rewards-column></sqm-invoice-table-rewards-column>
+      <sqm-invoice-table-download-column></sqm-invoice-table-download-column>
+      <sqm-invoice-table-date-column column-title="Date"></sqm-invoice-table-date-column>
+      <sqm-invoice-table-data-column
+        column-title="Invoice"
+        property="invoiceId"
+      ></sqm-invoice-table-data-column>
+      <sqm-invoice-table-data-column
+        column-title="Earnings"
+        property="earnings"
+      ></sqm-invoice-table-data-column>
+      <sqm-invoice-table-data-column
+        column-title="Taxed Amount"
+        property="taxedAmount"
+      ></sqm-invoice-table-data-column>
+      <sqm-invoice-table-data-column
+        column-title="Earnings after tax"
+        property="netEarnings"
+      ></sqm-invoice-table-data-column>
     </sqm-invoice-table>
   );
 });
