fbba7a567858ee78caa2429218fd43ece189c48f
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2024-03-25 16:34:23 -0700

    improve spacing

--- packages/mint-components/src/components/tax-and-cash/sqm-indirect-tax-form/sqm-indirect-tax-form-view.tsx
+++ packages/mint-components/src/components/tax-and-cash/sqm-indirect-tax-form/sqm-indirect-tax-form-view.tsx
@@ -168,6 +168,7 @@ const style = {
   PageDescriptionText: {
     color: "var(--sl-color-neutral-500)",
     fontSize: "var(--sl-font-size-medium)",
+    marginBottom: "var(--sl-spacing-small)",
   },
 };
 
