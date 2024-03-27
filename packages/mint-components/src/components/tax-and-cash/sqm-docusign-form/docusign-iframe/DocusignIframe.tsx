9d5315a85a56aaa4d663d7eda76e769b7fb0cbac
Locrian24 <locrian24@gmail.com>
2024-03-27 09:45:08 -0700

    check e.origin includes saasquatch domain instead of explicit check

--- packages/mint-components/src/components/tax-and-cash/sqm-docusign-form/docusign-iframe/DocusignIframe.tsx
+++ packages/mint-components/src/components/tax-and-cash/sqm-docusign-form/docusign-iframe/DocusignIframe.tsx
@@ -131,8 +131,7 @@ export const DocusignIframe = ({
   if (states.urlLoading) return <DocusignLoadingView />;
 
   const callback = useCallback((e) => {
-    // TODO: CHANGE THIS WHEN ACTUAL URL IS AVAILABLE
-    if (e.origin !== "https://staging.referralsaasquatch.com") return;
+    if (e.origin?.includes("referralsaasquatch.com")) return;
     if (!e.data) return;
 
     callbacks.onStatusChange(e.data.eventStatus);
