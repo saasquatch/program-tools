86dff2c62db94da33f87df846d71ebabffb2cf1b
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2024-05-07 10:57:13 -0700

    allow 2 character locales as well

--- packages/component-environment/src/contexts/LocaleContext.ts
+++ packages/component-environment/src/contexts/LocaleContext.ts
@@ -5,7 +5,7 @@ import { debug as _debug } from "../debug";
 const debug = (...args: any[]) => _debug(LOCALE_CONTEXT_NAME, ...args);
 
 export function validateLocale(locale?: string) {
-  if (locale && /^[a-z]{2}_(?:[A-Z]{2}|[0-9]{3})$/.test(locale)) {
+  if (locale && /^[a-z]{2}(_(?:[A-Z]{2}|[0-9]{3}))?$/.test(locale)) {
     return locale;
   }
 }
