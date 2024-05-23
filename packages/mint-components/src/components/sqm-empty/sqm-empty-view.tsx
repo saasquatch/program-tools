a1e27cdf2c989c9e11c13726091546c5a10620b1
Sam Beveridge <sam.beveridge@referralsaasquatch.com>
2024-05-23 11:12:24 -0700

    added email template for leaderboard feature support link

--- packages/mint-components/src/components/sqm-empty/sqm-empty-view.tsx
+++ packages/mint-components/src/components/sqm-empty/sqm-empty-view.tsx
@@ -6,11 +6,17 @@ export interface EmptyStateViewProps {
   emptyStateHeader: string;
   emptyStateText: string;
   supportText?: string;
+  missingFeature?: string;
 }
 
 export function EmptyStateView(props: EmptyStateViewProps) {
-  const { emptyStateHeader, emptyStateImage, emptyStateText, supportText } =
-    props;
+  const {
+    emptyStateHeader,
+    emptyStateImage,
+    emptyStateText,
+    supportText,
+    missingFeature = "",
+  } = props;
 
   return (
     <sqm-portal-container padding="large" gap="medium" part="sqm-base">
@@ -32,7 +38,17 @@ export function EmptyStateView(props: EmptyStateViewProps) {
               },
               {
                 supportText: (
-                  <a target="_blank" href={`example.com`}>
+                  <a
+                    target="_blank"
+                    href={`mailto:saasquatch-support@impact.com?subject=Next steps for ${missingFeature} feature&body=Hi Support Team, %0D%0A
+                  %0D%0A  
+                  I am interested to learn more about the ${missingFeature} feature. Please let me know the next steps to access this feature, including any necessary plan upgrades.%0D%0A
+                  %0D%0A 
+                  - Feature Interested In: ${missingFeature}%0D%0A
+                  - Company Name: [Please fill out your company name here]%0D%0A
+                  %0D%0A
+                  Thank you`}
+                  >
                     {supportText}
                   </a>
                 ),
