37f50bae55873243e5f1f131a435c4abd6bf8f23
Locrian24 <locrian24@gmail.com>
2024-11-21 10:26:52 -0800

    prerelease 1.11.0-7

--- packages/mint-components/src/components/tax-and-cash/sqm-tax-and-cash/data.ts
+++ packages/mint-components/src/components/tax-and-cash/sqm-tax-and-cash/data.ts
@@ -83,20 +83,19 @@ export const GET_USER = gql`
               type
               dateCreated
             }
-            # !!!!! UNCOMMENT ME !!!!!!!!
-            # withdrawalSettings {
-            #   paymentMethod
-            #   paypalEmailAddress
-            #   bankCountry
-            #   bankAccountNumber
-            #   paymentSchedulingType
-            #   paymentThreshold
-            #   paymentDay
-            # }
-            # payoutsAccount {
-            #   hold
-            #   balance
-            # }
+            withdrawalSettings {
+              paymentMethod
+              paypalEmailAddress
+              bankCountry
+              bankAccountNumber
+              paymentSchedulingType
+              paymentThreshold
+              paymentDay
+            }
+            payoutsAccount {
+              hold
+              balance
+            }
           }
         }
       }
