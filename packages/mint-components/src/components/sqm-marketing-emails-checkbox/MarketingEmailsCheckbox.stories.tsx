b6f367e3dd98b81aca2f72473c29c7a34ff9eec5
Zach Harrison <zharrison100@gmail.com>
2025-02-14 10:32:01 -0800

    Add checkbox specs

--- packages/mint-components/src/components/sqm-marketing-emails-checkbox/MarketingEmailsCheckbox.stories.tsx
+++ packages/mint-components/src/components/sqm-marketing-emails-checkbox/MarketingEmailsCheckbox.stories.tsx
@@ -1,19 +1,12 @@
 import { h } from "@stencil/core";
-// import scenario from "./sqm-link-button.feature";
-import { MarketingEmailsCheckbox } from "./sqm-marketing-emails-checkbox";
+import scenario from "./sqm-marketing-emails-checkbox.feature";
 
 export default {
   title: "Components/Marketing Emails Checkbox",
-  //   parameters: {
-  //     scenario,
-  //   },
+  parameters: {
+    scenario,
+  },
 };
-
-// const defaultProps: MarketingEmailsCheckbox = {
-//   link: "https://example.com",
-//   openInNewTab: false,
-//   buttonText: "Button Text",
-// };
 
 export const Default = () => {
   return <sqm-marketing-emails-checkbox></sqm-marketing-emails-checkbox>;
