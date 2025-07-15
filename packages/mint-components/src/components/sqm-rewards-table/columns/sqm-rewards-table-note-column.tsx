79a6d09fc24dedba92f09e1da1bd937bb6c4c7c7
Andy <andy.luo@impact.com>
2025-07-15 13:46:41 -0700

    Rename example UI names for components

--- packages/mint-components/src/components/sqm-rewards-table/columns/sqm-rewards-table-note-column.tsx
+++ packages/mint-components/src/components/sqm-rewards-table/columns/sqm-rewards-table-note-column.tsx
@@ -8,7 +8,7 @@ import { RewardTableColumn } from "./RewardTableColumn";
  * @uiName Reward Table Customer Note Column
  * @validParents ["sqm-rewards-table"]
  * @exampleGroup Rewards
- * @example Reward Table Customer Note Column - <sqm-rewards-table-customer-note-column column-title="Note"></sqm-rewards-table-customer-note-column>
+ * @example Customer Note Column - <sqm-rewards-table-customer-note-column column-title="Note"></sqm-rewards-table-customer-note-column>
  */
 @Component({
   tag: "sqm-rewards-table-customer-note-column",
