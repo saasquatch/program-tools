f82ce04fe919663023e7cc4f02e84fe9d68898a3
Zach Harrison <zharrison100@gmail.com>
2025-10-08 08:58:06 -0700

    Test portal background default

--- packages/mint-components/src/components/sqm-portal-container/sqm-portal-container-view.tsx
+++ packages/mint-components/src/components/sqm-portal-container/sqm-portal-container-view.tsx
@@ -41,7 +41,7 @@ export function PortalContainerView(
       maxWidth: props.maxWidth,
       margin: props.center && "auto",
       justifyContent: props.justifyContent,
-      backgroundColor: props.backgroundColor || "var(--sqm-portal-background)",
+      backgroundColor: props.backgroundColor || "inherit",
     },
   };
 
