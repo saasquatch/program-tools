3623f96798a5331ee2c3b13bc4ce0ec060143525
Ashley Osuna <ashley.osuna@impact.com>
2026-09-10 20:39:19 -0700

    review comment

--- packages/mint-components/src/components/tax-and-cash/sqm-banking-info-form/sqm-banking-info-form-view.tsx
+++ packages/mint-components/src/components/tax-and-cash/sqm-banking-info-form/sqm-banking-info-form-view.tsx
@@ -340,7 +340,7 @@ export const BankingInfoFormView = (props: BankingInfoFormViewProps) => {
   };
   return (
     <sl-form
-      part="form-container"
+      part="container"
       class={classes.FormWrapper}
       onSl-submit={callbacks.onSubmit}
       ref={(el: HTMLFormElement) => (refs.formRef.current = el)}
