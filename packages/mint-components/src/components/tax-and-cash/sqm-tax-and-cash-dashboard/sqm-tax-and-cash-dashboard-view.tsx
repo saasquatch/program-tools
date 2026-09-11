65791c769485c68d04e24e590403d8dea953312f
Ashley Osuna <ashley.osuna@impact.com>
2026-09-10 20:10:50 -0700

    cleanup

--- packages/mint-components/src/components/tax-and-cash/sqm-tax-and-cash-dashboard/sqm-tax-and-cash-dashboard-view.tsx
+++ packages/mint-components/src/components/tax-and-cash/sqm-tax-and-cash-dashboard/sqm-tax-and-cash-dashboard-view.tsx
@@ -1055,7 +1055,7 @@ export const TaxAndCashDashboardView = (props: TaxAndCashDashboardProps) => {
         {alertInfo && (
           <sqm-form-message
             loading={states.loading}
-            exportparts="base: alert-base, icon:alert-icon"
+            exportparts="warningalert-base: alert-base, warningalert-icon: alert-icon, erroralert-base: alert-base, erroralert-icon: alert-icon, infoalert-base: alert-base, infoalert-icon: alert-icon"
             type={alertInfo?.alertType}
             class={alertInfo?.class}
             icon={alertInfo?.icon}
